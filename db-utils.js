c670564b10f95d43e332f515019c641561c49218
Nihanth Subramanya <nhnt11@gmail.com>
2018-02-15 19:48:17 +0530

    Fix database setup abstraction and use it in Subscribers test

--- db-utils.js
+++ db-utils.js
@@ -3,33 +3,23 @@
 const pg = require("pg");
 
 const AppConstants = require("./app-constants");
-const subscribers = require("./subscribers");
 
-async function setup() {
-  const client = new pg.Client({
-    database: AppConstants.DATABASE_NAME,
-    host: AppConstants.DATABASE_HOST,
-    port: AppConstants.DATABASE_PORT,
-  });
+const client = new pg.Client({
+  database: AppConstants.DATABASE_NAME,
+  host: AppConstants.DATABASE_HOST,
+  port: AppConstants.DATABASE_PORT,
+});
 
-  try {
-    await client.connect();
-    await client.query("DROP TABLE users;");
-    await client.query("CREATE TABLE users ( id serial PRIMARY KEY, email varchar(320) UNIQUE );");
-    await client.end();
-  } catch (e) {
-    console.log(e);
-  }
-}
+const DBUtils = {
+  async setupDatabase() {
+    try {
+      await client.connect();
+      await client.query("DROP TABLE users;");
+      await client.query("CREATE TABLE users ( email varchar(320) UNIQUE );");
+    } finally {
+      await client.end();
+    }
+  },
+};
 
-async function smokeTest() {
-  await setup();
-  const email = "test@test.com";
-  console.log(await subscribers.addUser(email));
-  console.log(await subscribers.getUser(email));
-  console.log(await subscribers.addUser(email));
-  console.log(await subscribers.deleteUser(email));
-  console.log(await subscribers.getUser(email));
-}
-
-smokeTest();
+module.exports = DBUtils;
--- tests/test-subscribers.js
+++ tests/test-subscribers.js
@@ -1,12 +1,24 @@
 "use strict";
 
 const test = require("tape");
+const pg = require("pg");
 
+const AppConstants = require("../app-constants");
 const Subscribers = require("../subscribers");
+const DBUtils = require("../db-utils");
 
-test("Test adding, getting, and deleting subscribers", (t) => {
-  t.plan(5);
-  (async () => {
+// eslint-disable-next-line no-process-env
+if (!process.env.TESTING_ENVIRONMENT) {
+  console.log("Attempting to run database tests without TESTING_ENVIRONMENT set, exiting.");
+  return;
+}
+
+const tests = [];
+tests.push({
+  msg: "Test adding, getting, and deleting subscribers",
+  callback: async (t) => {
+    t.plan(5);
+
     const email = "test@test.com";
 
     let ret = await Subscribers.addUser(email);
@@ -15,14 +27,12 @@ test("Test adding, getting, and deleting subscribers", (t) => {
       error: null,
     }, "Add a user.");
 
-
     ret = await Subscribers.getUser(email);
     t.deepEqual(ret, {
       success: true,
       error: null,
       email,
     }, "Get the same user.");
-
 
     ret = await Subscribers.addUser(email);
     t.deepEqual(ret, {
@@ -31,18 +41,31 @@ test("Test adding, getting, and deleting subscribers", (t) => {
       duplicate: true,
     }, "Add the same user again.");
 
-
     ret = await Subscribers.deleteUser(email);
     t.deepEqual(ret, {
       success: true,
       error: null,
     }, "Delete the user.");
 
-
     ret = await Subscribers.getUser(email);
     t.deepEqual(ret, {
       success: false,
       error: null,
     }, "Try getting the user - should fail.");
-  })();
+  },
 });
+
+(async function setupDbAndRunTests() {
+  try {
+    await DBUtils.setupDatabase();
+    for (const t of tests) {
+      test(t.msg, (testObj) => {
+        (async () => {
+          await t.callback(testObj);
+        })();
+      });
+    }
+  } catch (e) {
+    console.log(e);
+  }
+})();
