f48654c3a19fce8755a2817d0a9bd0fbb70480ad
groovecoder <luke.crouch@gmail.com>
2018-04-23 12:12:23 +0200

    use env vars via AppConstants for knex config

--- db/knexfile.js
+++ db/knexfile.js
@@ -1,10 +1,10 @@
 "use strict";
 
+const AppConstants = require("../app-constants");
+
 module.exports = {
   client: "postgresql",
-  connection: {
-    database: "blurts",
-  },
+  connection: AppConstants.DATABASE_URL,
   pool: {
     min: 2,
     max: 10,
