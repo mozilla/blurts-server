4b20e4fb990a0c60f86fe8711a6239dcf9f5183f
Nihanth Subramanya <nhnt11@gmail.com>
2018-04-25 19:18:26 +0200

    Use monitor view for index route, show breach data in monitor view only if (arbitrary) breach parameter supplied in URL.

--- routes/home.js
+++ routes/home.js
@@ -8,24 +8,21 @@ const DBUtils = require("../db/utils");
 const router = express.Router();
 const urlEncodedParser = bodyParser.urlencoded({ extended: false });
 
-router.get("/", urlEncodedParser, (req, res) => {
-  res.render("home", {
-    title: "Firefox Breach Alerts",
-  });
-});
-
-router.get("/monitor", urlEncodedParser, (req, res) => {
+function handleIndexRoute(req, res) {
   res.render("monitor", {
     title: "Firefox Monitor",
-    breach: {
+    breach: req.query.breach ? {
       name: "AllMusic Breach",
       date: "6 December 2015",
       dataClasses: "Email addresses, IP addresses, Passwords, Usernames, Website activity",
       acCount: "1,436,486",
       description: "In December 2015, the service for creating and running AllMusic servers suffered a data breach that impacted 1.4 million subscribers.",
-    },
+    } : null,
   });
-});
+}
+
+router.get("/", urlEncodedParser, handleIndexRoute);
+router.get("/monitor", urlEncodedParser, handleIndexRoute);
 
 router.post("/scan", urlEncodedParser, async (req, res) => {
   const email = req.body.email;
