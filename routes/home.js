4c4dbb64aadc39a0828ea3cf83c0dd4a4cce35f8
Nihanth Subramanya <nhnt11@gmail.com>
2018-04-26 00:17:10 +0200

    Fix AllMusic thumbnail in monitor view

--- routes/home.js
+++ routes/home.js
@@ -12,7 +12,7 @@ function handleIndexRoute(req, res) {
   res.render("monitor", {
     title: "Firefox Monitor",
     breach: req.query.breach ? {
-      name: "AllMusic Breach",
+      name: "AllMusic",
       date: "6 December 2015",
       dataClasses: "Email addresses, IP addresses, Passwords, Usernames, Website activity",
       acCount: "1,436,486",
