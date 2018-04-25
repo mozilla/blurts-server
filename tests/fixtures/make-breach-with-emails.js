bc28e886a09b00ad8c66c60d4de48c61c34df290
Nihanth Subramanya <nhnt11@gmail.com>
2018-04-25 18:26:53 +0200

    Change dummy breach data to look more realistic

--- tests/fixtures/make-breach-with-emails.js
+++ tests/fixtures/make-breach-with-emails.js
@@ -9,29 +9,29 @@ const extraTestEmail = process.argv[2];
 
 const sampleBreaches = [
   {
-    name: "Test Breach 1",
+    name: "Linkedin",
     meta: {
-      date: "1/1/1",
-      dataClasses: "a, b, c",
-      acCount: 123,
+      date: "5 May 2012",
+      dataClasses: "Email addresses, Passwords",
+      acCount: "164,611,595",
     },
     emails: [ "test1@test.com", "test2@test.com" ],
   },
   {
-    name: "Test Breach 2",
+    name: "Adobe",
     meta: {
-      date: "2/2/2",
-      dataClasses: "d, e, f",
-      acCount: 456,
+      date: "4 October 2013",
+      dataClasses: "Email addresses, Password hints, Passwords, Usernames",
+      acCount: "152,445,165",
     },
     emails: [ "test2@test.com", "test3@test.com" ],
   },
   {
-    name: "Test Breach 3",
+    name: "AllMusic",
     meta: {
-      date: "3/3/3",
-      dataClasses: "g, h, i",
-      acCount: 789,
+      date: "6 December 2015",
+      dataClasses: "Email addresses, IP addresses, Passwords, Usernames, Website activity",
+      acCount: "1,436,486",
     },
     emails: [ "test3@test.com", "test1@test.com" ],
   },
