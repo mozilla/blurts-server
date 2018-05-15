2e4cc228cb98e39fee29da414c1a4f3c038bffc5
groovecoder <luke.crouch@gmail.com>
2018-05-15 15:15:06 -0500

    update make-breach-with-emails to match

--- tests/fixtures/make-breach-with-emails.js
+++ tests/fixtures/make-breach-with-emails.js
@@ -1,11 +1,24 @@
 "use strict";
 
+const arg = require("arg");
+
 require("dotenv").load();
 
 const DBUtils = require("../../db/utils");
 
 
-const extraTestEmail = process.argv[2];
+const args = arg({
+  "--createAMBreach": Boolean,
+  "--extraTestEmail": String,
+  "--help": Boolean,
+});
+
+if (args["--help"]) {
+  console.log("Usage: node make-breach-with-emails.js [--createAMBreach] [--extraTestEmail=<...>]");
+  console.log("--createAMBreach creates the 'AllMusic' test fixture breach.");
+  console.log("--extraTestEmail adds the supplied test email address and includes it in the LinkedIn, Adobe, and AllMusic breaches.");
+  process.exit();
+}
 
 const sampleBreaches = [
   {
@@ -23,12 +36,20 @@ const sampleBreaches = [
 ];
 
 (async () => {
+  if (args['--createAMBreach']) {
+    const allMusicBreach = await DBUtils.createBreach("AllMusic", {
+      Name: "AllMusic",
+      BreachDate: "2015-012-06",
+      DataClasses: ["Email addresses", "IP addresses", "Passwords", "Usernames", "Website activity"],
+      PwnCount: 1436486,
+    });
+  }
   for (const sB of sampleBreaches) {
     const breach = await DBUtils.getBreachByName(sB.name);
     for (const e of sB.emails) {
       await DBUtils.addBreachedEmail(sB.name, e);
-      if (extraTestEmail) {
-        DBUtils.addBreachedEmail(sB.name, extraTestEmail);
+      if (args["--extraTestEmail"]) {
+        DBUtils.addBreachedEmail(sB.name, args["--extraTestEmail"]);
       }
     }
   }
@@ -36,10 +57,7 @@ const sampleBreaches = [
   const testEmail = "test1@test.com";
   const foundBreaches = await DBUtils.getBreachesForEmail(testEmail);
   await DBUtils.deleteBreach(999999);
-  console.log(`\n\n${testEmail} was found in the following breaches:\n`);
-  console.log(foundBreaches.map(b => b.name));
   const breach = await DBUtils.getBreachByName("LinkedIn");
-  console.log(breach);
   await DBUtils.setBreachedHashNotified(breach, testEmail);
   // eslint-disable-next-line no-process-exit
   process.exit();
