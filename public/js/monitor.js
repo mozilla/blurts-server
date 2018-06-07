11d1f2532a7a9559eae1963a79d1900be3db00b4
Lesley Norton <lesley@lesleynorton.com>
2018-06-06 22:24:04 -0500

    tweaks
    tweaks

--- public/js/monitor.js
+++ public/js/monitor.js
@@ -2,6 +2,28 @@
 
 "use strict";
 
+// function doXHR(aURL, aBodyObj, aAlertText, aDebug=true) {
+//   return new Promise((resolve) => {
+//     const xhr = new XMLHttpRequest();
+//     xhr.onreadystatechange = function() {
+//       if (this.readyState === 4) {
+//         if (aAlertText) {
+//           alert(aAlertText);
+//         }
+//         if (aDebug) {
+//           const responseDumpElt = document.getElementById("responseDump");
+//           responseDumpElt.appendChild(document.createTextNode(xhr.response));
+//           responseDumpElt.appendChild(document.createElement("hr"));
+//           responseDumpElt.appendChild(document.createElement("br"));
+//         }
+//         resolve(xhr.response);
+//       }
+//     };
+//     xhr.open("POST", aURL, true);
+//     xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+//     xhr.send(JSON.stringify(aBodyObj));
+//   });
+// }
 function isValidEmail(val) {
   // https://stackoverflow.com/a/46181
   const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -9,8 +31,7 @@ function isValidEmail(val) {
 }
 
 function enableBtnIfEmailValid(e) {
-  console.log(e);
-  const emailBtn = document.getElementById("submit-email");
+  const emailBtn = document.getElementById("subscribe-email-btn");
   if (isValidEmail(e.target.value)) {
     emailBtn.disabled = false;
   } else {
@@ -32,7 +53,7 @@ function displayLoader(){
 }
 
 function showFalseDoor(){
-  const falseDoorBlurb = "<div class='section-container'><h4>Thank you for trying Firefox Monitor</h4><p>Firefox Monitor is a concept we are testing. During this test, we are not storing email addresses. This means that while we will use your email to give you real results about data breaches, we will not keep your email to alert you in case of future breaches.</p><p>We hope to provide this service soon, but in the meantime, you can stay up-to-date on Firefox Monitor and other new features when you sign up for the <a href='https://www.mozilla.org/newsletter/firefox/'>Firefox newsletter.</a></p><button class='button' id='close-false-door'>Close</button></div>";
+  const falseDoorBlurb = "<div class='section-container'><h4>Thank you for trying Firefox Monitor</h4><p>FireFox Monitor is a concept we are testing. We hope to provide the service to everyone soon.</p><p>Stay up-to-date with Firefox Monitor and other new features when you sign up for the <a href='https://www.mozilla.org/newsletter/firefox/'>Firefox newsletter.</a></p><button class='button' id='close-false-door'>Close</button></div>";
   const falseDoor = document.createElement("div");
   falseDoor.setAttribute("id", "false-door");
   document.body.appendChild(falseDoor);
@@ -43,24 +64,33 @@ function showFalseDoor(){
   };
 }
 
+async function sha1(message) {
+  const msgBuffer = new TextEncoder("utf-8").encode(message);
+  const hashBuffer = await crypto.subtle.digest("SHA-1", msgBuffer);
+  const hashArray = Array.from(new Uint8Array(hashBuffer));
+  const hashHex = hashArray.map(b => ("00" + b.toString(16)).slice(-2)).join("");
+  return hashHex;
+}
+
 async function hashEmailAndSend(emailFormSubmitEvent) {
   emailFormSubmitEvent.preventDefault();
   const emailForm = emailFormSubmitEvent.target;
-  // luke's code for sending to sha1 etc
+  for (const emailInput of emailForm.querySelectorAll("input[type=email]")) {
+    emailForm.querySelector("input[name=emailHash]").value = await sha1(emailInput.value);
+    emailInput.value = "";
+  }
   emailForm.submit();
   displayLoader();
 }
 
-
 if(document.querySelector(".email-scan")){
+  //removes "loading-data" class from button even when user clicks the back button. 
   window.addEventListener("pageshow", removeLoader);
   document.querySelector(".email-scan").addEventListener("submit", hashEmailAndSend);
-  document.querySelector(".email-to-hash").addEventListener("input", enableBtnIfEmailValid);
 }
 
-//removes "loading-data" class from button even when user clicks the back button. 
+$(document).foundation();
+
 document.querySelector("#sign-up").addEventListener("click", showFalseDoor);
-
-
-
-
+// document.querySelector("#subscribe-fxa-btn").addEventListener("click", doOauth);
+// document.querySelector("#subscribe-email-input").addEventListener("input", enableBtnIfEmailValid);
