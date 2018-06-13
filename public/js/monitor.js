7766a02574530341186eb6acd89efc9e99834804
lesleyjanenorton <lesley@lesleynorton.com>
2018-06-12 21:26:02 -0500

    analytics and dnt-helper

--- public/js/monitor.js
+++ public/js/monitor.js
@@ -1,4 +1,54 @@
 "use strict";
+/* global ga */
+
+function sendEvent(category, action, label){
+  ga("send", "event", category, action, label);
+}
+
+function handleSignUpClicks(string){
+  const eventCategory = "Sign Ups";
+  const eventAction = "Clicked Sign Up";
+  if(window.location.href.indexOf("breach") > -1){
+    sendEvent(eventCategory, eventAction, "Sign up button clicked from featured breach.");
+  }
+  else if (document.getElementById("what-to-do")) {
+    sendEvent(eventCategory, eventAction, "Sign up button clicked after scanning email with breaches.");
+  }
+  else {
+    if(window.location.href.indexOf("scan") > -1){
+      sendEvent(eventCategory, eventAction, "Sign up button clicked after scanning email with no breaches.");
+    }
+    else {
+      sendEvent(eventCategory, eventAction, "Sign up button clicked from landing page.");
+    }
+  }
+}
+
+function handleEmailScans(){
+  const eventCategory = "Scans";
+  const eventAction = "Scanned Email Address";
+  if(window.location.href.indexOf("breach") > -1){
+    sendEvent(eventCategory, eventAction, "User submitted email from a featured breach.");
+  }
+  else if(document.getElementById("what-to-do")){
+    sendEvent(eventCategory, eventAction, "User submitted additional email.");
+  }
+  else {
+    sendEvent(eventCategory, eventAction, "User submitted email from homepage.");
+  }
+}
+
+function handlePageViews(){
+  const eventCategory = "Scan Results";
+  const eventAction = "Viewed Scan Results";
+  if(window.location.href.indexOf("scan") > -1){
+    if(document.getElementById("what-to-do")){
+      sendEvent(eventCategory, eventAction, "Found Breaches");
+    } else {
+      sendEvent(eventCategory, eventAction, "No Breaches");
+    }
+  }
+}
 
 function isValidEmail(val) {
   // https://stackoverflow.com/a/46181
@@ -29,6 +79,7 @@ function displayLoader(){
 }
 
 function showFalseDoor(){
+  handleSignUpClicks();
   const falseDoor = document.getElementById("false-door");
   falseDoor.classList.remove("hidden");
   document.getElementById("close-false-door").onclick = function (){
@@ -46,6 +97,7 @@ async function sha1(message) {
 
 async function hashEmailAndSend(emailFormSubmitEvent) {
   emailFormSubmitEvent.preventDefault();
+  handleEmailScans();
   const emailForm = emailFormSubmitEvent.target;
   for (const emailInput of emailForm.querySelectorAll("input[type=email]")) {
     emailForm.querySelector("input[name=emailHash]").value = await sha1(emailInput.value);
@@ -60,5 +112,5 @@ if(document.querySelector(".email-scan")){
   document.querySelector(".email-scan").addEventListener("submit", hashEmailAndSend);
   document.querySelector(".email-to-hash").addEventListener("input", enableBtnIfEmailValid);
 }
-
-document.querySelector("#sign-up").addEventListener("click", showFalseDoor);
+window.addEventListener("pageshow", handlePageViews);
+document.getElementById("sign-up").addEventListener("click", showFalseDoor);
