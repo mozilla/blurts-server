a7c81b2e0aebd4d5c610d4ab9c4f9e78c1ae3c68
Peter deHaan <peter@deseloper.com>
2018-06-14 10:26:41 -0700

    Initial work on load tests

---
+++ loadtests/README.md
@@ -0,0 +1,13 @@
+# Firefox Monitor Loadtests
+
+## Requirements
+
+- Python 3.6
+- pip
+- venv
+
+## Setup
+
+```
+TODO
+```
---
+++ loadtests/loadtest.py
@@ -0,0 +1,18 @@
+from molotov import scenario
+
+
+HOMEPAGE = 'https://fx-breach-alerts.herokuapp.com'
+SCAN = 'https://fx-breach-alerts.herokuapp.com/scan'
+TEST_EMAIL_HASH = 'b84c4c03b2af4050ac2d3b105e58adf83fa5af05'
+
+
+@scenario(weight=50)
+async def scenario_one(session):
+    async with session.get(HOMEPAGE) as resp:
+        assert resp.status == 200
+
+
+@scenario(weight=50)
+async def scenario_two(session):
+    async with session.post(SCAN, data={'emailHash': TEST_EMAIL_HASH}) as resp:
+        assert resp.status == 200
