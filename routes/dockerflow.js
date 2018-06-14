46bff4cb83c7d3f2295844c3849c178b6642a623
Nihanth Subramanya <nhnt11@gmail.com>
2018-06-13 18:02:58 -0700

    Don't overwrite version.json in dockerflow route

--- routes/dockerflow.js
+++ routes/dockerflow.js
@@ -1,28 +1,11 @@
 "use strict";
 
 const express = require("express");
-const fs = require("fs");
 const path = require("path");
-const {version, homepage} = require("../package.json");
-
 
 const router = express.Router();
 
-let commit;
-try {
-  commit = require("git-rev-sync").short();
-} catch (err) {
-  console.error("Error in git-rev-sync: ", err);
-}
-
 const versionJsonPath = path.join(__dirname, "..", "version.json");
-const versionJson = {
-  commit,
-  source: homepage,
-  version,
-};
-
-fs.writeFileSync(versionJsonPath, JSON.stringify(versionJson, null, 2) + "\n");
 
 router.get("/__version__", (req, res) => {
   return res.sendFile(versionJsonPath);
@@ -36,6 +19,4 @@ router.get("/__lbheartbeat__", (req, res) => {
   return res.send("OK");
 });
 
-
 module.exports = router;
-
