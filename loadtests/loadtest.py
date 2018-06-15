33ec48cfd93544543643306eec7755011d20b4ad
Peter deHaan <peter@deseloper.com>
2018-06-14 17:02:49 -0700

    Mild refactoring

--- loadtests/loadtest.py
+++ loadtests/loadtest.py
@@ -1,19 +1,21 @@
+from os import getenv
 from molotov import scenario
 
 
-HOMEPAGE = 'https://fx-breach-alerts.herokuapp.com'
-SCAN = 'https://fx-breach-alerts.herokuapp.com/scan'
-# This hash is for test@example.com
-TEST_EMAIL_HASH = 'b84c4c03b2af4050ac2d3b105e58adf83fa5af05'
+SERVER_URL = getenv('SERVER_URL', 'https://fx-breach-alerts.herokuapp.com')
+HOMEPAGE_URL = SERVER_URL
+SCAN_URL = '{}/scan'.format(SERVER_URL)
+# This hash is for test@example.com (via `node scripts/make-sha1-hashes.js`)
+TEST_EMAIL_HASH = '567159d622ffbb50b11b0efd307be358624a26ee'
 
 
 @scenario(weight=50)
-async def scenario_one(session):
-    async with session.get(HOMEPAGE) as resp:
+async def homepage_test(session):
+    async with session.get(HOMEPAGE_URL) as resp:
         assert resp.status == 200
 
 
 @scenario(weight=50)
-async def scenario_two(session):
-    async with session.post(SCAN, data={'emailHash': TEST_EMAIL_HASH}) as resp:
+async def scan_test(session):
+    async with session.post(SCAN_URL, data={'emailHash': TEST_EMAIL_HASH}) as resp:
         assert resp.status == 200
