2e49c90a3902ec4dd68b30a6e039273fd84e4d9a
Nihanth Subramanya <nhnt11@gmail.com>
2018-06-29 12:52:17 +0200

    Upper case the hash before sending

--- public/js/monitor.js
+++ public/js/monitor.js
@@ -143,7 +143,7 @@ async function sha1(message) {
   const hashBuffer = await crypto.subtle.digest("SHA-1", msgBuffer);
   const hashArray = Array.from(new Uint8Array(hashBuffer));
   const hashHex = hashArray.map(b => ("00" + b.toString(16)).slice(-2)).join("");
-  return hashHex;
+  return hashHex.toUpperCase();
 }
 
 async function hashEmailAndSend(emailFormSubmitEvent) {
