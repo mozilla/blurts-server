2e0ea95854b33399c4c09acf978fb20bd4305c9b
Luke Crouch <lcrouch@mozilla.com>
2018-08-10 13:57:26 -0500

    for #251: use textParser for SES

--- routes/ses.js
+++ routes/ses.js
@@ -4,10 +4,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const router = express.Router();
-const jsonParser = bodyParser.json();
+const textParser = bodyParser.text();
 
 
-router.post("/notification", jsonParser, async (req, res) => {
+router.post("/notification", textParser, async (req, res) => {
   try {
     console.log("ses-notification request body: ", req.body);
 
