8a2ab1acba9256980e8d1af9c8746b6d86c30f9c
Luke Crouch <lcrouch@mozilla.com>
2018-08-23 16:42:37 -0500

    SES_NOTIFICATION_LOG_ONLY flag

--- routes/ses.js
+++ routes/ses.js
@@ -3,6 +3,7 @@
 const bodyParser = require("body-parser");
 const express = require("express");
 
+const AppConstants = require("../app-constants");
 const {sesAuth} = require("../middleware");
 const {notification} = require("../controllers/ses");
 
@@ -10,7 +11,13 @@ const {notification} = require("../controllers/ses");
 const router = express.Router();
 const textParser = bodyParser.text();
 
-router.use("/notification", sesAuth);
-router.post("/notification", textParser, notification);
+if (AppConstants.SES_NOTIFICATION_LOG_ONLY) {
+  router.post("/notification", textParser, (req, res, next) => {
+    console.log("SES Notification request body: ", req.body);
+  });
+} else {
+  router.use("/notification", sesAuth);
+  router.post("/notification", textParser, notification);
+}
 
 module.exports = router;
