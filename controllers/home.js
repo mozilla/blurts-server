88357649e2290472a64c1c4d56d1254117867e40
Lesley Norton <lesley@lesleynorton.com>
2018-08-23 13:26:29 -0500

    Prevent 'featured breach' results on subsequent email scans.
    Changes conditional in scan-form.hbs to scan form to make sure that feautred breach results are only returned when a user scans their email from a featured breach "/?breach=" page and not on scans initiated from results pages that contain a featuredBreach listing.

--- controllers/home.js
+++ controllers/home.js
@@ -5,14 +5,17 @@ const TIPS = require("../tips");
 
 function home(req, res) {
   let featuredBreach = null;
+  let scanFeaturedBreach = null;
   if (req.query.breach) {
     const reqBreachName = req.query.breach.toLowerCase();
     featuredBreach = req.app.locals.breaches.filter(breach => breach.Name.toLowerCase() === reqBreachName)[0];
+    scanFeaturedBreach = true;
   }
   res.render("monitor", {
     title: "Firefox Monitor",
     featuredBreach: featuredBreach,
     passwordTips: TIPS,
+    scanFeaturedBreach,
   });
 }
 
