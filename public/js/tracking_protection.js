9677725334c193f60616af55faf7e8da095b8818
Lesley Norton <lesley@lesleynorton.com>
2018-08-27 17:02:58 -0500

    Move browser name script to grab browser type even if tracking protection is enabled.

--- public/js/tracking_protection.js
+++ public/js/tracking_protection.js
@@ -26,19 +26,19 @@
 		} catch (e) {
 			return "unknown";
 		}
-	}
+  }
+  
+  if (browserName() == "firefox") {
+    if (document.getElementById("download-firefox")) {
+    document.getElementById("download-firefox").classList.add("hide");
+    }
+  }
 	
 if (!_dntEnabled()) {
 	(function(i,s,o,g,r,a,m){i["GoogleAnalyticsObject"]=r;i[r]=i[r]||function(){
 	(i[r].q=i[r].q||[]).push(arguments);},i[r].l=1*new Date();a=s.createElement(o),
 	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m);
 	})(window,document,"script","https://www.google-analytics.com/analytics.js","ga");
-
-	if (browserName() == "firefox") {
-		if (document.getElementById("download-firefox")) {
-		document.getElementById("download-firefox").classList.add("hide");
-		}
-	}
 }
 
 if(typeof(ga) !== "undefined") {
