4ddb47a0602d34d3743ab8c6d02c0cb1ab074784
Lesley Norton <lesley@lesleynorton.com>
2018-08-28 13:36:11 -0500

    Adds handlebars function to spell numbers less than 10 on scan results page.

--- hbs-helpers.js
+++ hbs-helpers.js
@@ -36,16 +36,34 @@ const HBSHelpers = {
       }
     });
 
-    hbs.registerHelper("math", function(lValue, operator, rValue) {
+    hbs.registerHelper("breachMath", function(lValue, operator, rValue) {
       lValue = parseFloat(lValue);
       rValue=parseFloat(rValue);
-      return {
+      const returnValue = {
         "+": lValue + rValue,
         "-": lValue - rValue,
         "*": lValue - rValue,
         "/": lValue / rValue,
         "%": lValue % rValue,
       }[operator];
+      if (returnValue < 10) {
+        return {
+          1: "one",
+          2: "two",
+          3: "three",
+          4: "four",
+          5: "five",
+          6: "six",
+          7: "seven",
+          8: "eight",
+          9: "nine",
+        }[returnValue];
+      }
+      return returnValue;
+    });
+
+    hbs.registerHelper("encodeURI", function(input) {
+      return encodeURI(input);
     });
   },
 };
