b255f5df414ab4535ca619a3034ab5053cb696e3
Lesley Norton <lesley@lesleynorton.com>
2018-10-17 15:54:25 -0500

    Review updates

--- hibp.js
+++ hibp.js
@@ -67,7 +67,7 @@ const HIBP = {
     const emailLogos = [];
     const images = fs.readdirSync("./public/img/logos/");
     images.forEach(file => {
-      if (file.endsWith("png")) {
+      if (file.endsWith(".png")) {
         emailLogos.push(file);
       }
     });
@@ -89,7 +89,7 @@ const HIBP = {
           log.info(`missing image ${breach.Name}`);
           breach.ImgSrc = "missing-logo-icon";
         } else {
-          breach.ImgSrc = `${breach.Name}`;
+          breach.ImgSrc = breach.Name;
         }
         breaches.push(breach);
       }
@@ -134,7 +134,7 @@ const HIBP = {
     // ]
     for (const breachedAccount of response.body) {
       if (sha1.toUpperCase() === sha1Prefix + breachedAccount.hashSuffix) {
-        foundBreaches = allBreaches.filter(breach => breachedAccount.websites.includes(breach.Title));
+        foundBreaches = allBreaches.filter(breach => breachedAccount.websites.includes(breach.Name));
         break;
       }
     }
