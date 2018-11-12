9728872f3886e6408e5be72509654243a1b58153
Lesley Norton <lesley@lesleynorton.com>
2018-11-12 14:41:39 -0600

    Update sensitive featured breach pages.
    Remove scanning functionality from featured breach pages when breach.IsSensitive.

--- controllers/home.js
+++ controllers/home.js
@@ -5,15 +5,21 @@
 function home(req, res) {
   let featuredBreach = null;
   let scanFeaturedBreach = false;
+  let breachIsSensitive = false;
+
   if (req.query.breach) {
     const reqBreachName = req.query.breach.toLowerCase();
     featuredBreach = req.app.locals.breaches.filter(breach => breach.Name.toLowerCase() === reqBreachName)[0];
     scanFeaturedBreach = true;
+    if (featuredBreach.IsSensitive) {
+      breachIsSensitive = true;
+    }
   }
   res.render("monitor", {
     title: req.fluentFormat("home-title"),
     featuredBreach: featuredBreach,
     scanFeaturedBreach,
+    breachIsSensitive,
   });
 }
 
