28c53f3c383ab636c1cbb73c9553943b8869c6f8
Luke Crouch <lcrouch@mozilla.com>
2019-01-17 15:12:21 -0600

    bug 1517343 - add index to verified column

---
+++ db/migrations/20190117150910_add_verified_index.js
@@ -0,0 +1,14 @@
+"use strict";
+
+
+exports.up = function(knex, Promise) {
+  return knex.schema.table("subscribers", table => {
+    table.index("verified", "subscribers_verified_idx");
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.table("subscribers", table => {
+    table.dropIndex("verified", "subscribers_verified_idx");
+  });
+};
