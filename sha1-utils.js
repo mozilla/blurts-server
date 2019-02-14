229679d8cad8e052d4db0ac72d4d7e1ad63fb0cb
Lesley Norton <lesley@lesleynorton.com>
2019-02-14 15:15:06 -0600

    Remove .toUpperCase() from sha1()
    This is goofing up hibp.test.js.

--- sha1-utils.js
+++ sha1-utils.js
@@ -3,7 +3,7 @@
 const crypto = require("crypto");
 
 function getSha1(email) {
-  return crypto.createHash("sha1").update(email).digest("hex").toUpperCase();
+  return crypto.createHash("sha1").update(email).digest("hex");
 }
 
 module.exports = getSha1;
