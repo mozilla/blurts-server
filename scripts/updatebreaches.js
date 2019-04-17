68f6ac62c2ec0098d86d993ad505ca1c58a577af
Luke Crouch <lcrouch@mozilla.com>
2019-04-17 15:59:31 -0500

    refactor into lib/remote-settings module

--- scripts/updatebreaches.js
+++ scripts/updatebreaches.js
@@ -1,35 +1,16 @@
 "use strict";
 
-const got = require("got");
-
 const AppConstants = require("../app-constants");
 const HIBP = require("../hibp");
+const RemoteSettings = require("../lib/remote-settings");
 
 
-const BREACHES_COLLECTION = "fxmonitor-breaches";
-const FX_RS_RECORDS = `${AppConstants.FX_REMOTE_SETTINGS_WRITER_SERVER}/buckets/main-workspace/collections/${BREACHES_COLLECTION}/records`;
-const FX_RS_COLLECTION = `${AppConstants.FX_REMOTE_SETTINGS_WRITER_SERVER}/buckets/main-workspace/collections/${BREACHES_COLLECTION}`;
-const FX_RS_BEARER_TOKEN = AppConstants.FX_REMOTE_SETTINGS_BEARER_TOKEN;
-
-
-if (!FX_RS_BEARER_TOKEN) {
+if (
+  !AppConstants.FX_REMOTE_SETTINGS_BEARER_TOKEN ||
+  !AppConstants.FX_REMOTE_SETTINGS_WRITER_SERVER
+) {
   console.error("updatebreaches requires FX_RS_BEARER_TOKEN.");
   process.exit(1);
-}
-
-
-async function whichBreachesAreNotInRemoteSettingsYet(hibpBreaches) {
-  const fxRSRecords = await got(FX_RS_RECORDS, {
-    json: true,
-    headers: {
-      "authorization": `Bearer ${FX_RS_BEARER_TOKEN}`,
-    },
-  });
-  const remoteSettingsBreachesSet = new Set(
-    fxRSRecords.body.data.map(b => b.Name)
-  );
-
-  return hibpBreaches.filter( ({Name}) => !remoteSettingsBreachesSet.has(Name) );
 }
 
 
@@ -37,7 +18,7 @@ async function whichBreachesAreNotInRemoteSettingsYet(hibpBreaches) {
   const allHibpBreaches = await HIBP.req("/breaches");
   const verifiedSiteBreaches = HIBP.filterBreaches(allHibpBreaches.body);
 
-  const newBreaches = await whichBreachesAreNotInRemoteSettingsYet(verifiedSiteBreaches);
+  const newBreaches = await RemoteSettings.whichBreachesAreNotInRemoteSettingsYet(verifiedSiteBreaches);
 
   if (newBreaches.length <= 0) {
     console.log("No new breaches detected.");
@@ -55,26 +36,17 @@ async function whichBreachesAreNotInRemoteSettingsYet(hibpBreaches) {
       AddedDate: breach.AddedDate,
     };
 
+    console.log("New breach detected: \n", data);
+
     try {
-      // Create the record
-      await got.post(FX_RS_RECORDS, {
-        headers: {
-          "Content-Type": "application/json",
-          "authorization": `Bearer ${FX_RS_BEARER_TOKEN}`,
-        },
-        body: JSON.stringify({data: data}),
-      });
+      await RemoteSettings.postNewBreachToBreachesCollection(data);
     } catch (e) {
       console.error(e);
       process.exit(1);
     }
   }
-  // Request a review on the collection
-  await got.patch(FX_RS_COLLECTION, {
-    headers: {
-      "Content-Type": "application/json",
-      "authorization": `Bearer ${FX_RS_BEARER_TOKEN}`,
-    },
-    body: JSON.stringify({data: {status: "to-review"}}),
-  });
+
+  console.log("Requesting review on breaches collection");
+  await RemoteSettings.requestReviewOnBreachesCollection();
+
 })();
