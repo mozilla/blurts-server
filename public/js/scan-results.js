14b1172dafc68c121b203aa535a74e802db973f0
Lesley Norton <lesley@lesleynorton.com>
2019-04-22 14:36:52 -0500

    Clean up/relocate parts of monitor.js

--- public/js/scan-results.js
+++ public/js/scan-results.js
@@ -1,6 +1,23 @@
 "use strict";
 
-(async() => {
+(() => {
+
+  const showAdditionalBreaches = () => {
+    const showBreachesButton = document.getElementById("show-all-breaches-wrapper");
+    const additionalBreaches = document.getElementById("additional-breaches");
+    [showBreachesButton, additionalBreaches].forEach(el => {
+      ["show", "hide"].forEach(className => {
+        el.classList.toggle(className);
+      });
+    });
+  };
+
+  const showAllBreaches = document.getElementById("show-all-breaches");
+
+  if (showAllBreaches) {
+    showAllBreaches.addEventListener("click", showAdditionalBreaches);
+  }
+
   if (document.getElementById("scannedEmail")) {
     const scannedEmail = document.getElementById("scannedEmail");
     const emailId = scannedEmail.dataset.scannedEmailId;
