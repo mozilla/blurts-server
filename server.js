6753d75da555cdf0639169d566667b1dd0a2baa1
Luke Crouch <lcrouch@mozilla.com>
2019-09-23 09:00:57 -0500

    bug 1582239: disable helmet csp browserSniff

--- server.js
+++ server.js
@@ -127,6 +127,7 @@ app.use(helmet.contentSecurityPolicy({
     styleSrc: STYLE_SOURCES,
     reportUri: "/__cspreport__",
   },
+  browserSniff: false,
 }));
 app.use(helmet.referrerPolicy({ policy: "strict-origin-when-cross-origin" }));
 app.use(express.static("public", {
