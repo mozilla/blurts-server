f7221c1c369f9993f20cdab8a10c831d4647c8b5
Luke Crouch <lcrouch@mozilla.com>
2019-10-03 16:57:19 -0500

    for #1225: try well-known/change-password urls

--- controllers/breach-details.js
+++ controllers/breach-details.js
@@ -1,8 +1,11 @@
 "use strict";
 
+const got = require("got");
+const { URL } = require("url");
+
 const HIBP = require("../hibp");
 
-function getBreachDetail(req, res) {
+async function getBreachDetail(req, res) {
 
   const allBreaches = req.app.locals.breaches;
 
@@ -13,10 +16,44 @@ function getBreachDetail(req, res) {
     return res.redirect("/");
   }
 
+  const changePWLink = await getChangePWLink(featuredBreach);
+
   res.render("breach-detail", {
     title: req.fluentFormat("home-title"),
     featuredBreach,
+    changePWLink,
   });
+}
+
+async function getChangePWLink(breach) {
+  if (breach.DataClasses.includes("passwords")) {
+    const breachSchemeAndHost = "https://" + breach.Domain;
+    const wellKnownChangePasswordUrl = new URL("/.well-known/change-password", breachSchemeAndHost);
+    const wwwBreachSchemeAndHost = "https://www." + breach.Domain;
+    const wwwWellKnownChangePasswordUrl = new URL("/.well-known/change-password", wwwBreachSchemeAndHost);
+    try {
+      const resp = await got(wellKnownChangePasswordUrl, {
+        followRedirect: false,
+        timeout: 1000,
+      });
+      // Detect domains that first redirect to www. before the change-password URL
+      if (resp.statusCode === 301 && resp.headers.location === wwwWellKnownChangePasswordUrl.toString()) {
+        try {
+          const resp = await got(wwwWellKnownChangePasswordUrl, {
+            followRedirect: false,
+            timeout: 1000,
+          });
+          return resp.statusCode === 301 ? resp.headers.location : breachSchemeAndHost;
+        } catch (e) {
+          return breachSchemeAndHost;
+        }
+      }
+      return resp.statusCode === 301 ? resp.headers.location : breachSchemeAndHost;
+    } catch (e) {
+      return breachSchemeAndHost;
+    }
+  }
+  return "";
 }
 
 module.exports = {
