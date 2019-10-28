25947e6f63db4e3181181ba1462380bf35e26280
lesley <lnorton@mozilla.com>
2019-10-28 14:51:43 -0500

    Add temp route and "sendEmailToPreFxaSubscribers" controller fn.

--- routes/hibp.js
+++ routes/hibp.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 const bearerToken = require("express-bearer-token");
 
 const {asyncMiddleware} = require("../middleware");
-const {notify, breaches} = require("../controllers/hibp");
+const {notify, breaches, sendEmailToPreFxaSubscribers} = require("../controllers/hibp");
 
 
 const router = express.Router();
@@ -15,5 +15,6 @@ const jsonParser = bodyParser.json();
 router.use("/notify", bearerToken());
 router.post("/notify", jsonParser, asyncMiddleware(notify));
 router.get("/breaches", jsonParser, asyncMiddleware(breaches));
+router.get("/sendPreFxaEmail", jsonParser, asyncMiddleware(sendEmailToPreFxaSubscribers));
 
 module.exports = router;
