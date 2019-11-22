12908b1ad6ee01adbb3c11551dc41ba25ad292f2
lesley <lnorton@mozilla.com>
2019-11-22 16:01:16 -0600

    Send PDL

--- template-helpers/breach-detail.js
+++ template-helpers/breach-detail.js
@@ -15,7 +15,7 @@ function getVars(args) {
 // How to dynamically detect Data Aggregator breaches?
 
 function getBreachCategory(breach) {
-  if (["Exactis", "Apollo", "YouveBeenScraped", "ElasticsearchSalesLeads", "Estonia", "MasterDeeds"].includes(breach.Name)) {
+  if (["Exactis", "Apollo", "YouveBeenScraped", "ElasticsearchSalesLeads", "Estonia", "MasterDeeds", "PDL"].includes(breach.Name)) {
     return "data-aggregator-breach";
   }
   if (!breach.IsVerified) {
