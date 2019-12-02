7e4ab25eaf9a15ca7cb64b533640fc9345f6bd6e
Luke Crouch <lcrouch@mozilla.com>
2019-12-02 16:13:44 -0600

    fix #1411: add breaches_resolved JSON to subscribers table

---
+++ db/migrations/20191202161125_add_breaches_resolved_column.js
@@ -0,0 +1,13 @@
+"use strict";
+
+exports.up = function(knex) {
+  return knex.schema.table("subscribers", table => {
+    table.jsonb("breaches_resolved");
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.table("subscribers", table => {
+    table.dropColumn("breaches_resolved");
+  });
+};
