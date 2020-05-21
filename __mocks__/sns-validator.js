020bd5d29c88827a474ee6fafa287ea6d17def9e
Maxx Crawford <maxx.crawford@gmail.com>
2020-05-21 15:03:08 -0500

    Fixed all spacing lint errors

--- __mocks__/sns-validator.js
+++ __mocks__/sns-validator.js
@@ -3,11 +3,11 @@
 // See https://jestjs.io/docs/en/manual-mocks
 function MessageValidator() {}
 MessageValidator.prototype.validate = function (hash, cb) {
-  if (hash.Signature === "invalid") {
-    cb(new Error("The message signature is invalid."));
-    return;
-  }
-  cb(null, hash);
+    if (hash.Signature === "invalid") {
+        cb(new Error("The message signature is invalid."));
+        return;
+    }
+    cb(null, hash);
 };
 
 module.exports = MessageValidator;
