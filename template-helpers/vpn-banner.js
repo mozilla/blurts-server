05806f93376c2bba56254ba7102b3cb0f80b9c6d
Amri Toufali <atoufali@mozilla.com>
2021-08-31 13:23:57 -0700

    add optional chaining to Maxmind data results - also refactors output to show short location in header and full location in main content

--- template-helpers/vpn-banner.js
+++ template-helpers/vpn-banner.js
@@ -1,24 +1,29 @@
+// IP location info includes GeoLite2 data created by MaxMind, available from https://www.maxmind.com.
+// Test different IPs by comparing to the corresponding json, for example: https://github.com/maxmind/MaxMind-DB/blob/main/source-data/GeoLite2-City-Test.json
+
 "use strict";
 
+const AppConstants = require("../app-constants");
 const fs = require("fs");
 const Reader = require("@maxmind/geoip2-node").Reader;
+const maxmindDb = AppConstants.MAXMIND_DB_PATH || "./tests/mmdb/GeoLite2-City-Test.mmdb";
 
 function getIpInfo(args) {
-    const dbBuffer = fs.readFileSync("./tests/mmdb/GeoLite2-City-Test.mmdb");
+    const dbBuffer = fs.readFileSync(maxmindDb);
     const reader = Reader.openBuffer(dbBuffer);
-    const clientIp = args.data.root.constants.NODE_ENV === "dev" ? "2.125.160.216" : args.data.root.req.ip; // TODO: normalize IP for different ip4/ip6 formats
+    const clientIp = args.data.root.constants.NODE_ENV === "dev" ? "216.160.83.56" : args.data.root.req.ip; // TODO: normalize IP for different ip4/ip6 formats
     const info = { ip: clientIp };
-    let geoData, city, stateOrCountry;
+    let geoData, locationArr;
 
     try {
         geoData = reader.city(clientIp);
-        city = geoData.city.names.en || ""; // TODO: add optional chaining after Node version upgrade
-        stateOrCountry = geoData.subdivisions[0].isoCode || geoData.country.isoCode || ""; // TODO: add optional chaining after Node version upgrade
+        locationArr = [geoData.city?.names.en, geoData.subdivisions?.[0].isoCode, geoData.country?.isoCode].filter(str => str);
     } catch (e) {
         console.warn(e);
     }
 
-    info.location = `${city}${stateOrCountry ? `, ${stateOrCountry}` : ""}`;
+    info.shortLocation = locationArr.slice(0, 2).join(", "); // shows the first two location values from the ones available
+    info.fullLocation = locationArr.join(", "); // shows up to three location values from the ones available
 
     return info;
 }
