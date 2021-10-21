d734c769caf5bad6de49179b09ae0ddde73564f3
Mike Heavers <mheavers@gmail.com>
2021-10-21 16:26:21 -0700

    updated FAQ content

--- template-helpers/footer.js
+++ template-helpers/footer.js
@@ -12,9 +12,14 @@ function getFooterLinks(args) {
       href: "/about",
     },
     {
-      title: "Frequently Asked Questions",
-      stringId: "frequently-asked-questions",
+      title: "Monitor FAQ",
+      stringId: "remove-footer-monitor-faq",
       href: "https://support.mozilla.org/kb/firefox-monitor-faq",
+    },
+    {
+      title: "Data Removal FAQ",
+      stringId: "remove-footer-data-removal-faq",
+      href: "/remove-faq",
     },
     {
       title: "Terms & Privacy",
@@ -35,32 +40,41 @@ function getAboutPageStrings(args) {
   const locales = args.data.root.req.supportedLocales;
   const aboutPageStrings = [
     {
-      headline:"how-fxm-1-headline",
+      headline: "how-fxm-1-headline",
       subhead: "how-fxm-1-blurb",
       localizedCta: LocaleUtils.fluentFormat(locales, "scan-submit"),
       href: "/",
       eventCategory: "About Page: Search Your Email",
     },
     {
-      headline:"how-fxm-2-headline",
+      headline: "how-fxm-2-headline",
       subhead: "how-fxm-2-blurb",
       ctaId: "signUp",
       localizedCta: LocaleUtils.fluentFormat(locales, "sign-up-for-alerts"),
     },
     {
-      headline:"how-fxm-3-headline",
+      headline: "how-fxm-3-headline",
       subhead: "how-fxm-3-blurb",
-      localizedCta: LocaleUtils.fluentFormat(locales, "download-firefox-banner-button"),
+      localizedCta: LocaleUtils.fluentFormat(
+        locales,
+        "download-firefox-banner-button"
+      ),
       href: "https://www.mozilla.org/firefox",
       eventCategory: "About Page: Download Firefox",
       download: "download",
     },
   ];
 
-  aboutPageStrings.forEach(aboutBlock => {
-    aboutBlock.headline = LocaleUtils.fluentFormat(locales, aboutBlock.headline);
+  aboutPageStrings.forEach((aboutBlock) => {
+    aboutBlock.headline = LocaleUtils.fluentFormat(
+      locales,
+      aboutBlock.headline
+    );
     aboutBlock.subhead = LocaleUtils.fluentFormat(locales, aboutBlock.subhead);
-    aboutBlock.localizedCta = LocaleUtils.fluentFormat(locales, aboutBlock.localizedCta);
+    aboutBlock.localizedCta = LocaleUtils.fluentFormat(
+      locales,
+      aboutBlock.localizedCta
+    );
   });
   return aboutPageStrings;
 }
