7dfd6d00930dca7c83c34148446c59d4828e206b
Amri Toufali <atoufali@mozilla.com>
2022-01-11 20:37:39 -0800

    move education video urls to .env vars

--- template-helpers/product-education-video.js
+++ template-helpers/product-education-video.js
@@ -1,23 +1,14 @@
 "use strict";
 
+const AppConstants = require("../app-constants");
 const { getString } = require("./hbs-helpers");
 
 function productEducation(type, args) {
   const productType = type.toLowerCase();
-  let videoSrc;
-
-  switch (productType) {
-    case "relay":
-      videoSrc = "https://monitor.cdn.mozilla.net/videos/FF_Relay_version_02.mp4";
-      break;
-    case "vpn":
-      videoSrc = "https://monitor.cdn.mozilla.net/videos/Mozilla_VPN.mp4";
-      break;
-  }
 
   return {
     headingTxt: getString(`${productType}-video-headline`, args),
-    videoSrc,
+    videoSrc: AppConstants[`EDUCATION_VIDEO_URL_${type.toUpperCase()}`],
   };
 }
 
