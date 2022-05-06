be1fe012fb1bc61763b0d0f0400f34aaaf66f90d
Amri Toufali <atoufali@mozilla.com>
2022-05-06 15:33:05 -0700

    fix test-home-page.js merge conflict

--- tests/integration/tests/test-home-page.js
+++ tests/integration/tests/test-home-page.js
@@ -23,7 +23,7 @@ describe("Firefox Monitor homepage", function () {
       browser.checkFullPageScreen("Home_Page", {
         hideElements: [$$(".breach-info-wrapper")],
       })
-    ).to.be.within(0, 33);
+    ).to.be.within(0, 34.99);
   });
 
   it("should load correct number of breaches from an email input", function () {
