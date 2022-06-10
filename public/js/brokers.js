b6b84e9fa87be48321584195622166f683fa69b9
Amri Toufali <atoufali@mozilla.com>
2022-06-10 11:21:06 -0700

    remove redundant "use strict"

--- public/js/brokers.js
+++ public/js/brokers.js
@@ -1,5 +1,3 @@
-"use strict";
-
 document.querySelectorAll(".toggle-scan-result-button").forEach(button => {
   button.addEventListener("click", (event) => {
     button.parentElement.parentElement.classList.toggle("is-expanded");
