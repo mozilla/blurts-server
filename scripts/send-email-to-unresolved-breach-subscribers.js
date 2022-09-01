031d234608221711fba216ebc1dd0422dbc4067b
Amri Toufali <atoufali@mozilla.com>
2022-09-01 15:22:50 -0700

    cleanup

--- scripts/send-email-to-unresolved-breach-subscribers.js
+++ scripts/send-email-to-unresolved-breach-subscribers.js
@@ -1,54 +1,18 @@
 const DB = require('../db/DB')
 const EmailUtils = require('../email-utils')
 const { LocaleUtils } = require('../locale-utils')
+const AppConstants = require('../app-constants')
 const { argv } = require('node:process')
 
-const testEmails = argv[2] // expects a comma-separated string with no spaces e.g. test1@test.com,test2@test.com,...
-
-let subscribers
-
-if (testEmails) {
-  subscribers = []
-
-  testEmails.split(',').forEach(email => {
-    subscribers.push({
-      primary_email: email,
-      signup_language: 'en',
-      primary_verification_token: 'dev-token-for-testing',
-      breach_stats: {
-        passwords: {
-          count: 1,
-          numResolved: 0
-        },
-        numBreaches: {
-          count: 2,
-          numResolved: 0,
-          numUnresolved: 2
-        },
-        monitoredEmails: {
-          count: 2
-        }
-      }
-    })
-  })
-}
-
-sendUnresolvedBreachEmails()
-
-async function sendUnresolvedBreachEmails () {
+async function sendUnresolvedBreachEmails (subscribers = null) {
   let count = 0
 
   LocaleUtils.init()
   await EmailUtils.init()
   await DB.createConnection()
 
-  console.log('Starting cron job for monthly unresolved breach emails')
+  if (!subscribers) subscribers = await DB.getSubscribersWithUnresolvedBreaches() // if test subscribers are not available, pull from db
 
-  if (subscribers) {
-    console.log('- Test email list detected')
-  } else {
-    subscribers = await DB.getSubscribersWithUnresolvedBreaches()
-  }
   console.log('- Recipient total:', subscribers.length)
 
   for (const subscriber of subscribers) {
@@ -76,4 +40,42 @@ async function sendUnresolvedBreachEmails () {
 
   await DB.destroyConnection()
   console.log(`- Successfully emailed ${count} of ${subscribers.length} subscribers`)
+}
+
+if (argv[2]) {
+  // For testing, pass a comma-separated arg with no spaces, for example:
+  // node scripts/send-email-to-unresolved-breach-subscribers.js test1@test.com,test2@test.com,...
+  const subscribers = []
+  const emails = argv[2].split(',')
+
+  console.log('Testing job for monthly unresolved breach emails...')
+  console.log('- sending test emails to', emails)
+
+  emails.forEach(email => {
+    subscribers.push({
+      primary_email: email,
+      signup_language: 'en',
+      primary_verification_token: 'dev-token-for-testing',
+      breach_stats: {
+        passwords: {
+          count: 1,
+          numResolved: 0
+        },
+        numBreaches: {
+          count: 2,
+          numResolved: 0,
+          numUnresolved: 2
+        },
+        monitoredEmails: {
+          count: 2
+        }
+      }
+    })
+  })
+  sendUnresolvedBreachEmails(subscribers)
+} else if (AppConstants.MONTHLY_CRON_ENABLED === 'true') {
+  console.log('Running job for monthly unresolved breach emails...')
+  sendUnresolvedBreachEmails()
+} else {
+  console.log('Job for monthly unresolved breach emails was not run. MONTHLY_CRON_ENABLED expects the string "true".')
 }
