5d8df083614a105ae0c74a95ffbbe2929997aa6f
Joey Zhou <jozhou@mozilla.com>
2022-10-10 10:19:10 -0700

    feat: add breach details page skeleton

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,9 @@
 import express from 'express'
 import { landingPage } from '../controllers/index.js'
-
+import { breachDetailsPage } from '../controllers/breach/breach-details.js'
 const router = express.Router()
 
 router.get('/', landingPage)
+router.get('/breach-details', breachDetailsPage)
 
 export default router
