23a77dd614732907de60d430bf32681dc322abbd
Joey Zhou <jozhou@mozilla.com>
2022-10-13 16:16:11 -0700

    feat: routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,13 @@
 import express from 'express'
 import { landingPage } from '../controllers/index.js'
 import { breachDetailsPage } from '../controllers/breach-details.js'
+import { requireSessionUser } from '../middleware/user.js'
+import AuthRoutes from './auth.js'
 const router = express.Router()
 
 router.get('/', landingPage)
-router.get('/breach-details', breachDetailsPage)
+router.get('/breach-details', requireSessionUser, breachDetailsPage)
+router.use('/auth', AuthRoutes)
+// router.use(notFound)
 
 export default router
