a4d5225d836c26bc711ffc0d36c9eaeac576a835
John Whitlock <jwhitlock@mozilla.com>
2022-10-18 15:03:59 -0500

    Simplify is-coveralls-configured.js
    .coveralls.yml is the original config format, and is defined in the Ruby
    on Rails coveralls documentation. However, we don't really intend for
    people to upload coverage from development, so let's drop the mention.
    
    The script exits with error code 0 by default, so only be explicit with
    a process.exit() in the failure case.

--- scripts/is-coveralls-configured.js
+++ scripts/is-coveralls-configured.js
@@ -7,15 +7,12 @@ Exit 1 if coveralls will fail
 
 For required environment variables, see:
 https://github.com/nickmerwin/node-coveralls
-
-Coveralls for Ruby also supports .coveralls.yml
 */
 
 const { existsSync } = require('node:fs')
 
 if (process.env.COVERALLS_REPO_TOKEN) {
   console.log('coveralls configured with environment variable COVERALLS_REPO_TOKEN.')
-  process.exit(0)
 } else if (existsSync('.coveralls.yml')) {
   console.log('coveralls configured with configuration file ".coveralls.yml".')
 } else {
