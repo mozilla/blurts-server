096274370e7ed0a250b8500e45f3b82fd8fea375
Joey Zhou <jozhou@mozilla.com>
2022-10-25 07:14:24 -0700

    review comment fix

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,15 +1,12 @@
 import { Router } from 'express'
-import pkg from 'body-parser'
 
 import { asyncMiddleware } from '../middleware/util.js'
 import { init, confirmed, logout } from '../controllers/auth.js'
-const { json } = pkg
 
 const router = Router()
-const jsonParser = json()
 
-router.get('/init', jsonParser, init)
-router.get('/confirmed', jsonParser, asyncMiddleware(confirmed))
+router.get('/init', init)
+router.get('/confirmed', asyncMiddleware(confirmed))
 router.get('/logout', logout)
 
 export default router
