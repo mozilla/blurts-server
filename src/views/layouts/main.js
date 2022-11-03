0e0a1741e56ff7e7829781700891678d3af63402
Amri Toufali <atoufali@mozilla.com>
2022-11-02 20:16:16 -0700

    add optimized web fonts

--- src/views/layouts/main.js
+++ src/views/layouts/main.js
@@ -37,9 +37,6 @@ export const mainLayout = data => `
     <meta charset='utf-8'>
     <meta name="viewport" content="width=320, initial-scale=1">
     <title>${getPattern('home-title')}</title>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet"> 
     <link href='css/index.css' type='text/css' rel='stylesheet'>
     <script src='js/index.js' type='module'></script>
   </head>
