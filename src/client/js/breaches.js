6772f97cccbe4a6098198e7d7c7c8d54cc6e0c03
Joey Zhou <jozhou@mozilla.com>
2022-11-08 09:42:49 -0800

    add test breaches button to breaches detail

---
+++ src/client/js/breaches.js
@@ -0,0 +1,24 @@
+console.log('breaches update test')
+// update button
+const b = document.getElementById('update-breaches')
+b.addEventListener('click', async _ => {
+  console.log('haha')
+  try {
+    const resp = await fetch('http://localhost:6060/api/v1/user/breaches', {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        affectedEmail: 'josephyzhou@gmail.com',
+        recencyIndex: '13',
+        resolutionsChecked: ['passwords',
+          'dates-of-birth', 'email-addresses']
+      })
+    })
+
+    console.log('completed: ', resp)
+  } catch (err) {
+    console.error(`Error: ${err}`)
+  }
+})
