78fcba944232f25232753c6b42ce0a26404ad15b
Amri Toufali <atoufali@mozilla.com>
2022-11-12 07:01:47 -0800

    fix temp breaches update-test error

--- src/client/js/breaches.js
+++ src/client/js/breaches.js
@@ -1,24 +1,26 @@
 // TODO: REMOVE -- this is just an example of updating breach resolution
-console.log('breaches update test')
 // update button
 const b = document.getElementById('update-breaches')
-b.addEventListener('click', async _ => {
-  try {
-    const resp = await fetch('http://localhost:6060/api/v1/user/breaches', {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        affectedEmail: 'josephyzhou@gmail.com',
-        recencyIndex: '13',
-        resolutionsChecked: ['passwords',
-          'dates-of-birth', 'email-addresses']
+if (b) {
+  console.log('breaches update test')
+  b.addEventListener('click', async _ => {
+    try {
+      const resp = await fetch('http://localhost:6060/api/v1/user/breaches', {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          affectedEmail: 'josephyzhou@gmail.com',
+          recencyIndex: '13',
+          resolutionsChecked: ['passwords',
+            'dates-of-birth', 'email-addresses']
+        })
       })
-    })
 
-    console.log('completed: ', resp.json())
-  } catch (err) {
-    console.error(`Error: ${err}`)
-  }
-})
+      console.log('completed: ', resp.json())
+    } catch (err) {
+      console.error(`Error: ${err}`)
+    }
+  })
+}
