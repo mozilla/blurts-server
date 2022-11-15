1af30efbc12b1e0a3c75f2f270945d5825d35521
Joey Zhou <jozhou@mozilla.com>
2022-11-15 14:53:38 -0800

    more error types

--- src/utils/error.js
+++ src/utils/error.js
@@ -1,9 +1,3 @@
-class UnauthorizedError extends Error {
-  constructor (...params) {
-    super(...params)
-    this.statusCode = '403'
-  }
-}
 
 class UserInputError extends Error {
   constructor (...params) {
@@ -12,7 +6,38 @@ class UserInputError extends Error {
   }
 }
 
+class UnauthorizedError extends Error {
+  constructor (...params) {
+    super(...params)
+    this.statusCode = '403'
+  }
+}
+
+class NotFoundError extends Error {
+  constructor (...params) {
+    super(...params)
+    this.statusCode = '404'
+  }
+}
+class MethodNotAllowedError extends Error {
+  constructor (...params) {
+    super(...params)
+    this.statusCode = '405'
+  }
+}
+
+class RateLimitError extends Error {
+  constructor (...params) {
+    super(...params)
+    this.statusCode = '429'
+  }
+}
+
 export {
   UnauthorizedError,
-  UserInputError
+  UserInputError,
+  NotFoundError,
+  MethodNotAllowedError,
+  RateLimitError
+
 }
