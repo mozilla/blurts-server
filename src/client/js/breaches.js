e4a889a073a1433251f8aa03f57f05d2bdac3fea
Amri Toufali <atoufali@mozilla.com>
2022-11-21 15:43:22 -0800

    fix incompatible app-constants module in client

--- src/client/js/breaches.js
+++ src/client/js/breaches.js
@@ -1,12 +1,11 @@
 // TODO: REMOVE -- this is just an example of updating breach resolution
 // update button
-import AppConstants from '../../app-constants.js'
 const b = document.getElementById('update-breaches')
 if (b) {
   console.log('breaches update test')
   b.addEventListener('click', async _ => {
     try {
-      const resp = await fetch(`${AppConstants.SERVER_URL}/api/v1/user/breaches`, {
+      const resp = await fetch('api/v1/user/breaches', {
         method: 'PUT',
         headers: {
           'Content-Type': 'application/json'
