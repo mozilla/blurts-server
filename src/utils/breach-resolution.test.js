95f5e8b943c3ab6a82993c38b8ff65cf5424069f
Joey Zhou <jozhou@mozilla.com>
2022-11-29 23:12:36 -0800

    add more breach resolution tests, fix general case

--- src/utils/breach-resolution.test.js
+++ src/utils/breach-resolution.test.js
@@ -1,9 +1,55 @@
-import { filterBreachDataTypes } from './breach-resolution.js'
+import { initFluentBundles, updateAppLocale } from './fluent.js'
+import { filterBreachDataTypes, appendBreachResolutionChecklist, BreachDataTypes } from './breach-resolution.js'
 
 describe('utils:breach-resolution', () => {
-  it('filterBreachDataTypes', () => {
-    const resp = filterBreachDataTypes(['passwords'])
+  beforeAll(async () => {
+    await initFluentBundles()
+    updateAppLocale('en')
+  })
+
+  test('filterBreachDataTypes', () => {
+    const resp = filterBreachDataTypes(['passwords', 'irrelevant'])
     expect(resp.length).toBe(1)
     expect(resp).toEqual(['passwords'])
+  })
+
+  test('appendBreachResolutionChecklist: two data classes', () => {
+    const userBreachData = {
+      verifiedEmails: [{
+        email: 'affected@email.com',
+        breaches: [{
+          DataClasses: [BreachDataTypes.Passwords, BreachDataTypes.Email],
+          Name: 'companyName',
+          Domain: 'companyName.com'
+        }]
+      }],
+      unverifiedEmails: []
+    }
+    appendBreachResolutionChecklist(userBreachData)
+    console.log(JSON.stringify(userBreachData))
+    expect(userBreachData.verifiedEmails[0].breaches[0].breachChecklist).toBeDefined()
+    expect(userBreachData.verifiedEmails[0].breaches[0].breachChecklist[BreachDataTypes.Passwords].header)
+      .toEqual('Go to <a>https://companyName.com</a> to change your password and enable two-factor authentication (2FA).')
+    expect(userBreachData.verifiedEmails[0].breaches[0].breachChecklist[BreachDataTypes.Passwords].priority).toEqual(1)
+  })
+
+  test('appendBreachResolutionChecklist: no data classes', () => {
+    const userBreachData = {
+      verifiedEmails: [{
+        email: 'affected@email.com',
+        breaches: [{
+          DataClasses: [],
+          Name: 'companyName',
+          Domain: 'companyName.com'
+        }]
+      }],
+      unverifiedEmails: []
+    }
+    appendBreachResolutionChecklist(userBreachData)
+    console.log(JSON.stringify(userBreachData))
+    expect(userBreachData.verifiedEmails[0].breaches[0].breachChecklist).toBeDefined()
+    expect(userBreachData.verifiedEmails[0].breaches[0].breachChecklist[BreachDataTypes.General].header)
+      .toEqual('Reach out to companyName to inform them about this breach and ask for specific steps you can take.')
+    expect(userBreachData.verifiedEmails[0].breaches[0].breachChecklist[BreachDataTypes.General].priority).toEqual(13)
   })
 })
