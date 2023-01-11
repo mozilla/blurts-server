6a67bd6b6101d1c5dad1334480bc1df66fbead0c
Robert Helmer <rhelmer@mozilla.com>
2023-01-10 17:35:50 -0800

    use Ava as test runner for V2 using GH actions, with c8 for code coverage

--- src/utils/breach-resolution.test.js
+++ src/utils/breach-resolution.test.js
@@ -1,55 +1,55 @@
+import test from 'ava'
+
 import { initFluentBundles, updateLocale } from './fluent.js'
 import { filterBreachDataTypes, appendBreachResolutionChecklist, BreachDataTypes } from './breach-resolution.js'
 
-describe('utils:breach-resolution', () => {
-  beforeAll(async () => {
-    await initFluentBundles()
-    updateLocale('en')
-  })
+test.before(async () => {
+  await initFluentBundles()
+  updateLocale('en')
+})
 
-  test('filterBreachDataTypes', () => {
-    const resp = filterBreachDataTypes(['passwords', 'irrelevant'])
-    expect(resp.length).toBe(1)
-    expect(resp).toEqual(['passwords'])
-  })
+test('filterBreachDataTypes', t => {
+  const resp = filterBreachDataTypes(['passwords', 'irrelevant'])
+  t.is(resp.length, 1)
+  t.deepEqual(resp, ['passwords'])
+})
 
-  test('appendBreachResolutionChecklist: two data classes', () => {
-    const userBreachData = {
-      verifiedEmails: [{
-        email: 'affected@email.com',
-        breaches: [{
-          DataClasses: [BreachDataTypes.Passwords, BreachDataTypes.Email],
-          Name: 'companyName',
-          Domain: 'companyName.com'
-        }]
-      }],
-      unverifiedEmails: []
-    }
-    appendBreachResolutionChecklist(userBreachData)
-    console.log(JSON.stringify(userBreachData))
-    expect(userBreachData.verifiedEmails[0].breaches[0].breachChecklist).toBeDefined()
-    expect(userBreachData.verifiedEmails[0].breaches[0].breachChecklist[BreachDataTypes.Passwords].header)
-      .toEqual('Go to <a>https://companyName.com</a> to change your password and enable two-factor authentication (2FA).')
-    expect(userBreachData.verifiedEmails[0].breaches[0].breachChecklist[BreachDataTypes.Passwords].priority).toEqual(1)
-  })
+test('appendBreachResolutionChecklist: two data classes', t => {
+  const userBreachData = {
+    verifiedEmails: [{
+      email: 'affected@email.com',
+      breaches: [{
+        DataClasses: [BreachDataTypes.Passwords, BreachDataTypes.Email],
+        Name: 'companyName',
+        Domain: 'companyName.com'
+      }]
+    }],
+    unverifiedEmails: []
+  }
+  appendBreachResolutionChecklist(userBreachData)
+  console.log(JSON.stringify(userBreachData))
+  t.truthy(userBreachData.verifiedEmails[0].breaches[0].breachChecklist)
+  t.is(userBreachData.verifiedEmails[0].breaches[0].breachChecklist[BreachDataTypes.Passwords].header,
+    'Go to <a>https://companyName.com</a> to change your password and enable two-factor authentication (2FA).')
+  t.is(userBreachData.verifiedEmails[0].breaches[0].breachChecklist[BreachDataTypes.Passwords].priority, 1)
+})
 
-  test('appendBreachResolutionChecklist: no data classes', () => {
-    const userBreachData = {
-      verifiedEmails: [{
-        email: 'affected@email.com',
-        breaches: [{
-          DataClasses: [],
-          Name: 'companyName',
-          Domain: 'companyName.com'
-        }]
-      }],
-      unverifiedEmails: []
-    }
-    appendBreachResolutionChecklist(userBreachData)
-    console.log(JSON.stringify(userBreachData))
-    expect(userBreachData.verifiedEmails[0].breaches[0].breachChecklist).toBeDefined()
-    expect(userBreachData.verifiedEmails[0].breaches[0].breachChecklist[BreachDataTypes.General].header)
-      .toEqual('Reach out to companyName to inform them about this breach and ask for specific steps you can take.')
-    expect(userBreachData.verifiedEmails[0].breaches[0].breachChecklist[BreachDataTypes.General].priority).toEqual(13)
-  })
+test('appendBreachResolutionChecklist: no data classes', t => {
+  const userBreachData = {
+    verifiedEmails: [{
+      email: 'affected@email.com',
+      breaches: [{
+        DataClasses: [],
+        Name: 'companyName',
+        Domain: 'companyName.com'
+      }]
+    }],
+    unverifiedEmails: []
+  }
+  appendBreachResolutionChecklist(userBreachData)
+  console.log(JSON.stringify(userBreachData))
+  t.truthy(userBreachData.verifiedEmails[0].breaches[0].breachChecklist)
+  t.is(userBreachData.verifiedEmails[0].breaches[0].breachChecklist[BreachDataTypes.General].header,
+    'Reach out to companyName to inform them about this breach and ask for specific steps you can take.')
+  t.is(userBreachData.verifiedEmails[0].breaches[0].breachChecklist[BreachDataTypes.General].priority, 13)
 })
--- src/utils/breach-summary.test.js
+++ src/utils/breach-summary.test.js
@@ -1,193 +1,193 @@
+import test from 'ava'
+
 import { breachedDataTypes, breachSummary } from './breach-summary.js'
 
-describe('utils:breach-summary', () => {
-  test('breachSummary: handles null input', () => {
-    const verifiedEmails = null
-    const resp = breachSummary(verifiedEmails)
-    expect(resp.monitoredEmails.count).toEqual(0)
-  })
+test('breachSummary: handles null input', t => {
+  const verifiedEmails = null
+  const resp = breachSummary(verifiedEmails)
+  t.is(resp.monitoredEmails.count, 0)
+})
 
-  test('breachSummary: handles empty input', () => {
-    const verifiedEmails = []
-    const resp = breachSummary(verifiedEmails)
-    expect(resp.monitoredEmails.count).toEqual(0)
-  })
+test('breachSummary: handles empty input', t => {
+  const verifiedEmails = []
+  const resp = breachSummary(verifiedEmails)
+  t.is(resp.monitoredEmails.count, 0)
+})
 
-  test('breachSummary: unresolved breaches', () => {
-    const verifiedEmails = [{
-      email: 'test@test.com',
-      breaches: [],
-      primary: true
+test('breachSummary: unresolved breaches', t => {
+  const verifiedEmails = [{
+    email: 'test@test.com',
+    breaches: [],
+    primary: true
+  }, {
+    email: 'test2@test.com',
+    breaches: [{
+      recencyIndex: 17,
+      Name: 'Gravatar',
+      Title: 'Gravatar',
+      Domain: 'gravatar.com',
+      BreachDate: '2020-10-03T07:00:00.000Z',
+      AddedDate: '2021-12-05T22:45:58.000Z',
+      ModifiedDate: '2021-12-08T01:47:02.000Z',
+      PwnCount: 113990759,
+      Description: "In October 2020, <a href=\"https://www.bleepingcomputer.com/news/security/online-avatar-service-gravatar-allows-mass-collection-of-user-info/\" target=\"_blank\" rel=\"noopener\">a security researcher published a technique for scraping large volumes of data from Gravatar, the service for providing globally unique avatars </a>. 167 million names, usernames and MD5 hashes of email addresses used to reference users' avatars were subsequently scraped and distributed within the hacking community. 114 million of the MD5 hashes were cracked and distributed alongside the source hash, thus disclosing the original email address and accompanying data. Following the impacted email addresses being searchable in HIBP, <a href=\"https://en.gravatar.com/support/data-privacy\" target=\"_blank\" rel=\"noopener\">Gravatar release an FAQ detailing the incident</a>.",
+      LogoPath: 'Gravatar.png',
+      DataClasses: [
+        'email-addresses'
+      ],
+      IsVerified: true,
+      IsFabricated: false,
+      IsSensitive: false,
+      IsRetired: false,
+      IsSpamList: false,
+      IsMalware: false,
+      IsResolved: false,
+      ResolutionsChecked: []
+    }],
+    primary: false
+  }]
+  const resp = breachSummary(verifiedEmails)
+  t.is(resp.monitoredEmails.count, 2)
+  t.is(resp.numBreaches.count, 1)
+  t.is(resp.numBreaches.numResolved, 0)
+  t.is(resp.numBreaches.numUnresolved, 1)
+})
+test('breachSummary: resolved breaches', t => {
+  const verifiedEmails = [{
+    email: 'test@test.com',
+    breaches: [{
+      recencyIndex: 1,
+      Name: 'Test',
+      DataClasses: [
+        'email-addresses',
+        'passwords',
+        'ip-addresses',
+        'something-else'
+      ],
+      IsResolved: true,
+      ResolutionsChecked: ['email-addresses',
+        'passwords',
+        'ip-addresses',
+        'something-else']
+    }],
+    primary: true
+  }, {
+    email: 'test2@test.com',
+    breaches: [{
+      recencyIndex: 17,
+      Name: 'Gravatar',
+      Title: 'Gravatar',
+      Domain: 'gravatar.com',
+      BreachDate: '2020-10-03T07:00:00.000Z',
+      AddedDate: '2021-12-05T22:45:58.000Z',
+      ModifiedDate: '2021-12-08T01:47:02.000Z',
+      PwnCount: 113990759,
+      Description: "In October 2020, <a href=\"https://www.bleepingcomputer.com/news/security/online-avatar-service-gravatar-allows-mass-collection-of-user-info/\" target=\"_blank\" rel=\"noopener\">a security researcher published a technique for scraping large volumes of data from Gravatar, the service for providing globally unique avatars </a>. 167 million names, usernames and MD5 hashes of email addresses used to reference users' avatars were subsequently scraped and distributed within the hacking community. 114 million of the MD5 hashes were cracked and distributed alongside the source hash, thus disclosing the original email address and accompanying data. Following the impacted email addresses being searchable in HIBP, <a href=\"https://en.gravatar.com/support/data-privacy\" target=\"_blank\" rel=\"noopener\">Gravatar release an FAQ detailing the incident</a>.",
+      LogoPath: 'Gravatar.png',
+      DataClasses: [
+        'email-addresses'
+      ],
+      IsVerified: true,
+      IsFabricated: false,
+      IsSensitive: false,
+      IsRetired: false,
+      IsSpamList: false,
+      IsMalware: false,
+      IsResolved: true,
+      ResolutionsChecked: ['email-addresses']
     }, {
-      email: 'test2@test.com',
-      breaches: [{
-        recencyIndex: 17,
-        Name: 'Gravatar',
-        Title: 'Gravatar',
-        Domain: 'gravatar.com',
-        BreachDate: '2020-10-03T07:00:00.000Z',
-        AddedDate: '2021-12-05T22:45:58.000Z',
-        ModifiedDate: '2021-12-08T01:47:02.000Z',
-        PwnCount: 113990759,
-        Description: "In October 2020, <a href=\"https://www.bleepingcomputer.com/news/security/online-avatar-service-gravatar-allows-mass-collection-of-user-info/\" target=\"_blank\" rel=\"noopener\">a security researcher published a technique for scraping large volumes of data from Gravatar, the service for providing globally unique avatars </a>. 167 million names, usernames and MD5 hashes of email addresses used to reference users' avatars were subsequently scraped and distributed within the hacking community. 114 million of the MD5 hashes were cracked and distributed alongside the source hash, thus disclosing the original email address and accompanying data. Following the impacted email addresses being searchable in HIBP, <a href=\"https://en.gravatar.com/support/data-privacy\" target=\"_blank\" rel=\"noopener\">Gravatar release an FAQ detailing the incident</a>.",
-        LogoPath: 'Gravatar.png',
-        DataClasses: [
-          'email-addresses'
-        ],
-        IsVerified: true,
-        IsFabricated: false,
-        IsSensitive: false,
-        IsRetired: false,
-        IsSpamList: false,
-        IsMalware: false,
-        IsResolved: false,
-        ResolutionsChecked: []
-      }],
-      primary: false
-    }]
-    const resp = breachSummary(verifiedEmails)
-    expect(resp.monitoredEmails.count).toEqual(2)
-    expect(resp.numBreaches.count).toEqual(1)
-    expect(resp.numBreaches.numResolved).toEqual(0)
-    expect(resp.numBreaches.numUnresolved).toEqual(1)
-  })
-  test('breachSummary: resolved breaches', () => {
-    const verifiedEmails = [{
-      email: 'test@test.com',
-      breaches: [{
-        recencyIndex: 1,
-        Name: 'Test',
-        DataClasses: [
-          'email-addresses',
-          'passwords',
-          'ip-addresses',
-          'something-else'
-        ],
-        IsResolved: true,
-        ResolutionsChecked: ['email-addresses',
-          'passwords',
-          'ip-addresses',
-          'something-else']
-      }],
-      primary: true
+      recencyIndex: 2,
+      Name: 'Test2',
+      DataClasses: [
+        'email-addresses',
+        'passwords'
+      ],
+      IsResolved: false,
+      ResolutionsChecked: []
     }, {
-      email: 'test2@test.com',
-      breaches: [{
-        recencyIndex: 17,
-        Name: 'Gravatar',
-        Title: 'Gravatar',
-        Domain: 'gravatar.com',
-        BreachDate: '2020-10-03T07:00:00.000Z',
-        AddedDate: '2021-12-05T22:45:58.000Z',
-        ModifiedDate: '2021-12-08T01:47:02.000Z',
-        PwnCount: 113990759,
-        Description: "In October 2020, <a href=\"https://www.bleepingcomputer.com/news/security/online-avatar-service-gravatar-allows-mass-collection-of-user-info/\" target=\"_blank\" rel=\"noopener\">a security researcher published a technique for scraping large volumes of data from Gravatar, the service for providing globally unique avatars </a>. 167 million names, usernames and MD5 hashes of email addresses used to reference users' avatars were subsequently scraped and distributed within the hacking community. 114 million of the MD5 hashes were cracked and distributed alongside the source hash, thus disclosing the original email address and accompanying data. Following the impacted email addresses being searchable in HIBP, <a href=\"https://en.gravatar.com/support/data-privacy\" target=\"_blank\" rel=\"noopener\">Gravatar release an FAQ detailing the incident</a>.",
-        LogoPath: 'Gravatar.png',
-        DataClasses: [
-          'email-addresses'
-        ],
-        IsVerified: true,
-        IsFabricated: false,
-        IsSensitive: false,
-        IsRetired: false,
-        IsSpamList: false,
-        IsMalware: false,
-        IsResolved: true,
-        ResolutionsChecked: ['email-addresses']
-      }, {
-        recencyIndex: 2,
-        Name: 'Test2',
-        DataClasses: [
-          'email-addresses',
-          'passwords'
-        ],
-        IsResolved: false,
-        ResolutionsChecked: []
-      }, {
-        recencyIndex: 3,
-        Name: 'Test3',
-        DataClasses: [
-          'something-else'
-        ],
-        IsResolved: false,
-        ResolutionsChecked: []
-      }],
-      primary: false
-    }]
-    const resp = breachSummary(verifiedEmails)
-    expect(resp.monitoredEmails.count).toEqual(2)
-    expect(resp.numBreaches.count).toEqual(4)
-    expect(resp.numBreaches.numResolved).toEqual(2)
-    expect(resp.numBreaches.numUnresolved).toEqual(2)
-  })
+      recencyIndex: 3,
+      Name: 'Test3',
+      DataClasses: [
+        'something-else'
+      ],
+      IsResolved: false,
+      ResolutionsChecked: []
+    }],
+    primary: false
+  }]
+  const resp = breachSummary(verifiedEmails)
+  t.is(resp.monitoredEmails.count, 2)
+  t.is(resp.numBreaches.count, 4)
+  t.is(resp.numBreaches.numResolved, 2)
+  t.is(resp.numBreaches.numUnresolved, 2)
+})
 
-  test('breachedDataTypes', () => {
-    const verifiedEmails = [{
-      email: 'test@test.com',
-      breaches: [{
-        recencyIndex: 1,
-        Name: 'Test',
-        DataClasses: [
-          'email-addresses',
-          'passwords',
-          'ip-addresses',
-          'something-else'
-        ],
-        IsResolved: true,
-        ResolutionsChecked: ['email-addresses',
-          'passwords',
-          'ip-addresses',
-          'something-else']
-      }],
-      primary: true
+test('breachedDataTypes', t => {
+  const verifiedEmails = [{
+    email: 'test@test.com',
+    breaches: [{
+      recencyIndex: 1,
+      Name: 'Test',
+      DataClasses: [
+        'email-addresses',
+        'passwords',
+        'ip-addresses',
+        'something-else'
+      ],
+      IsResolved: true,
+      ResolutionsChecked: ['email-addresses',
+        'passwords',
+        'ip-addresses',
+        'something-else']
+    }],
+    primary: true
+  }, {
+    email: 'test2@test.com',
+    breaches: [{
+      recencyIndex: 17,
+      Name: 'Gravatar',
+      Title: 'Gravatar',
+      Domain: 'gravatar.com',
+      BreachDate: '2020-10-03T07:00:00.000Z',
+      AddedDate: '2021-12-05T22:45:58.000Z',
+      ModifiedDate: '2021-12-08T01:47:02.000Z',
+      PwnCount: 113990759,
+      Description: "In October 2020, <a href=\"https://www.bleepingcomputer.com/news/security/online-avatar-service-gravatar-allows-mass-collection-of-user-info/\" target=\"_blank\" rel=\"noopener\">a security researcher published a technique for scraping large volumes of data from Gravatar, the service for providing globally unique avatars </a>. 167 million names, usernames and MD5 hashes of email addresses used to reference users' avatars were subsequently scraped and distributed within the hacking community. 114 million of the MD5 hashes were cracked and distributed alongside the source hash, thus disclosing the original email address and accompanying data. Following the impacted email addresses being searchable in HIBP, <a href=\"https://en.gravatar.com/support/data-privacy\" target=\"_blank\" rel=\"noopener\">Gravatar release an FAQ detailing the incident</a>.",
+      LogoPath: 'Gravatar.png',
+      DataClasses: [
+        'email-addresses'
+      ],
+      IsVerified: true,
+      IsFabricated: false,
+      IsSensitive: false,
+      IsRetired: false,
+      IsSpamList: false,
+      IsMalware: false,
+      IsResolved: true,
+      ResolutionsChecked: ['email-addresses']
     }, {
-      email: 'test2@test.com',
-      breaches: [{
-        recencyIndex: 17,
-        Name: 'Gravatar',
-        Title: 'Gravatar',
-        Domain: 'gravatar.com',
-        BreachDate: '2020-10-03T07:00:00.000Z',
-        AddedDate: '2021-12-05T22:45:58.000Z',
-        ModifiedDate: '2021-12-08T01:47:02.000Z',
-        PwnCount: 113990759,
-        Description: "In October 2020, <a href=\"https://www.bleepingcomputer.com/news/security/online-avatar-service-gravatar-allows-mass-collection-of-user-info/\" target=\"_blank\" rel=\"noopener\">a security researcher published a technique for scraping large volumes of data from Gravatar, the service for providing globally unique avatars </a>. 167 million names, usernames and MD5 hashes of email addresses used to reference users' avatars were subsequently scraped and distributed within the hacking community. 114 million of the MD5 hashes were cracked and distributed alongside the source hash, thus disclosing the original email address and accompanying data. Following the impacted email addresses being searchable in HIBP, <a href=\"https://en.gravatar.com/support/data-privacy\" target=\"_blank\" rel=\"noopener\">Gravatar release an FAQ detailing the incident</a>.",
-        LogoPath: 'Gravatar.png',
-        DataClasses: [
-          'email-addresses'
-        ],
-        IsVerified: true,
-        IsFabricated: false,
-        IsSensitive: false,
-        IsRetired: false,
-        IsSpamList: false,
-        IsMalware: false,
-        IsResolved: true,
-        ResolutionsChecked: ['email-addresses']
-      }, {
-        recencyIndex: 2,
-        Name: 'Test2',
-        DataClasses: [
-          'email-addresses',
-          'passwords'
-        ],
-        IsResolved: false,
-        ResolutionsChecked: []
-      }, {
-        recencyIndex: 3,
-        Name: 'Test3',
-        DataClasses: [
-          'something-else'
-        ],
-        IsResolved: false,
-        ResolutionsChecked: []
-      }],
-      primary: false
-    }]
-    const resp = breachedDataTypes(verifiedEmails, 'test2@test.com')
-    expect(resp['test@test.com']?.passwords).toEqual(1)
-    expect(resp['test@test.com']['email-addresses']).toEqual(1)
-    expect(resp['test2@test.com']?.passwords).toEqual(1)
-    expect(resp['test2@test.com']['email-addresses']).toEqual(2)
-  })
+      recencyIndex: 2,
+      Name: 'Test2',
+      DataClasses: [
+        'email-addresses',
+        'passwords'
+      ],
+      IsResolved: false,
+      ResolutionsChecked: []
+    }, {
+      recencyIndex: 3,
+      Name: 'Test3',
+      DataClasses: [
+        'something-else'
+      ],
+      IsResolved: false,
+      ResolutionsChecked: []
+    }],
+    primary: false
+  }]
+  const resp = breachedDataTypes(verifiedEmails, 'test2@test.com')
+  t.is(resp['test@test.com']?.passwords, 1)
+  t.is(resp['test@test.com']['email-addresses'], 1)
+  t.is(resp['test2@test.com']?.passwords, 1)
+  t.is(resp['test2@test.com']['email-addresses'], 2)
 })
