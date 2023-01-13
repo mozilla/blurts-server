3caaa1c19c52a949678a35ba5653bbc66a0e07f8
Robert Helmer <rhelmer@mozilla.com>
2023-01-12 17:32:55 -0800

    backend work for v2 settings MNTOR-1004

--- src/controllers/settings.js
+++ src/controllers/settings.js
@@ -2,16 +2,183 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+import {
+  getUserEmails,
+  resetUnverifiedEmailAddress,
+  addSubscriberUnverifiedEmailHash,
+  removeOneSecondaryEmail,
+  getEmailById,
+  verifyEmailHash
+} from '../db/tables/email_addresses.js'
+
+import { setAllEmailsToPrimary } from '../db/tables/subscribers.js'
+
+import { fluentError, getMessage } from '../utils/fluent.js'
+import EmailUtils from '../utils/email.js'
 import { mainLayout } from '../views/main.js'
 import { settings } from '../views/partials/settings.js'
+import AppConstants from '../app-constants.js'
+import { getBreachesForEmail } from '../utils/hibp.js'
+import { getTemplate } from '../views/email-2022.js'
+import { generateToken } from '../utils/csrf.js'
 
-function settingsPage (req, res) {
+async function settingsPage (req, res) {
+  const emails = await getUserEmails(req.session.user.id)
+  // Add primary subscriber email to the list
+  emails.push({
+    email: req.session.user.primary_email,
+    sha1: req.session.user.primary_sha1,
+    primary: true,
+    verified: true
+  })
+
+  const breachCounts = new Map()
+  emails.forEach((email) => breachCounts.set(email.email, 0))
+
+  const allBreaches = req.app.locals.breaches
+  for (const email of emails) {
+    const breaches = await getBreachesForEmail(
+      email.sha1,
+      allBreaches,
+      true,
+      false
+    )
+    breachCounts.set(email.email, breaches.length)
+  }
+
   const data = {
-    fxaProfile: req.user.fxa_profile_json,
-    partial: settings
+    partial: settings,
+    emails,
+    breachCounts,
+    limit: AppConstants.MAX_NUM_ADDRESSES,
+    csrfToken: generateToken(res)
   }
 
   res.send(mainLayout(data))
 }
 
-export { settingsPage }
+async function addEmail (req, res) {
+  const sessionUser = await req.user
+  const email = req.body.email
+  // FIXME what should we use in v2 for email address validation?
+  if (!email) {
+    throw fluentError('user-add-invalid-email')
+  }
+
+  if (sessionUser.email_addresses.length >= AppConstants.MAX_NUM_ADDRESSES) {
+    throw fluentError('user-add-too-many-emails')
+  }
+  _checkForDuplicateEmail(sessionUser, email)
+
+  const unverifiedSubscriber = await addSubscriberUnverifiedEmailHash(
+    req.session.user,
+    email
+  )
+
+  await _sendVerificationEmail(unverifiedSubscriber.id)
+
+  // TODO: what should we return to the client?
+  return res.json('Resent the email')
+}
+
+function _checkForDuplicateEmail (sessionUser, email) {
+  email = email.toLowerCase()
+  if (email === sessionUser.primary_email.toLowerCase()) {
+    throw fluentError('user-add-duplicate-email')
+  }
+  for (const secondaryEmail of sessionUser.email_addresses) {
+    if (email === secondaryEmail.email.toLowerCase()) {
+      throw fluentError('user-add-duplicate-email')
+    }
+  }
+}
+
+async function removeEmail (req, res) {
+  const emailId = req.body.emailId
+  const sessionUser = req.user
+  const existingEmail = await getEmailById(emailId)
+
+  if (existingEmail.subscriber_id !== sessionUser.id) {
+    throw fluentError('error-not-subscribed')
+  }
+
+  removeOneSecondaryEmail(emailId)
+  res.redirect('/user/settings')
+}
+
+async function resendEmail (req, res) {
+  const emailId = req.body.emailId
+  const sessionUser = req.user
+  const existingEmail = await getUserEmails(sessionUser.id)
+
+  const filteredEmail = existingEmail.filter(
+    (a) => a.email === emailId && a.subscriber_id === sessionUser.id
+  )
+
+  if (!filteredEmail) {
+    throw fluentError('user-verify-token-error')
+  }
+
+  await _sendVerificationEmail(emailId)
+
+  // TODO: what should we return to the client?
+  return res.json('Resent the email')
+}
+
+async function _sendVerificationEmail (emailId) {
+  const unverifiedEmailAddressRecord = await resetUnverifiedEmailAddress(
+    emailId
+  )
+  const recipientEmail = unverifiedEmailAddressRecord.email
+  await EmailUtils.sendEmail(
+    recipientEmail,
+    getMessage('email-subject-verify'),
+    getTemplate,
+    {
+      recipientEmail,
+      ctaHref: EmailUtils.getVerificationUrl(unverifiedEmailAddressRecord),
+      utmCampaign: 'email_verify',
+      unsubscribeUrl: EmailUtils.getUnsubscribeUrl(
+        unverifiedEmailAddressRecord,
+        'account-verification-email'
+      ),
+      whichPartial: 'email_partials/email_verify',
+      heading: getMessage('email-verify-heading'),
+      subheading: getMessage('email-verify-subhead')
+    }
+  )
+}
+
+async function verifyEmail (req, res) {
+  const token = req.query.token
+  await verifyEmailHash(token)
+
+  return res.redirect('/user/settings')
+}
+
+async function updateCommunicationOptions (req, res) {
+  try {
+    const sessionUser = req.user
+    // 0 = Send breach alerts to the email address found in brew breach.
+    // 1 = Send all breach alerts to user's primary email address.
+    const allEmailsToPrimary = Number(req.body.communicationOption) === 1
+    const updatedSubscriber = await setAllEmailsToPrimary(
+      sessionUser,
+      allEmailsToPrimary
+    )
+    req.session.user = updatedSubscriber
+
+    return res.json('Comm options updated')
+  } catch (ex) {
+    return res.json('Error updating comm options')
+  }
+}
+
+export {
+  settingsPage,
+  resendEmail,
+  addEmail,
+  removeEmail,
+  verifyEmail,
+  updateCommunicationOptions
+}
--- src/routes/api/v1/user.js
+++ src/routes/api/v1/user.js
@@ -2,16 +2,22 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import { Router } from 'express'
+import { Router, json } from 'express'
 
 import { asyncMiddleware } from '../../../middleware/util.js'
 import { requireSessionUser } from '../../../middleware/auth.js'
 import { methodNotAllowed } from '../../../middleware/error.js'
 import { putBreachResolution, getBreaches } from '../../../controllers/breaches.js'
+import { addEmail, resendEmail, removeEmail, verifyEmail, updateCommunicationOptions } from '../../../controllers/settings.js'
 
 const router = Router()
 // breaches
 router.put('/breaches', requireSessionUser, asyncMiddleware(putBreachResolution))
 router.get('/breaches', requireSessionUser, asyncMiddleware(getBreaches))
+router.post('/email', requireSessionUser, asyncMiddleware(addEmail))
+router.post('/resend-email', requireSessionUser, asyncMiddleware(resendEmail))
+router.post('/remove-email', requireSessionUser, asyncMiddleware(removeEmail))
+router.get('/verify-email', asyncMiddleware(verifyEmail))
+router.post('/update-comm-option', json(), requireSessionUser, asyncMiddleware(updateCommunicationOptions))
 router.use(methodNotAllowed)
 export default router
