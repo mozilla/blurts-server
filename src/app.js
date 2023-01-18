2eb0d0aa192acdc039fc9fc48cbe93a6d5868099
Andrea Marchesini <amarchesini@mozilla.com>
2023-01-18 13:07:02 -0400

    Add license headers in source files

--- src/app.js
+++ src/app.js
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 import express from 'express'
 import session from 'express-session'
 import connectRedis from 'connect-redis'
