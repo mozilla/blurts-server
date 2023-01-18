3117e110440e7af03eb36ab64d58ae1b6e37d440
Florian Zia <zia.florian@gmail.com>
2023-01-18 13:44:18 +0100

    chore: Add data for testing purposes

--- src/controllers/settings.js
+++ src/controllers/settings.js
@@ -3,7 +3,36 @@ import { settings } from '../views/partials/settings.js'
 
 function settingsPage (req, res) {
   const data = {
-    partial: settings
+    partial: settings,
+    // TODO: The entries below are for testing only and should be removed.
+    csrfToken: 'csrf123',
+    emails: [
+      {
+        email: 'test+primary@mozilla.com',
+        id: 'id123',
+        primary: true,
+        subscriber_id: 'sub123',
+        verified: true
+      },
+      {
+        email: 'test+verified@mozilla.com',
+        id: 'id123',
+        primary: false,
+        subscriber_id: 'sub123',
+        verified: true
+      },
+      {
+        email: 'test+uverified@mozilla.com',
+        id: 'id123',
+        primary: false,
+        subscriber_id: 'sub123',
+        verified: false
+      }
+    ],
+    limit: 5,
+    breachCounts: {
+      get: () => 123
+    }
   }
 
   res.send(mainLayout(data))
