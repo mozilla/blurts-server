1223d1d9716f4dea6950abe6904593ca4bd8723e
Florian Zia <zia.florian@gmail.com>
2023-01-23 17:54:10 +0100

    chore: Add data update test

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -1,1 +1,26 @@
 console.log('Hello World from app.js')
+
+const circleCharts = document.querySelectorAll('.chart-test')
+circleCharts.forEach(chart => {
+  chart.addEventListener('click', () => {
+    chart.setAttribute('data-chart', JSON.stringify({
+      title: 'Chart updated',
+      data: [
+        {
+          key: 'resolved',
+          name: 'Resolved',
+          count: 1,
+          color: '#9059ff',
+          showPercentage: true
+        },
+        {
+          key: 'unresolved',
+          name: 'Unresolved',
+          count: 5,
+          color: '#321c64'
+        }
+      ],
+      isDonut: true
+    }))
+  })
+})
