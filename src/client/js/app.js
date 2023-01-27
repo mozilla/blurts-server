5e388df481c71b58b792616cafc86c7f8029e386
Florian Zia <zia.florian@gmail.com>
2023-01-27 13:11:18 +0100

    chore: Clean up

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -3,24 +3,3 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 console.log('Hello World from app.js')
-
-const circleCharts = document.querySelectorAll('.chart-test')
-circleCharts.forEach(chart => {
-  chart.addEventListener('click', () => {
-    chart.setAttribute('title', 'Updated')
-    chart.setAttribute('data', JSON.stringify([
-      {
-        key: 'resolved',
-        name: 'Resolved',
-        count: 5,
-        color: '#9059ff'
-      },
-      {
-        key: 'unresolved',
-        name: 'Unresolved',
-        count: 0,
-        color: '#321c64'
-      }
-    ]))
-  })
-})
