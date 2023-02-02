3a4a6cef3f07b305e2a3fed37eee16f275e50f44
Joey Zhou <jozhou@mozilla.com>
2023-02-02 08:43:51 -0800

    fixing sub calls

--- src/utils/hibp.js
+++ src/utils/hibp.js
@@ -231,8 +231,8 @@ async function subscribeHash (sha1) {
   const sha1Prefix = sha1.slice(0, 6).toUpperCase()
   const path = '/range/subscribe'
   const options = {
-    method: 'POST',
-    json: { hashPrefix: sha1Prefix }
+    Method: 'POST',
+    Body: { hashPrefix: sha1Prefix }
   }
 
   return await kAnonReq(path, options)
@@ -250,9 +250,9 @@ async function subscribeHash (sha1) {
  */
 async function deleteSubscribedHash (sha1) {
   const sha1Prefix = sha1.slice(0, 6).toUpperCase()
-  const path = `/range${sha1Prefix}`
+  const path = `/range/${sha1Prefix}`
   const options = {
-    method: 'DELETE'
+    Method: 'DELETE'
   }
 
   return await kAnonReq(path, options)
