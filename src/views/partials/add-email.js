85cc6411354d20664b4231fe5854ff56d06ee019
Amri Toufali <atoufali@mozilla.com>
2023-02-03 12:07:21 -0800

    add Mozilla Public License

--- src/views/partials/add-email.js
+++ src/views/partials/add-email.js
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 export const addEmail = data => `
 <header>
   <h1>${data.something} Add another email address</h1>
