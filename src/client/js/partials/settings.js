470d29d1f401db4d668da5b04de315332b7efa0f
Amri Toufali <atoufali@mozilla.com>
2023-02-14 13:47:35 -0800

    delete obsolete code

--- src/client/js/partials/settings.js
+++ src/client/js/partials/settings.js
@@ -35,67 +35,6 @@ if (settingsAlertOptionsInputs?.length) {
   }
 }
 
-const settingsAddEmail = document.querySelector('.js-settings-add-email-opener')
-if (settingsAddEmail) {
-  settingsAddEmail.addEventListener('click', async _ => {
-    try {
-      const addEmailDialog = document.getElementById('js-settings-modal')
-      addEmailDialog.showModal()
-    } catch (err) {
-      console.error(`Error: ${err}`)
-    }
-  })
-}
-
-const settingsClose = document.getElementById('js-settings-close')
-if (settingsClose) {
-  settingsClose.addEventListener('click', async _ => {
-    try {
-      const addEmailDialog = document.getElementById('js-settings-modal')
-      addEmailDialog.close()
-    } catch (err) {
-      console.error(`Error: ${err}`)
-    }
-  })
-}
-
-const settingsAddVerification = document.getElementById('js-settings-modal-send-verification')
-if (settingsAddVerification) {
-  settingsAddVerification.addEventListener('click', async _ => {
-    const email = document.getElementById('js-settings-email-modal-input').value
-    const csrfToken = document
-      .querySelector('.js-settings[data-csrf-token]')
-      .getAttribute('data-csrf-token')
-
-    const response = await fetch('/api/v1/user/email', {
-      headers: {
-        'Content-Type': 'application/json',
-        'x-csrf-token': csrfToken
-      },
-      mode: 'same-origin',
-      method: 'POST',
-      body: JSON.stringify({ email })
-    })
-
-    if (!response?.ok) {
-      throw new Error(`Sending verification email failed: ${response?.error}`)
-    }
-
-    const addEmailDialogContent = document.getElementById('js-settings-modal-content')
-    // TODO: Localize string below
-    addEmailDialogContent.textContent = `Verify the link sent to ${email} to add it to Firefox Monitor. Manage all email addresses in Settings.`
-
-    const addEmailDialogControls = document.getElementById('js-settings-modal-controls')
-    addEmailDialogControls.hidden = true
-
-    const addEmailDialog = document.getElementById('js-settings-modal')
-    addEmailDialog.addEventListener('close', _ => {
-      addEmailDialogControls.hidden = false
-      return window.location.reload(true)
-    }, { once: true })
-  })
-}
-
 const settingsRemoveEmailButtons = document.getElementsByClassName('js-remove-email-button')
 if (settingsRemoveEmailButtons?.length) {
   for (const removeEmailButton of settingsRemoveEmailButtons) {
