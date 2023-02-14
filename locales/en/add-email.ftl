521dbac82e9d0f85357523ec3e93243e97ec50bc
Amri Toufali <atoufali@mozilla.com>
2023-02-13 21:28:46 -0800

    add locale strings

---
+++ locales/en/add-email.ftl
@@ -0,0 +1,21 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+### Dialog window that allows a user to add a new email address to be monitored
+
+add-email-add-another-heading = Add another email address
+
+# $total is the number of emails a user is allowed to add
+add-email-your-account-includes = 
+  {
+    $total ->
+      [one] Your account includes monitoring of { $total } email address. Add a new email address to see if it’s been involved in a breach.
+     *[other] Your account includes monitoring of up to { $total } email addresses. Add a new email address to see if it’s been involved in a breach.
+  }
+
+add-email-address-input-label = Email address
+add-email-send-verification-button = Send verification link
+
+# $email is the newly added email address. $settings-href is the URL for the Settings page. HTML tags should not be translated, e.g. `<a>`
+add-email-verify-the-link = Verify the link sent to { $email } to add it to { -brand-fx-monitor }. Manage all email addresses in <a { $settings-href }>Settings</a>.
--- src/views/partials/add-email.js
+++ src/views/partials/add-email.js
@@ -2,22 +2,24 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+import { getMessage } from '../../utils/fluent.js'
+
 export const addEmail = data => `
 <header>
   <button class='close'></button>
   <img src='/images/dialog-email-clouds.svg'>
-  <h2>Add another email address</h2>
+  <h2>${getMessage('add-email-add-another-heading')}</h2>
 </header>
 <form>
-  <p>Your account includes monitoring of up to ${data.emailLimit} email addresses. Add a new email address to see if it’s been involved in a breach.</p>
+  <p>${getMessage('add-email-your-account-includes', { total: data.emailLimit })}</p>
   <input type='hidden' name='csrf-token' value='${data.csrfToken}'>
   <label class='text-field'>
-    <span>Email address</span>
+    <span>${getMessage('add-email-address-input-label')}</span>
     <input type='email' name='email-address' required>
   </label>
-  <button class='primary' type='submit' name='email-submit'>Send verification link</button>
+  <button class='primary' type='submit' name='email-submit'>${getMessage('add-email-send-verification-button')}</button>
 </form>
 <template data-success>
-  <p class='add-email-success'>Verify the link sent to <b class='current-email'>bnye@hotmail.com</b> to add it to Firefox Monitor. Manage all email addresses in <a href='/user/settings'><b>Settings</b></a>.</p>
+  <p class='add-email-success'>${getMessage('add-email-verify-the-link', { email: '<b class="current-email"></b>', 'settings-href': 'href="/user/settings"' })}</p>
 </template>
 `
