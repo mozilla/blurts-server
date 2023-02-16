9a7e20f2f565ea650d9196734796c3c2903c391d
Florian Zia <zia.florian@gmail.com>
2023-02-16 18:53:54 +0100

    wip: Non-auth version of email preview

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,29 +3,36 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 import express from 'express'
-import { landingPage } from '../controllers/landing.js'
+
+import adminRoutes from './admin.js'
+import authRoutes from './auth.js'
+import dockerFlowRoutes from './dockerflow.js'
+import hibpApiRoutes from './api/v1/hibp.js'
+import previewRoutes from './preview.js'
+import userApiRoutes from './api/v1/user.js'
+import userRoutes from './user.js'
+
 import { dialog } from '../controllers/dialog.js'
+import { landingPage } from '../controllers/landing.js'
 import { notFoundPage } from '../controllers/notFound.js'
 import { notFound } from '../middleware/error.js'
 import { robotsTxt } from '../middleware/robots.js'
-import dockerFlowRoutes from './dockerflow.js'
-import authRoutes from './auth.js'
-import userRoutes from './user.js'
-import hibpApiRoutes from './api/v1/hibp.js'
-import userApiRoutes from './api/v1/user.js'
-import adminRoutes from './admin.js'
 
 const router = express.Router()
 
-router.use('/', dockerFlowRoutes)
 router.get('/', landingPage)
-router.get('*/dialog/:name', dialog)
 router.get('/robots.txt', robotsTxt)
-router.use('/oauth', authRoutes)
-router.use('/user', userRoutes)
+router.get('*/dialog/:name', dialog)
+
+
+router.use('/', dockerFlowRoutes)
 router.use('/admin', adminRoutes)
-router.use('/api/v1/user/', userApiRoutes)
 router.use('/api/v1/hibp/', hibpApiRoutes)
+router.use('/api/v1/user/', userApiRoutes)
+router.use('/oauth', authRoutes)
+router.use('/preview', previewRoutes)
+router.use('/user', userRoutes)
+
 router.use(notFound)
 router.use(notFoundPage)
 
