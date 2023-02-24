1b3b64b7b193a2543a90dfac992fe577107dabf4
Joey Zhou <jozhou@mozilla.com>
2023-02-24 12:55:52 -0800

    fix login

--- src/playwright.config.js
+++ src/playwright.config.js
@@ -106,6 +106,6 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   // webServer: {
   //   command: 'npm run start',
-  //   port: 6060,
-  // },
+  //   port: 6060
+  // }
 })
