fe35e4549cf368ebce7e3a0beb8a69100fce1e56
Amri Toufali <atoufali@mozilla.com>
2023-02-23 23:05:07 -0800

    update toast logic

--- src/client/js/components/toast-alert.js
+++ src/client/js/components/toast-alert.js
@@ -2,32 +2,63 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-const html = `
+const html = (ttl, fadeDuration) => `
 <style>
   :host{
-    contain: style paint;
+    contain: content;
     position: fixed;
-    top: 0;
-    margin: var(--padding-md) 0;
-    border-radius: var(--border-radius);
-    color: white;
-    background-color: var(--red-70);
+    top: var(--padding-md);
     left: 50%;
-    transform: translateX(-50%);
+    transform: translate(-50%, var(--toast-y, 0));
+    border-radius: var(--border-radius);
+    background-color: var(--red-70);
+    color: white;
     z-index: 2;
+    transition: transform 300ms;
+    animation: fly-in 500ms, fade-out ${fadeDuration}ms ${ttl}ms forwards;
   }
 
   :host([hidden]) {
     display: none 
   }
+
+  output{
+    display: inline-block;
+    padding: var(--padding-sm) var(--padding-lg);
+  }
+
+  @keyframes fly-in{
+    from{
+      opacity: 0;
+      transform: translate(-50%, -100%);
+    }
+    to{
+      opacity: 1;
+    }
+  }
+
+  @keyframes fade-out{
+    from{
+      opacity: 1;
+    }
+    to{
+      opacity: 0;
+    }
+  }
 </style>
+<output></output>
 `
 
 customElements.define('toast-alert', class extends HTMLElement {
   constructor () {
     super()
+
+    this.ttl = 6000
+    this.fadeDuration = 600
+
     this.attachShadow({ mode: 'open' })
-    this.shadowRoot.innerHTML = html
+    this.shadowRoot.innerHTML = html(this.ttl, this.fadeDuration)
+    this.output = this.shadowRoot.querySelector('output')
   }
 
   get message () {
@@ -35,23 +66,22 @@ customElements.define('toast-alert', class extends HTMLElement {
   }
 
   set message (txt) {
-    console.log('set message')
-    const output = document.createElement('output')
-    output.delay = setTimeout(this.killMessage, 6000, output, this)
-    output.textContent = txt
-    this.shadowRoot.append(output)
+    this.output.textContent = txt
   }
 
   connectedCallback () {
-    const toasts = document.querySelectorAll('toast-alert')
-    toasts.forEach((toast, i) => {
-      const y = 100 * (toasts.length - 1 - i)
-      toast.style.setProperty('transform', `translateY(${y}%)`)
+    const toasts = Array.from(document.querySelectorAll('toast-alert'))
+    const otherToasts = toasts.filter(toast => toast !== this).reverse()
+
+    otherToasts.forEach((toast, i) => {
+      toast.style.setProperty('--toast-y', `${110 * (i + 1)}%`)
     })
+
+    this.timeout = setTimeout(() => this.kill(), this.ttl + this.fadeDuration)
   }
 
-  killMessage (output, host) {
-    clearTimeout(output.delay)
-    host.remove()
+  kill () {
+    clearTimeout(this.timeout)
+    this.remove()
   }
 })
