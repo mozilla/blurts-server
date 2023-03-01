da4b1bc4076ec2831551c5d596068dc93844a8da
Joey Zhou <jozhou@mozilla.com>
2023-03-01 12:58:19 -0800

    global setup saves login

--- src/playwright.config.js
+++ src/playwright.config.js
@@ -105,7 +105,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run start',
+    command: 'npm start',
     port: 6060
   }
 })
