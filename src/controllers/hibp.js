b9c9b1e023f36638b89696dad15dfb042494b177
Robert Helmer <rhelmer@mozilla.com>
2023-03-02 05:58:21 -0800

    chore: ensure all required dependencies for v2 are in src npm workspace (#2869)
    * chore: ensure all required dependencies for v2 are in src npm workspace
    
    ---------
    
    Co-authored-by: Amri Toufali <atoufali@mozilla.com>

--- src/controllers/hibp.js
+++ src/controllers/hibp.js
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import { acceptedLanguages, negotiateLanguages } from 'fluent-langneg'
+import { acceptedLanguages, negotiateLanguages } from '@fluent/langneg'
 import AppConstants from '../app-constants.js'
 
 import { getSubscribersByHashes } from '../db/tables/subscribers.js'
