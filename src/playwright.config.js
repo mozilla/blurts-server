f6d556fac8b18fb3e40d26812b6b529825d8c6f2
Joey Zhou <jozhou@mozilla.com>
2023-03-04 12:38:47 -0800

    increase timeout for github actions

--- src/playwright.config.js
+++ src/playwright.config.js
@@ -15,13 +15,13 @@ dotenv.config()
 export default defineConfig({
   testDir: './e2e/specs',
   /* Maximum time one test can run for. */
-  timeout: 60_000,
+  timeout: 120_000,
 
   /* Global setup */
   globalSetup: './e2e/globalSetup.js',
 
   /* Max time in milliseconds the whole test suite can to prevent CI breaking. */
-  globalTimeout: 360_000,
+  globalTimeout: 600_000,
 
   // adding missing snapshots for later comparison
   updateSnapshots: 'missing',
