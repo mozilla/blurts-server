d806908d4a8913a62cdaa237adbe773fdd6717fd
Robert Helmer <rhelmer@mozilla.com>
2023-03-21 21:45:40 -0700

    honor DNT

--- src/views/mainLayout.js
+++ src/views/mainLayout.js
@@ -36,14 +36,25 @@ const mainLayout = data => `
     <script src='/js/index.js' type='module'></script>
 
     <!-- Google tag (gtag.js) -->
-    <script nonce='${data.nonce}'>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    'https://www.googletagmanager.com/gtag/js?id='+i+dl;var n=d.querySelector('[nonce]');
-    n&&j.setAttribute('nonce',n.nonce||n.getAttribute('nonce'));f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','${AppConstants.GA4_MEASUREMENT_ID}');
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date()); gtag('config', '${AppConstants.GA4_MEASUREMENT_ID}');</script>
+    <script nonce='${data.nonce}' type='module'>
+      import { dntEnabled } from '/js/analytics_dnt-helper.js'
+      if (!dntEnabled()) {
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtag/js?id='+i+dl;var n=d.querySelector('[nonce]');
+        n&&j.setAttribute('nonce',n.nonce||n.getAttribute('nonce'));f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','${AppConstants.GA4_MEASUREMENT_ID}');
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date()); gtag('config', '${AppConstants.GA4_MEASUREMENT_ID}');
+        window.gtag = gtag
+      } else {
+        function gtag() {
+          console.debug("Google Analytics disbled by DNT")
+        }
+        window.gtag = gtag
+      }
+      </script>
     <!-- End Google tag (gtag.js) -->
   </head>
   <body>
