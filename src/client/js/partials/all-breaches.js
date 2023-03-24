0fb591551aab4831647f2096e093c7e80fd8dfb1
Amri Toufali <atoufali@mozilla.com>
2023-03-23 21:52:29 -0700

    fix search and avoid page reload on enter keypress

--- src/client/js/partials/all-breaches.js
+++ src/client/js/partials/all-breaches.js
@@ -2,27 +2,31 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-const allBreachesPartial = document.querySelector('[data-partial="allBreachesPartial"]')
+const allBreaches = document.querySelector('[data-partial="allBreaches"]')
+let search, breachCards
 
-if (allBreachesPartial) {
-  const search = document.getElementById('breach-search')
+function init () {
+  search = document.getElementById('breach-search')
   search.value = ''
-
-  const filter = () => {
-    const breachCards = document.querySelectorAll('.breach-card')
-    if (search.value.length === 0) {
-      breachCards.forEach(card => (card.style.display = ''))
-    } else {
-      breachCards.forEach(card => {
-        if (card.text.toLowerCase().includes(search.value.toLowerCase())) {
-          card.style.display = ''
-        } else {
-          card.style.display = 'none'
-        }
-      })
-    }
-  }
-
-  search.addEventListener('keyup', () => filter())
-  search.form.addEventListener('submit', () => filter())
+  search.addEventListener('keyup', filter)
+  search.form.addEventListener('submit', filter)
+  breachCards = allBreaches.querySelectorAll('.breach-card')
 }
+
+function filter (e) {
+  e.preventDefault()
+
+  if (search.value.length === 0) {
+    breachCards.forEach(card => (card.style.display = ''))
+  } else {
+    breachCards.forEach(card => {
+      if (card.text.toLowerCase().includes(search.value.toLowerCase())) {
+        card.style.display = ''
+      } else {
+        card.style.display = 'none'
+      }
+    })
+  }
+}
+
+if (allBreaches) init()
