fac64c9a3c97b36802f130110e8ff4b911888fb4
Joey Zhou <josephyzhou@gmail.com>
2023-04-03 08:36:48 -0700

    Moving csrf to routing index

--- src/routes/index.js
+++ src/routes/index.js
@@ -19,20 +19,21 @@ import { dialog } from '../controllers/dialog.js'
 import { landingPage } from '../controllers/landing.js'
 import { notFoundPage } from '../controllers/notFound.js'
 import { notFound } from '../middleware/error.js'
+import { doubleCsrfProtection } from '../utils/csrf.js'
 
 const router = express.Router()
 
 router.get('/', landingPage)
 router.get('*/dialog/:name', dialog)
 
-router.use('/', dockerFlowRoutes)
-router.use('/admin', adminRoutes)
+router.use('/', doubleCsrfProtection, dockerFlowRoutes)
+router.use('/admin', doubleCsrfProtection, adminRoutes)
 router.use('/api/v1/hibp/', hibpApiRoutes)
-router.use('/api/v1/user/', userApiRoutes)
-router.use('/oauth', authRoutes)
-router.use('/user', userRoutes)
-router.use('/breaches', breachesRoutes)
-router.use('/breach-details', breachDetailsRoutes)
+router.use('/api/v1/user/', doubleCsrfProtection, userApiRoutes)
+router.use('/oauth', doubleCsrfProtection, authRoutes)
+router.use('/user', doubleCsrfProtection, userRoutes)
+router.use('/breaches', doubleCsrfProtection, breachesRoutes)
+router.use('/breach-details', doubleCsrfProtection, breachDetailsRoutes)
 
 // Do not make the non-auth previews available on prod
 if (AppConstants.NODE_ENV !== 'production') {
