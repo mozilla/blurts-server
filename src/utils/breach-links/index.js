b0756430190b92fc4f86925464006ec969721e58
Florian Zia <zia.florian@gmail.com>
2023-04-04 15:41:10 +0200

    chore: Add list of links with their status generated by https://github.com/pdehaan/blurts-https-stats

---
+++ src/utils/breach-links/index.js
@@ -0,0 +1,18 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+/**
+ * Check if the provided link is a valid link
+ *
+ * @param {string} linkUrl
+ * @returns {boolean} True if link is valid
+ */
+
+function isValidLink (linkUrl) {
+  return true
+}
+
+export {
+  isValidLink
+}
