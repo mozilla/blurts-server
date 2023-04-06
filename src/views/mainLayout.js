238f0dfe8de21210a45b015e85c79b6c08674d42
Robert Helmer <rhelmer@mozilla.com>
2023-04-06 13:46:48 -0700

    MNTOR-1509 - re-implement DNT check so it works with Safari

--- src/views/mainLayout.js
+++ src/views/mainLayout.js
@@ -42,20 +42,25 @@ const mainLayout = data => `
 
     <!-- Google tag (gtag.js) -->
     <script nonce='${data.nonce}'>
-      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-      'https://www.googletagmanager.com/gtag/js?id='+i+dl;var n=d.querySelector('[nonce]');
-      n&&j.setAttribute('nonce',n.nonce||n.getAttribute('nonce'));f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','${AppConstants.GA4_MEASUREMENT_ID}');
-      function gtag(){dataLayer.push(arguments);}
-      ${AppConstants.GA4_DEBUG_MODE
-        ? `gtag('config', '${AppConstants.GA4_MEASUREMENT_ID}', { 'debug_mode': true })`
-        : ''}
-      gtag('js', new Date());
-      gtag('config', '${AppConstants.GA4_MEASUREMENT_ID}',
-        { cookie_domain: window.location.hostname, cookie_flags: "SameSite=None;Secure" }
-      );
+      if (!navigator.doNotTrack || navigator.doNotTrack !== '1') {
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtag/js?id='+i+dl;var n=d.querySelector('[nonce]');
+        n&&j.setAttribute('nonce',n.nonce||n.getAttribute('nonce'));f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','${AppConstants.GA4_MEASUREMENT_ID}');
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${AppConstants.GA4_MEASUREMENT_ID}', {
+          cookie_domain: window.location.hostname,
+          cookie_flags: "SameSite=None;Secure",
+          debug_mode: ${Boolean(AppConstants.GA4_DEBUG_MODE)}
+        });
+      } else {
+        function gtag() {q
+          console.debug("Google Analytics disbled by DNT")
+        }
+      }
       window.gtag = gtag
     </script>
     <!-- End Google tag (gtag.js) -->
