6d4f5f66e05496906444819303a8ecd5a9e7c050
Robert Helmer <rhelmer@mozilla.com>
2023-04-07 21:50:35 -0700

    formatting

--- src/views/partials/analytics.js
+++ src/views/partials/analytics.js
@@ -5,33 +5,33 @@
 import AppConstants from '../../app-constants.js'
 
 export const analytics = nonce => `
-<script nonce='${nonce}' async
-  src="https://www.googletagmanager.com/gtag/js?id=${AppConstants.GA4_MEASUREMENT_ID}">
-</script>
-<script nonce='${nonce}'>
-  if (navigator.doNotTrack === '1') {
-    function gtag() {
-      console.debug("Google Analytics disabled by DNT")
-    }
-  } else {
-    window.dataLayer = window.dataLayer || []
-    function gtag() {
-      dataLayer.push(arguments)
-    }
-    gtag('js', new Date())
-    gtag('config', '${AppConstants.GA4_MEASUREMENT_ID}', {
-      cookie_domain: window.location.hostname,
-      cookie_flags: "SameSite=None;Secure",
-      debug_mode: '${Boolean(AppConstants.GA4_DEBUG_MODE)}'
-    })
-
-    // Instrument CTA clicks for analytics.
-    document.querySelectorAll('[data-cta-id]').forEach(a =>
-      a.addEventListener('click', e => {
-        gtag('event', 'clicked_cta', { cta_id: a.dataset.ctaId })
+  <script nonce='${nonce}' async
+    src="https://www.googletagmanager.com/gtag/js?id=${AppConstants.GA4_MEASUREMENT_ID}">
+  </script>
+  <script nonce='${nonce}'>
+    if (navigator.doNotTrack === '1') {
+      function gtag() {
+        console.debug("Google Analytics disabled by DNT")
       }
-    ))
-  }
-  window.gtag = gtag
-</script>
+    } else {
+      window.dataLayer = window.dataLayer || []
+      function gtag() {
+        dataLayer.push(arguments)
+      }
+      gtag('js', new Date())
+      gtag('config', '${AppConstants.GA4_MEASUREMENT_ID}', {
+        cookie_domain: window.location.hostname,
+        cookie_flags: "SameSite=None;Secure",
+        debug_mode: '${Boolean(AppConstants.GA4_DEBUG_MODE)}'
+      })
+
+      // Instrument CTA clicks for analytics.
+      document.querySelectorAll('[data-cta-id]').forEach(a =>
+        a.addEventListener('click', e => {
+          gtag('event', 'clicked_cta', { cta_id: a.dataset.ctaId })
+        }
+      ))
+    }
+    window.gtag = gtag
+  </script>
 `
