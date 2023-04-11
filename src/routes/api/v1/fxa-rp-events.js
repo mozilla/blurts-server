78c99db0d1aba77fc633218961a3c78ce7173b55
Joey Zhou <josephyzhou@gmail.com>
2023-04-11 13:08:24 -0700

    lint

--- src/routes/api/v1/fxa-rp-events.js
+++ src/routes/api/v1/fxa-rp-events.js
@@ -2,13 +2,12 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
- import { Router } from 'express'
- import { asyncMiddleware } from '../../../middleware/util.js'
- import { fxaRpEvents } from '../../../controllers/fxa-rp-events.js'
- 
- const router = Router()
- 
- router.post('/fxa-rp-events', asyncMiddleware(fxaRpEvents))
- 
- export default router
- 
+import { Router } from 'express'
+import { asyncMiddleware } from '../../../middleware/util.js'
+import { fxaRpEvents } from '../../../controllers/fxa-rp-events.js'
+
+const router = Router()
+
+router.post('/fxa-rp-events', asyncMiddleware(fxaRpEvents))
+
+export default router
