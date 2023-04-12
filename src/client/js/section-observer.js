b55651408bbf3b7cce757aea5b186bcc7e05a4b2
Florian Zia <zia.florian@gmail.com>
2023-04-12 13:55:41 +0200

    feat: Add basic transitions to landing page sections

--- src/client/js/section-observer.js
+++ src/client/js/section-observer.js
@@ -3,11 +3,31 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 const classNameToObserve = 'section-transition'
+const classNameEntered = `${classNameToObserve}-entered`
+
+const CUE_INTERVAL = 150
+const SECTION_SCROLL_THRESHOLD = 0.1
+
 let observers
+let cueInterval
+const entryCue = []
+
+function handleShowSection () {
+  if (!entryCue?.length && cueInterval) {
+    clearInterval(cueInterval)
+    cueInterval = null
+    return
+  }
+
+  const nextEntry = entryCue.shift()
+  nextEntry.target.classList.add(classNameEntered)
+}
+
+function setCueInterval () {
+  cueInterval = setInterval(handleShowSection, CUE_INTERVAL)
+}
 
 function handleScroll (entries) {
-  const classNameEntered = `${classNameToObserve}-entered`
-
   entries.forEach(entry => {
     const sectionElement = entry.target
     const hasEntered = sectionElement.classList.contains(classNameEntered)
@@ -18,22 +38,38 @@ function handleScroll (entries) {
 
     const isInViewport = entry.isIntersecting
     if (isInViewport) {
-      sectionElement.classList.add(classNameEntered)
+      entryCue.push(entry)
+    }
+
+    if (!cueInterval) {
+      setCueInterval()
     }
   })
 }
 
 function init (sections) {
-  const observer = new IntersectionObserver(handleScroll)
+  const observer = new IntersectionObserver(handleScroll, {
+    threshold: SECTION_SCROLL_THRESHOLD
+  })
 
   observers = [...sections].map(section => {
     observer.observe(section)
     return observer
   })
+
+  setCueInterval()
 }
 
 if (!observers) {
-  const sections = document.getElementsByClassName(classNameToObserve)
+  const mediaQuery = window.matchMedia('(prefers-reduced-motion: no-preference)')
+  const allowMotion = mediaQuery && mediaQuery.matches
 
-  init(sections)
+  const sections = document.getElementsByClassName(classNameToObserve)
+  mediaQuery.addEventListener('change', () => {
+    [...sections].forEach(section => section.classList.add(classNameEntered))
+  })
+
+  if (allowMotion) {
+    init(sections)
+  }
 }
