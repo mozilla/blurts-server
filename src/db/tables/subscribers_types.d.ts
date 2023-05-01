27e28d07eeef92795bcaa8bcdf59671c14fe81fd
Joey Zhou <jozhou@mozilla.com>
2023-05-01 12:49:47 -0700

    add back comment

--- src/db/tables/subscribers_types.d.ts
+++ src/db/tables/subscribers_types.d.ts
@@ -2,6 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+// This file contains types for subscribers.js until it's fully typed
 export type SubscriberRow = {
   id: number;
   sha1: string;
