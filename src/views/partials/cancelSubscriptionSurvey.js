d6d5113c63daedc56a6d0a1d8f45d87ec596ce8c
Kaitlyn <kandres@mozilla.com>
2023-05-11 13:52:39 -0400

    fix lint errors

--- src/views/partials/cancelSubscriptionSurvey.js
+++ src/views/partials/cancelSubscriptionSurvey.js
@@ -4,35 +4,25 @@
 
 import { getMessage } from '../../utils/fluent.js'
 
-/**
- * @typedef {object} PartialData
- * @property {string} csrfToken
- * @property {number} emailLimit
- */
-
-/**
- * @param {PartialData} data
- * @returns string
- */
-
-export const cancelSubscriptionSurvey = () => `
-<div class='dialog'>
-<header>
-  <button class='close'></button>
-  <img src='/images/dialog-unsubscribe-plane.svg'>
-  <h2>${getMessage('settings-unsubscribe-dialog-title')}</h2>
-</header>
-<form id='unsubscribe-feedback-form' action='' method=''>
-<!-- TODO: Needs a submit endpoint for the form --->
-  <p>${getMessage('settings-unsubscribe-dialog-info')}</p>
-  <textarea id='unsubscribe-feedback' placeholder='${getMessage('settings-unsubscribe-dialog-message-placeholder')}'></textarea>
-  <p class='warning'>${getMessage('settings-unsubscribe-dialog-confirmation', {faq_href: 'href="https://support.mozilla.org/kb/general-questions-about-privacy-protection-scans#w_what-happens-when-i-unsubscribe-from-premium" target="_blank"'})}
-  </p>
-  <!-- TODO: Figure out if this button is functioning as a form submission or a link to go to another page --> 
-    <button id='continue-to-unsubscribe-flow-btn' type='submit' class='primary'>
-      ${getMessage('settings-unsubscribe-dialog-continue')}
-    </button>
-  <a href="/user/breaches">${getMessage('settings-unsubscribe-dialog-cancel')}</a>
-</form>
-</div>
+export const cancelSubscriptionSurvey = () =>
+`
+  <div class='dialog'>
+  <header>
+    <button class='close'></button>
+    <img src='/images/dialog-unsubscribe-plane.svg'>
+    <h2>${getMessage('settings-unsubscribe-dialog-title')}</h2>
+  </header>
+  <form id='unsubscribe-feedback-form' action='' method=''>
+  <!-- TODO: Needs a submit endpoint for the form --->
+    <p>${getMessage('settings-unsubscribe-dialog-info')}</p>
+    <textarea id='unsubscribe-feedback' placeholder='${getMessage('settings-unsubscribe-dialog-message-placeholder')}'></textarea>
+    <p class='warning'>${getMessage('settings-unsubscribe-dialog-confirmation', { faq_href: 'href="https://support.mozilla.org/kb/general-questions-about-privacy-protection-scans#w_what-happens-when-i-unsubscribe-from-premium" target="_blank"' })}
+    </p>
+    <!-- TODO: Figure out if this button is functioning as a form submission or a link to go to another page --> 
+      <button id='continue-to-unsubscribe-flow-btn' type='submit' class='primary'>
+        ${getMessage('settings-unsubscribe-dialog-continue')}
+      </button>
+    <a href="/user/breaches">${getMessage('settings-unsubscribe-dialog-cancel')}</a>
+  </form>
+  </div>
 `
