a7aee01f826ba54cb33a021803e7c1895fd0e185
Kaitlyn <kandres@mozilla.com>
2023-05-11 14:21:12 -0400

    add aria-label to button

--- src/views/partials/cancelSubscriptionSurvey.js
+++ src/views/partials/cancelSubscriptionSurvey.js
@@ -9,7 +9,7 @@ export const cancelSubscriptionSurvey = () =>
   <div class='dialog'>
   <header>
     <button class='close'></button>
-    <img src='/images/dialog-unsubscribe-plane.svg'>
+    <img alt='' src='/images/dialog-unsubscribe-plane.svg'>
     <h2>${getMessage('settings-unsubscribe-dialog-title')}</h2>
   </header>
   <form id='unsubscribe-feedback-form' action='' method=''>
@@ -19,7 +19,7 @@ export const cancelSubscriptionSurvey = () =>
     <p class='warning'>${getMessage('settings-unsubscribe-dialog-confirmation', { faq_href: 'href="https://support.mozilla.org/kb/general-questions-about-privacy-protection-scans#w_what-happens-when-i-unsubscribe-from-premium" target="_blank"' })}
     </p>
     <!-- TODO: Figure out if this button is functioning as a form submission or a link to go to another page --> 
-      <button id='continue-to-unsubscribe-flow-btn' type='submit' class='primary'>
+      <button aria-label='${getMessage('settings-unsubscribe-dialog-continue')}' id='continue-to-unsubscribe-flow-btn' type='submit' class='primary'>
         ${getMessage('settings-unsubscribe-dialog-continue')}
       </button>
     <a href="/user/breaches">${getMessage('settings-unsubscribe-dialog-cancel')}</a>
