9df6ee3aeb8839ff831ecf3078d3402e9f0307fc
Robert Helmer <rhelmer@mozilla.com>
2023-05-26 16:55:04 -0700

    Add sentry to nextJS branch (#3075)
    MNTOR-1641 - enable Sentry for NextJS, for front- and back-end code

---
+++ sentry.server.config.ts
@@ -0,0 +1,19 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: process.env.NODE_ENV === "development",
+});
