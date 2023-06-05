d26baf4e4619004d9c2b8ea27c5504c1ec7a7379
Joey Zhou <jozhou@mozilla.com>
2023-06-05 11:46:18 -0700

    fix lint js

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -7,10 +7,8 @@ const buildEslintCommand = (filenames) =>
  
 export default {
   '*.{js,jsx,ts,tsx}': [buildEslintCommand],
-  "*.{scss,css}": "stylelint --fix",
-  "*.{ts,tsx,jsx,scss}": "prettier --write",
+  "*.{ts,tsx,jsx,scss,css,md}": "prettier --write",
   // While we're migrating to Next.js, regular .js and .css files are still
   // likely to be the non-Next.js app. Thus, we scope those to /src/app:
-  "src/app/**/*.{ts,tsx,js,jsx,scss,css}": "prettier --write",
-  "*.md": "prettier --write",
+  "src/app/**/*.{ts,tsx,js,jsx,scss,css}": "prettier --write"
 };
--- src/db/tables/subscribers_types.d.ts
+++ src/db/tables/subscribers_types.d.ts
@@ -1,5 +1,8 @@
-// This file contains types for subscribers.js until it's fully typed
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+// This file contains types for subscribers.js until it's fully typed
 export type SubscriberRow = {
   id: number;
   sha1: string;
@@ -7,4 +10,4 @@ export type SubscriberRow = {
   verification_token: string;
   verified: boolean;
   onerep_profile_id?: number;
-}
+};
