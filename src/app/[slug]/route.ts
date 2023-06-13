7b6bb39f0fb631201c44474867fc50d1f348cbfe
Vincent <git@vincenttunru.com>
2023-06-13 09:12:03 +0200

    Fetch breach data and icons on heartbeat
    To ensure breach data and icons are available as soon as possible,
    this starts fetching them as soon as the __heartbeat__ endpoint
    receives a call. Note that they won't _delay_ that call; the
    Response is sent while the Promise callback is still pending.

--- src/app/[slug]/route.ts
+++ src/app/[slug]/route.ts
@@ -4,11 +4,17 @@
 
 import { NextRequest, NextResponse } from "next/server";
 import { vers } from "../../controllers/dockerflow.js";
+import { getBreaches, getBreachIcons } from "../functions/server/getBreaches";
 
 export async function GET(req: NextRequest) {
   // heartbeat route for dockerflow
   const slug = req.nextUrl.pathname;
   if (slug.includes("__heartbeat__") || slug.includes("__lbheartbeat__")) {
+    // Ensure breaches and their icons are loaded after app startup.
+    // Note: we do not `await` this Promise, to ensure we do not delay sending
+    //       the heartbeat response while we are still fetching the data.
+    // TODO: Replace with a cron job to fetch breach data and icons.
+    getBreaches().then(breaches => getBreachIcons(breaches));
     return NextResponse.json({ success: true, message: "OK" }, { status: 200 });
   }
 
