1081c52465a4ceeef7442fb20053776cbdc22202
Florian Zia <zia.florian@gmail.com>
2023-06-14 16:09:21 +0200

    fix: Load layout relavant scripts for landing page async

--- src/app/(nextjs_migration)/layout.tsx
+++ src/app/(nextjs_migration)/layout.tsx
@@ -15,8 +15,7 @@ export default async function MigrationLayout({
   const l10nBundles = getL10nBundles();
   return (
     <L10nProvider bundleSources={l10nBundles}>
-      {/* eslint-disable-next-line @next/next/no-sync-scripts */}
-      <script type="module" src="/nextjs_migration/client/js/resizeObserver.js" rel="preload" />
+      <script type="module" src="/nextjs_migration/client/js/resizeObserver.js" async />
       <Script type="module" src="/nextjs_migration/client/js/analytics.js" />
       {children}
     </L10nProvider>
