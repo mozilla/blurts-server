6c85601b4e608a88377b18f8d5dfb51edc831c91
Robert Helmer <rhelmer@mozilla.com>
2023-06-16 11:55:29 -0700

    format and use NEXT_PUBLIC_ for client config

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -24,7 +24,7 @@ Sentry.init({
   // This sets the sample rate to be 10%. You may want this to be 100% while
   // in development and sample at a lower rate in production
   replaysSessionSampleRate: ["development", "heroku"].includes(
-    process.env.NODE_ENV
+    process.env.NEXT_PUBLIC_NODE_ENV
   )
     ? 1.0
     : 0.1,
