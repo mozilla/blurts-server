ab8c8152fbe1f6a82ab6ccb8e916a1bcecb24060
Florian Zia <zia.florian@gmail.com>
2023-06-20 15:01:34 +0200

    chore: Move dialog GET request logic into trycatch block

--- src/app/api/v1/dialog/[...type]/route.ts
+++ src/app/api/v1/dialog/[...type]/route.ts
@@ -8,26 +8,22 @@ import type { NextRequest } from "next/server";
 import { getComponentAsString } from '../../../../functions/server/getComponentAsString'
 import AddEmailDialog from '../../../../(nextjs_migration)/components/server/AddEmailDialog'
 
-export async function GET(req: NextRequest) {
-  const dialogUrl = req.url
-  const dialogType = dialogUrl.split('/').slice(-1)[0]
-  let dialogContentString = ''
-
-  switch (dialogType) {
-    case 'addEmail':
-      dialogContentString = await getComponentAsString({
-        component: AddEmailDialog()
-      });
-      break;
-    default:
-      throw new Error(`No dialog found for: ${dialogUrl}`);
-  }
-
-  if (!dialogContentString) {
-    return NextResponse.json({ success: false }, { status: 500});
-  }
-
+export async function GET(req: NextRequest) {  
   try {
+    const dialogUrl = req.url
+    const dialogType = dialogUrl.split('/').slice(-1)[0]
+
+    let dialogContentString = ''
+    switch (dialogType) {
+      case 'addEmail':
+        dialogContentString = await getComponentAsString({
+          component: AddEmailDialog()
+        });
+        break;
+      default:
+        throw new Error(`No dialog found for: ${dialogUrl}`);
+    }
+
     return new NextResponse(dialogContentString, { status: 200 });
   } catch (e) {
     return NextResponse.json({ success: false }, { status: 500});
