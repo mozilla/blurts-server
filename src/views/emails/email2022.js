33886dea092be7bcff89763ecf3b76015674190d
Vincent <git@vincenttunru.com>
2023-06-21 15:50:44 +0200

    Fix HIBP link in verification email

--- src/views/emails/email2022.js
+++ src/views/emails/email2022.js
@@ -142,7 +142,12 @@ const emailFooter = (data, l10n) => {
       <p>
         ${getMessage('email-2022-hibp-attribution', {
         'hibp-link-attr': `href='${links(data).hibp}' rel='noopener'`
-      })}
+      })
+      // The following are special characters inserted by Fluent,
+      // which break the link when inserted into the tag.
+      ?.replaceAll("⁩", "")
+      .replaceAll("⁨", "")
+      }
       </p>
       <img
         alt='${getMessage('mozilla')}'
