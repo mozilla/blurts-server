e6edb26df8aa380ddebdb0c2c46d5653767e2650
Joey Zhou <jozhou@mozilla.com>
2023-06-22 13:34:25 -0700

    lint

--- src/app/api/v1/admin/feature-flags/[flagId]/route.ts
+++ src/app/api/v1/admin/feature-flags/[flagId]/route.ts
@@ -4,9 +4,7 @@
 
 import { NextRequest, NextResponse } from "next/server";
 import { getToken } from "next-auth/jwt";
-import {
-  getFeatureFlagByName
-} from "../../../../../../db/tables/featureFlags";
+import { getFeatureFlagByName } from "../../../../../../db/tables/featureFlags";
 import { isAdmin } from "../../../../utils/auth";
 import appConstants from "../../../../../../appConstants";
 
@@ -16,17 +14,16 @@ export async function GET(req: NextRequest) {
   const token = await getToken({ req });
   if (typeof token?.email === "string" && isAdmin(token?.email)) {
     // Signed in
-    const flagName = req.nextUrl.toString().split('/').pop() || ''
-    console.log({ flagName })
+    const flagName = req.nextUrl.toString().split("/").pop() || "";
+    console.log({ flagName });
     try {
-      const flag = await getFeatureFlagByName(flagName)
+      const flag = await getFeatureFlagByName(flagName);
       return NextResponse.json(flag);
     } catch (e) {
-      console.error(e)
+      console.error(e);
       return NextResponse.json({ success: false }, { status: 500 });
     }
-  }
-  else {
+  } else {
     // Not Signed in, redirect to home
     return NextResponse.redirect(appConstants.SERVER_URL, 301);
   }
--- src/app/api/v1/admin/feature-flags/route.ts
+++ src/app/api/v1/admin/feature-flags/route.ts
@@ -10,7 +10,7 @@ import {
   enableFeatureFlagByName,
   disableFeatureFlagByName,
   updateAllowList,
-  updateWaitList
+  updateWaitList,
 } from "../../../../../db/tables/featureFlags";
 
 import { FeatureFlag } from "../../../../../db/tables/featureFlags";
@@ -22,13 +22,12 @@ export async function GET(req: NextRequest) {
   if (typeof token?.email === "string" && isAdmin(token?.email)) {
     // Signed in
     try {
-      const flags = await getAllFeatureFlags()
+      const flags = await getAllFeatureFlags();
       return NextResponse.json(flags);
     } catch (e) {
       return NextResponse.json({ success: false }, { status: 500 });
     }
-  }
-  else {
+  } else {
     // Not Signed in, redirect to home
     return NextResponse.redirect(appConstants.SERVER_URL, 301);
   }
@@ -39,54 +38,52 @@ export async function POST(req: NextRequest) {
   if (typeof token?.email === "string" && isAdmin(token?.email)) {
     // Signed in
     try {
-      const newFlag = await req.json() as FeatureFlag
-      const resp = await addFeatureFlag(newFlag)
-      return NextResponse.json(resp)
+      const newFlag = (await req.json()) as FeatureFlag;
+      const resp = await addFeatureFlag(newFlag);
+      return NextResponse.json(resp);
     } catch (e) {
-      console.error(e)
+      console.error(e);
       return NextResponse.json({ success: false }, { status: 500 });
     }
-  }
-  else {
+  } else {
     // Not Signed in, redirect to home
     return NextResponse.redirect(appConstants.SERVER_URL, 301);
   }
 }
 
 type FeatureFlagPutRequest = {
-  name: string
-  isEnabled?: boolean
-  allowList?: [string]
-  waitList?: [string]
-}
+  name: string;
+  isEnabled?: boolean;
+  allowList?: [string];
+  waitList?: [string];
+};
 
 export async function PUT(req: NextRequest) {
   const token = await getToken({ req });
   if (typeof token?.email === "string" && isAdmin(token?.email)) {
     // Signed in
     try {
-      const reqBody = await req.json() as FeatureFlagPutRequest
+      const reqBody = (await req.json()) as FeatureFlagPutRequest;
       // if toggle request
       if (reqBody.isEnabled !== undefined && reqBody.isEnabled) {
-        await enableFeatureFlagByName(reqBody.name)
+        await enableFeatureFlagByName(reqBody.name);
       } else if (reqBody.isEnabled !== undefined && !reqBody.isEnabled) {
-        await disableFeatureFlagByName(reqBody.name)
+        await disableFeatureFlagByName(reqBody.name);
       }
 
       if (reqBody.allowList) {
-        await updateAllowList(reqBody.name, reqBody.allowList)
+        await updateAllowList(reqBody.name, reqBody.allowList);
       }
 
       if (reqBody.waitList) {
-        await updateWaitList(reqBody.name, reqBody.waitList)
+        await updateWaitList(reqBody.name, reqBody.waitList);
       }
 
-      return NextResponse.json({ success: true, name: reqBody.name })
+      return NextResponse.json({ success: true, name: reqBody.name });
     } catch (e) {
       return NextResponse.json({ success: false }, { status: 500 });
     }
-  }
-  else {
+  } else {
     // Not Signed in, redirect to home
     return NextResponse.redirect(appConstants.SERVER_URL, 301);
   }
