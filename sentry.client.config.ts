61c2369aaded513e011276b3065f4d8f22e0cd9f
Robert Helmer <rhelmer@mozilla.com>
2023-06-22 18:13:13 -0700

    use NODE_ENV

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -24,7 +24,7 @@ Sentry.init({
   // This sets the sample rate to be 10%. You may want this to be 100% while
   // in development and sample at a lower rate in production
   replaysSessionSampleRate: ["development", "heroku"].includes(
-    process.env.NEXT_PUBLIC_NODE_ENV
+    process.env.NODE_ENV
   )
     ? 1.0
     : 0.1,
@@ -33,8 +33,8 @@ Sentry.init({
   integrations: [
     new Sentry.Replay({
       // Additional Replay configuration goes in here, for example:
-      maskAllText: process.env.NEXT_PUBLIC_NODE_ENV === "development",
-      blockAllMedia: process.env.NEXT_PUBLIC_NODE_ENV === "development",
+      maskAllText: process.env.NODE_ENV === "development",
+      blockAllMedia: process.env.NODE_ENV === "development",
     }),
   ],
 });
