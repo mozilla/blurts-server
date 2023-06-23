1b230d0d997514f19d6039ac717a2a36ff591385
Robert Helmer <rhelmer@mozilla.com>
2023-06-22 18:43:22 -0700

    config: disable Sentry debug logging, and only enable full traces in dev envs (#3138)

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -12,24 +12,29 @@ Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
 
   // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1,
+  tracesSampleRate: ["development", "heroku"].includes(process.env.NODE_ENV)
+    ? 1.0
+    : 0.1,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: true,
+  debug: false,
 
   replaysOnErrorSampleRate: 1.0,
 
   // This sets the sample rate to be 10%. You may want this to be 100% while
   // in development and sample at a lower rate in production
-  replaysSessionSampleRate:
-    process.env.NEXT_PUBLIC_NODE_ENV === "development" ? 1.0 : 0.1,
+  replaysSessionSampleRate: ["development", "heroku"].includes(
+    process.env.NODE_ENV
+  )
+    ? 1.0
+    : 0.1,
 
   // You can remove this option if you're not planning to use the Sentry Session Replay feature:
   integrations: [
     new Sentry.Replay({
       // Additional Replay configuration goes in here, for example:
-      maskAllText: process.env.NEXT_PUBLIC_NODE_ENV === "development",
-      blockAllMedia: process.env.NEXT_PUBLIC_NODE_ENV === "development",
+      maskAllText: process.env.NODE_ENV === "development",
+      blockAllMedia: process.env.NODE_ENV === "development",
     }),
   ],
 });
