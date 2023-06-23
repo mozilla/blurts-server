5466150b2f58b03522b3861213a7cb8e960ee7e3
Joey Zhou <jozhou@mozilla.com>
2023-06-23 11:58:49 -0700

    update syncBreaches

--- src/scripts/syncBreaches.js
+++ src/scripts/syncBreaches.js
@@ -11,6 +11,56 @@
 
 import { req, formatDataClassesArray } from '../utils/hibp.js'
 import { getAllBreaches, upsertBreaches } from '../db/tables/breaches.js'
+import { mkdir, readdir } from "node:fs/promises";
+import { resolve as pathResolve } from "node:path";
+import { finished } from 'stream/promises';
+import fs from "fs";
+import { Readable } from 'stream';
+
+let logoMap;
+
+export async function getBreachIcons(breaches) {
+  if (logoMap) {
+    return logoMap;
+  }
+  async function fetchBreachIcons() {
+    const breachDomains = breaches
+      .map((breach) => breach.Domain)
+      .filter((breachDomain) => breachDomain.length > 0);
+
+    // make logofolder if it doesn't exist
+    const logoFolder = "public/logo_cache/";
+    if (!fs.existsSync(logoFolder)) await mkdir(logoFolder);
+
+    // read existing logos
+    const existingLogos = await readdir(logoFolder);
+    console.log({existingLogos})
+
+    // TODO: Batch to limit memory use?
+    const logoMapElems = (await Promise.all(
+      breachDomains.map(async (breachDomain) => {
+          const logoFilename = breachDomain.toLowerCase() + ".ico";
+          const logoPath = pathResolve(logoFolder, logoFilename);
+          if (existingLogos.includes(logoFilename)) {
+            console.log('skipping ', logoFilename)
+            return;
+          }
+          console.log(`fetching: , ${logoFilename}`)
+          const res = await fetch(
+            `https://icons.duckduckgo.com/ip3/${breachDomain}.ico`);
+          const fileStream = fs.createWriteStream(logoPath, { flags: 'wx' });
+          await finished(Readable.fromWeb(res.body).pipe(fileStream));
+      })
+    ));
+
+    logoMap = new Map(logoMapElems.filter(e => e != null));
+  }
+
+  // Start fetching breach icons, but do not await it so that we do not block
+  // pending requests:
+  await fetchBreachIcons();
+}
+
 
 const breachesResponse = await req('/breaches')
 const breaches = []
@@ -27,6 +77,9 @@ for (const breach of breachesResponse) {
 
 console.log('Breaches found: ', breaches.length)
 console.log('Unique breaches based on Name + BreachDate', seen.size)
+
+await getBreachIcons(breaches)
+console.log({logoMap})
 
 // sanity check: no duplicate breaches with Name + BreachDate
 if (seen.size !== breaches.length) {
