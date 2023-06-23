2a8c492a31bc436634ecd33011f826b353908287
ariyaIT <ariyadigital@protonmail.com>
2023-06-23 16:48:29 -0700

    use session

--- src/app/api/v1/admin/feature-flags/[flagId]/route.ts
+++ src/app/api/v1/admin/feature-flags/[flagId]/route.ts
@@ -3,17 +3,19 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 import { NextRequest, NextResponse } from "next/server";
-import { getToken } from "next-auth/jwt";
+import { getServerSession } from "next-auth";
 import { getFeatureFlagByName } from "../../../../../../db/tables/featureFlags";
-import { isAdmin } from "../../../../utils/auth";
+import { isAdmin, authOptions } from "../../../../utils/auth";
 import appConstants from "../../../../../../appConstants";
 
-export async function GET(req: NextRequest) {
-  const token = await getToken({ req });
-  if (typeof token?.email === "string" && isAdmin(token?.email)) {
+export async function GET(
+  req: NextRequest,
+  { params }: { params: { flagId: string } }
+) {
+  const session = await getServerSession(authOptions);
+  if (isAdmin(session?.user?.email || "")) {
     // Signed in
-    const flagName = req.nextUrl.toString().split("/").pop() || "";
-    console.log({ flagName });
+    const flagName = params.flagId;
     try {
       const flag = await getFeatureFlagByName(flagName);
       return NextResponse.json(flag);
