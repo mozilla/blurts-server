082024baee5ea741572db76d0d2dd8da7e21cb1c
Vincent <git@vincenttunru.com>
2023-06-26 17:09:08 +0200

    Convert test setup to Jest
    Next.js has integrated support for Jest.
    
    This also uncovered that we were ignoring the user's country and
    thus defaulting to the US in the Next.js port, so this also sets
    up country code detection for Next.js.
    
    This ports a couple of the existing tests; more complex ones will
    be ported in followup commits.

---
+++ src/app/functions/server/getCountryCode.ts
@@ -0,0 +1,30 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import type { headers as headersGetter } from "next/headers";
+
+export function getCountryCode(
+  headers: ReturnType<typeof headersGetter>
+): string {
+  // GCP can detect the user's country from their IP addresses, and pass it to
+  // us through this header:
+  const gcpDetectedRegion = headers.get("X-Client-Region");
+  if (gcpDetectedRegion) {
+    return gcpDetectedRegion.toLowerCase();
+  }
+  // On stage and production, the `X-Client-Region` should be available. To be
+  // able to locally test country-specific behaviour, we fall back to the
+  // Accept-Language:
+  const acceptLanguage = headers.get("Accept-Language");
+  if (acceptLanguage) {
+    const acceptedLocales = acceptLanguage.split(",");
+    const primaryLocale = acceptedLocales[0] ?? "";
+    const [locale, _weight] = primaryLocale.split(";");
+    const [_language, country] = (locale ?? "").split("-");
+    if (country) {
+      return country.toLowerCase();
+    }
+  }
+  return "us";
+}
