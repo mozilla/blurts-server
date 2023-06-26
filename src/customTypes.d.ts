d9b667ec7d0023543fea3a5b2273ff197c8914e3
Vincent <git@vincenttunru.com>
2023-06-26 14:34:57 +0200

    Add type definitions for database queries
    This allows many Knex operations to be typed, and allows us to
    track where the data we're dealing with came directly from the
    database.
    
    Note that I marked the `updated_at` columns as required on updates,
    so this also updates a couple of updates to actually update that
    column.

--- src/customTypes.d.ts
+++ src/customTypes.d.ts
@@ -2,37 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import { Subscriber } from "./app/(nextjs_migration)/(authenticated)/user/breaches/breaches";
-
-interface HTMLElement {
-  // We access this.shadowRoot in custom elements often, and we've almost always
-  // already called this.attachShadow({ mode: 'open' }). So, the default type
-  // that indicates that it might also be `null` generates a lot of noise for an
-  // error that we haven't run into yet. Thus, we override the default type to
-  // pretend that shadowRoot is always set.
-  shadowRoot: ShadowRoot;
-}
-
-// This lint rule does not apply to type definitions:
-// eslint-disable-next-line no-unused-vars
-type ViewPartial<ViewPartialParams = object> = (
-  data: ViewPartialParams & { partial: { name: string } }
-) => string;
-type GuestViewPartialData<ViewPartialParams = object> = {
-  partial: ViewPartial<ViewPartialParams>;
-  nonce: string;
-  meta?: {
-    title?: string;
-    socialTitle?: string;
-    socialDescription?: string;
-  };
-} & ViewPartialParams;
-type MainViewPartialData<ViewPartialParams = object> = {
-  fxaProfile: NonNullable<
-    import("express").Request["user"]
-  >["fxa_profile_json"];
-} & GuestViewPartialData<ViewPartialParams>;
-
 /**
  * See https://github.com/mozilla/fxa/blob/564949dfc69f0f675ebb4e5f267282c2546a5767/packages/fxa-profile-server/lib/routes/profile.js#L63-L77
  */
@@ -49,14 +18,6 @@ type FxaProfile = {
   metricsEnabled?: boolean;
   sub?: string;
 };
-declare namespace Express {
-  export interface Request {
-    user?: Subscriber & {
-      // TODO: Finish the type definition of the user object
-      fxa_profile_json?: FxaProfile;
-    };
-  }
-}
 
 declare module "mozlog" {
   type LogFunction = (_op: string, _details?: object | string) => void;
