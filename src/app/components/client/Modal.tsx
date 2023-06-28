e431a833f783307c7b6a7818a2c26a33b8920425
Kaitlyn <kandres@mozilla.com>
2023-06-28 14:35:08 -0400

    set up react-aria

--- src/app/components/client/Modal.tsx
+++ src/app/components/client/Modal.tsx
@@ -4,9 +4,19 @@
 
 "use client";
 
-import { ReactElement } from "react";
+import { ReactElement, useRef } from "react";
 import styles from "./Modal.module.scss"
 import { CloseBtn } from "../server/Icons";
+import {
+  OverlayContainer,
+  FocusScope,
+  useDialog,
+  useModal,
+  useOverlay,
+  usePreventScroll,
+  AriaOverlayProps,
+  useButton,
+} from "react-aria";
 
 export type Props = {
   isOpen: boolean,
@@ -14,20 +24,91 @@ export type Props = {
   content: ReactElement,
 }
 
+// export const Modal = (props: Props) => {
+//   if (!props.isOpen) {
+//     return null; // Render nothing if the modal is closed
+//   }
+
+//     return (
+//       <div role="dialog" aria-modal="true" aria-label="Modal" className={styles.modalOverlay}>
+//         <div className={styles.modal}>
+//           <div className={styles.modalContent}>
+//             {props.content}
+//             <button aria-label="Close modal" className={styles.closeButton} onClick={props.onClose}><CloseBtn alt="" width="14" height="14"/></button>
+//           </div>
+//         </div>
+//       </div>
+//     );
+
+// };
+
 export const Modal = (props: Props) => {
+
   if (!props.isOpen) {
-    return null; // Render nothing if the modal is closed
+    return null; 
   }
 
-    return (
-      <div role="dialog" aria-modal="true" aria-label="Modal" className={styles.modalOverlay}>
-        <div className={styles.modal}>
-          <div className={styles.modalContent}>
-            {props.content}
-            <button aria-label="Close modal" className={styles.closeButton} onClick={props.onClose}><CloseBtn alt="" width="14" height="14"/></button>
+  return (
+    <OverlayContainer>
+      <DialogBox
+        isOpen={props.isOpen}
+        headline={"Title goes here"}
+        body={"Body text goes here"}
+        onClose={() => props.onClose()}
+        isDismissable={true}
+        exitBtn={true}
+      ></DialogBox>
+    </OverlayContainer>
+  )
+
+};
+
+type DialogBox = {
+  headline: string | ReactElement;
+  body: string | ReactElement;
+  isOpen: boolean;
+  onClose: () => void;
+  exitBtn: boolean;
+};
+
+const DialogBox = (props: DialogBox & AriaOverlayProps) => {
+  const wrapperRef = useRef<HTMLDivElement>(null);
+  const { overlayProps, underlayProps } = useOverlay(props, wrapperRef);
+  usePreventScroll();
+  const { modalProps } = useModal();
+  const { dialogProps } = useDialog({}, wrapperRef);
+  const cancelButtonRef = useRef<HTMLButtonElement>(null);
+  const cancelButton = useButton(
+    { onPress: () => props.onClose() },
+    cancelButtonRef
+  );
+
+  return (
+    <div className={styles.underlay} {...underlayProps}>
+      <FocusScope contain restoreFocus autoFocus>
+        <div
+          {...overlayProps}
+          {...dialogProps}
+          {...modalProps}
+          ref={wrapperRef}
+        >
+          {props.exitBtn ? (
+            <button
+              {...cancelButton.buttonProps}
+              ref={cancelButtonRef}
+              className={styles["dismiss-button"]}
+            >
+              <CloseBtn alt="" width="14" height="14"/>
+            </button>
+          ) : null}
+          <div>
+            <div className={styles.hero}>
+              <h3 className={styles.headline}>{props.headline}</h3>
+            </div>
+            <div>{props.body}</div>
           </div>
         </div>
-      </div>
-    );
-
+      </FocusScope>
+    </div>
+  );
 };
