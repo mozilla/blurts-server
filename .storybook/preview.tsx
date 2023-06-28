41e4bf44272b52b9b20d05a770a8d053d742f7e9
Vincent <git@vincenttunru.com>
2023-06-28 18:42:17 +0200

    Add first screen of the onboarding flow

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -3,15 +3,29 @@ import { Inter } from "next/font/google";
 import type { Preview } from "@storybook/react";
 import "../src/app/globals.css";
 import { L10nProvider } from "../src/contextProviders/localization";
-import { getL10nBundles, getLocale } from "../src/app/functions/server/l10n";
 import { metropolis } from "../src/app/fonts/Metropolis/metropolis";
 import { ReactAriaI18nProvider } from "../src/contextProviders/react-aria";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 
+function loadL10nModule() {
+  if (process.env.NODE_ENV === "test") {
+    // In Jest, loading this module throws errors because it uses
+    // require.context(). Since we don't need to look at actual strings in our
+    // unit tests, we can just skip loading the bundles.
+    return {
+      getL10nBundles: () => [],
+      getLocale: () => "en",
+    };
+  }
+
+  return require("../src/app/functions/server/l10n");
+}
+
 const AppDecorator: Exclude<Preview["decorators"], undefined>[0] = (
   storyFn
 ) => {
+  const { getL10nBundles, getLocale } = loadL10nModule();
   const l10nBundles = getL10nBundles();
   return (
     <L10nProvider bundleSources={l10nBundles}>
---
+++ src/app/components/client/dialog/Dialog.tsx
@@ -0,0 +1,35 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import { ReactNode, useRef } from "react";
+import { AriaDialogProps, useDialog } from "react-aria";
+import styles from "./Dialog.module.scss";
+
+export type Props = {
+  children: ReactNode;
+  title?: ReactNode;
+  illustration?: ReactNode;
+} & AriaDialogProps;
+
+export const Dialog = ({
+  children,
+  title,
+  illustration,
+  ...otherProps
+}: Props) => {
+  const dialogRef = useRef<HTMLDivElement>(null);
+  const { dialogProps, titleProps } = useDialog(otherProps, dialogRef);
+
+  return (
+    <div {...dialogProps} ref={dialogRef} className={styles.dialog}>
+      {illustration ?? null}
+      {title && (
+        <h3 {...titleProps} className={styles.title}>
+          {title}
+        </h3>
+      )}
+      {children}
+    </div>
+  );
+};
