0aba84d2435ba939dba4fdf51bd07090ddbd1396
Vincent <git@vincenttunru.com>
2023-06-28 14:26:24 +0200

    Only use the dashboard shell on the dashboard

--- src/app/(proper_react)/redesign/MobileShell.tsx
+++ src/app/(proper_react)/redesign/MobileShell.tsx
@@ -9,9 +9,9 @@ import Link from "next/link";
 import Image from "next/image";
 import { Session } from "next-auth";
 import styles from "./MobileShell.module.scss";
-import monitorLogo from "./images/monitor-logo.webp";
-import { CloseBigIcon, ListIcon } from "../components/server/Icons";
-import { useL10n } from "../hooks/l10n";
+import monitorLogo from "../images/monitor-logo.webp";
+import { CloseBigIcon, ListIcon } from "../../components/server/Icons";
+import { useL10n } from "../../hooks/l10n";
 import { PageLink } from "./PageLink";
 
 export type Props = {
