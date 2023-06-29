1a0b03656eee3ba6ac58720321501390c0effd56
Vincent <git@vincenttunru.com>
2023-06-29 12:23:00 +0200

    Replace Chart.js by a plain SVG

--- src/app/components/client/Chart.tsx
+++ src/app/components/client/Chart.tsx
@@ -2,79 +2,112 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
- "use client";
+"use client";
 
- import { Chart as ChartJS, ArcElement, Tooltip, Legend, ChartOptions, Chart } from 'chart.js';
-import { Doughnut } from "react-chartjs-2";
-import styles from "./Chart.module.scss"
+import { Fragment, ReactNode } from "react";
+import { useL10n } from "../../hooks/l10n";
+import styles from "./Chart.module.scss";
 
 export type Props = {
-  labels: string[];
-  data: number[];
-  backgroundColors: string[];
+  data: Array<[string, number]>;
   totalExposures: number;
 };
 export const DoughnutChart = (props: Props) => {
-  const sumOfFixedExposures = props.data.reduce((total, num) => total + num, 0);
+  const l10n = useL10n();
+  const sumOfFixedExposures = props.data.reduce(
+    (total, [_label, num]) => total + num,
+    0
+  );
+  const percentages = props.data.map(([label, num]) => {
+    return [label, num / sumOfFixedExposures] as const;
+  });
 
-  // Draw stats in the center of doughnut chart
-  const textCenter = {
-    id: 'textCenter',
-    beforeDatasetsDraw(chart: any) {
-      const { ctx } = chart;
-      const xCoor = chart.getDatasetMeta(0).data[0].x;
-      const yCoor = chart.getDatasetMeta(0).data[0].y;
-      ctx.save();
+  const diameter = 100;
+  const ringWidth = 15;
+  const radius = (diameter - ringWidth) / 2;
+  const circumference = 2 * Math.PI * radius;
+  const sliceBorderWidth = 0;
+  const headingNumberSize = diameter / 8;
+  const headingLabelSize = headingNumberSize / 2;
+  const headingGap = 4;
 
-      ctx.font = '700 28px / 40px var(--font-metropolis), sans-serif';
-      ctx.fillStyle = 'black';
-      ctx.textAlign = 'center';
-      ctx.fillText(`${sumOfFixedExposures}`, xCoor, yCoor);
+  const slices = percentages.map(([label, percent], index) => {
+    const percentOffset = percentages
+      .slice(0, index)
+      .reduce((offset, [_label, num]) => offset + num, 0);
 
-      ctx.font = '400 14px / 21px var(--font-inter), sans-serif';
-      ctx.textAlign = 'center';
-      ctx.fillText("fixed", xCoor, yCoor + 20);
-    }
-  };
+    return (
+      <circle
+        key={label}
+        cx={diameter / 2}
+        cy={diameter / 2}
+        r={radius}
+        className={styles.slice}
+        fill="none"
+        strokeWidth={ringWidth}
+        strokeDasharray={`${circumference} ${circumference}`}
+        // Length of the slice
+        strokeDashoffset={circumference * (1 - percent) + sliceBorderWidth}
+        // Rotate it to not overlap the other slices
+        transform={`rotate(${-90 + 360 * percentOffset} ${diameter / 2} ${
+          diameter / 2
+        })`}
+      />
+    );
+  });
 
-
-  ChartJS.register(ArcElement, Tooltip, Legend, textCenter);
-
-  const data = {
-    labels: props.labels,
-    datasets: [
-      {
-        data: props.data,
-        backgroundColor: props.backgroundColors,
-      },
-    ],
-  };
-
-  const options: ChartOptions<'doughnut'> = {
-    plugins: {
-      legend: {
-        position: 'right',
-        align: 'center',
-        labels: {
-          usePointStyle: true,
-          // Add count next to each legend item
-          generateLabels: (chart: any) => {
-            const datasets = chart.data.datasets;
-            return datasets[0].data.map((data: any, i: number) => ({
-              text: `${chart.data.labels[i]} ${data}`,
-              fillStyle: datasets[0].backgroundColor[i],
-              index: i
-          }))},
-        },
-      },
-      [textCenter.id]: textCenter,
-    },
-  };
-  
   return (
-  <div className={styles.chartContainer}>
-    <Doughnut className={styles.doughnutChart} data={data} options={options} />
-    <p>This chart shows the total number of exposures that are fixed ({sumOfFixedExposures} out of {props.totalExposures}).</p>
-  </div>
+    <figure className={styles.chartContainer}>
+      <svg
+        // https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Roles/img_role#svg_and_roleimg
+        role="img"
+        aria-label={l10n
+          .getString("exposure-chart-heading", {
+            nr: sumOfFixedExposures,
+          })
+          .replace("<nr>", "")
+          .replace("</nr>", "")
+          .replace("<label>", "")
+          .replace("</label>", "")}
+        viewBox={`0 0 ${diameter} ${diameter}`}
+      >
+        <circle
+          cx={diameter / 2}
+          cy={diameter / 2}
+          r={radius}
+          fill="none"
+          strokeWidth={ringWidth}
+          className={styles.gutter}
+        />
+        {slices}
+        {l10n.getFragment("exposure-chart-heading", {
+          elems: {
+            nr: (
+              <text
+                className={styles.headingNr}
+                fontSize={headingNumberSize}
+                x={diameter / 2}
+                y={diameter / 2 - headingGap / 2}
+                textAnchor="middle"
+              />
+            ),
+            label: (
+              <text
+                className={styles.headingLabel}
+                fontSize={headingLabelSize}
+                x={diameter / 2}
+                y={diameter / 2 + headingLabelSize + headingGap / 2}
+                textAnchor="middle"
+              />
+            ),
+          },
+          vars: { nr: sumOfFixedExposures },
+        })}
+      </svg>
+      <figcaption>
+        This chart shows the total number of exposures that are fixed (
+        {sumOfFixedExposures} out of {props.totalExposures}).
+      </figcaption>
+    </figure>
   );
 };
