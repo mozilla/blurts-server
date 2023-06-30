707cdbbc7b4d5e14f1440c67fb0689c5be422afe
Vincent <git@vincenttunru.com>
2023-06-30 13:07:38 +0200

    Remove SSRProvider
    According to this log, it is no longer necessary:
    
    > In React 18, SSRProvider is not necessary and is a noop. You can
    > remove it from your app.

--- src/app/(proper_react)/layout.tsx
+++ src/app/(proper_react)/layout.tsx
@@ -5,21 +5,16 @@
 import { ReactNode } from "react";
 import { getL10nBundles, getLocale } from "../functions/server/l10n";
 import { L10nProvider } from "../../contextProviders/localization";
-import {
-  ReactAriaI18nProvider,
-  ReactAriaSsrProvider,
-} from "../../contextProviders/react-aria";
+import { ReactAriaI18nProvider } from "../../contextProviders/react-aria";
 
 export default async function Layout({ children }: { children: ReactNode }) {
   const l10nBundles = getL10nBundles();
 
   return (
     <L10nProvider bundleSources={l10nBundles}>
-      <ReactAriaSsrProvider>
-        <ReactAriaI18nProvider locale={getLocale(l10nBundles)}>
-          {children}
-        </ReactAriaI18nProvider>
-      </ReactAriaSsrProvider>
+      <ReactAriaI18nProvider locale={getLocale(l10nBundles)}>
+        {children}
+      </ReactAriaI18nProvider>
     </L10nProvider>
   );
 }
