62bde7af08e5013b41ce553e061c26b1f339f133
Kaitlyn <kandres@mozilla.com>
2023-07-05 16:37:51 -0400

    Add filter component

--- src/app/(proper_react)/ExposuresFilter.tsx
+++ src/app/(proper_react)/ExposuresFilter.tsx
@@ -2,26 +2,14 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import { Shell as ShellEl } from "./Shell";
-import { getL10n } from "../functions/server/l10n";
-import {
-  ExposureCard,
-  ExposureCardProps,
-} from "../components/client/ExposureCard";
-import styles from "./Dashboard.module.scss";
-import { DashboardTopBanner } from "./DashboardTopBanner";
-import { useL10n } from "../hooks/l10n";
+import styles from "./ExposuresFilter.module.scss";
 import { FilterIcon, QuestionMarkCircle } from "../components/server/Icons";
 import { ReactElement, useState } from "react";
 import { Modal } from "../components/client/Modal";
 import ModalImage from "../components/client/assets/modal-default-img.svg";
-import { ExposuresFilter } from "./ExposuresFilter";
+import { useL10n } from "../hooks/l10n";
 
-type DashboardProps = {
-  exposures: ExposureCardProps[];
-};
-
-export const Dashboard = (props: DashboardProps) => {
+export const ExposuresFilter = () => {
   const l10n = useL10n();
 
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -97,45 +85,66 @@ export const Dashboard = (props: DashboardProps) => {
   );
 
   return (
-    <ShellEl l10n={getL10n()} session={null}>
-      <div className={styles.container}>
-        <DashboardTopBanner type={"LetsFixDataContent"} chart={<></>} />
-        <section className={styles.exposuresArea}>
-          <div>
-            <h2>{l10n.getString("dashboard-exposures-area-headline")}</h2>
-            <p>
-              {l10n.getString("dashboard-exposures-area-description", {
-                // TODO: Use real user data
-                exposures_total_num: 90,
-                data_breach_total_num: 15,
-                data_broker_total_num: 75,
-              })}
-            </p>
-          </div>
-          <ExposuresFilter />
-          <ul className={styles.exposureList}>
-            {props.exposures.map((exposure, index) => (
-              <li key={index} className={styles.exposureListItem}>
-                <ExposureCard {...exposure} />
-              </li>
-            ))}
-          </ul>
-        </section>
-
-        {isModalOpen && (
-          <Modal
-            isOpen={isModalOpen}
-            onClose={handleClose}
-            image={ModalImage}
-            headline={modalTitle}
-            body={modalContent}
-            cta={{
-              content: l10n.getString("modal-cta-ok"),
-              link: handleClose,
-            }}
-          />
-        )}
+    <>
+      <div className={styles.filterHeaderWrapper}>
+        <ul className={styles.filterHeaderList}>
+          <li className={styles.hideOnMobile}>
+            <button
+              aria-label={l10n.getString("modal-open-filter-settings-alt")}
+              onClick={
+                () => setIsFilterPopupOpen(true) // TODO: Build filter popup
+              }
+            >
+              <FilterIcon width="16" height="16" alt={""} />
+            </button>
+            {l10n.getString("dashboard-exposures-filter")}
+          </li>
+          <li>{l10n.getString("dashboard-exposures-filter-company")}</li>
+          <li className={styles.hideOnMobile}>
+            {l10n.getString("dashboard-exposures-filter-exposure-type")}
+            <button
+              aria-label={l10n.getString("modal-open-alt")}
+              onClick={showModalContentExposureType}
+            >
+              <QuestionMarkCircle
+                width="15"
+                height="15"
+                alt={l10n.getString("modal-open-alt")}
+              />
+            </button>
+          </li>
+          <li className={styles.hideOnMobile}>
+            {l10n.getString("dashboard-exposures-filter-date-found")}
+          </li>
+          <li>
+            {l10n.getString("dashboard-exposures-filter-status")}
+            <button
+              aria-label={l10n.getString("modal-open-alt")}
+              onClick={showModalContentStatus}
+            >
+              <QuestionMarkCircle
+                width="15"
+                height="15"
+                alt={l10n.getString("modal-open-alt")}
+              />
+            </button>
+          </li>
+        </ul>
+        <div className={styles.rightSpace}></div>
       </div>
-    </ShellEl>
+      {isModalOpen && (
+        <Modal
+          isOpen={isModalOpen}
+          onClose={handleClose}
+          image={ModalImage}
+          headline={modalTitle}
+          body={modalContent}
+          cta={{
+            content: l10n.getString("modal-cta-ok"),
+            link: handleClose,
+          }}
+        />
+      )}
+    </>
   );
 };
--- src/app/components/server/Icons.tsx
+++ src/app/components/server/Icons.tsx
@@ -304,3 +304,29 @@ export const BentoIcon = (
     </svg>
   );
 };
+
+// Link to icon: https://www.figma.com/file/olFHozlwrdYlCkZaG4B262/Nebula-Design-System-V2-(WIP)?type=design&node-id=109-1405&mode=design&t=WbRVICi1MjCbri9t-4
+export const FilterIcon = (
+  props: SVGProps<SVGSVGElement> & { alt?: string }
+) => {
+  return (
+    <svg
+      width="18"
+      height="12"
+      viewBox="0 0 20 12"
+      role="img"
+      aria-label={props.alt}
+      aria-hidden={props.alt === ""}
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+      className={`${props.className ?? ""} ${styles.colorifyFill}`}
+    >
+      <title>{props.alt}</title>
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M8 12H10C10.55 12 11 11.55 11 11C11 10.45 10.55 10 10 10H8C7.45 10 7 10.45 7 11C7 11.55 7.45 12 8 12ZM0 1C0 1.55 0.45 2 1 2H17C17.55 2 18 1.55 18 1C18 0.45 17.55 0 17 0H1C0.45 0 0 0.45 0 1ZM4 7H14C14.55 7 15 6.55 15 6C15 5.45 14.55 5 14 5H4C3.45 5 3 5.45 3 6C3 6.55 3.45 7 4 7Z"
+      />
+    </svg>
+  );
+};
