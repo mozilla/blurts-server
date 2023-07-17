b7d4743c434b137c2d5d3961862b2dfe741dafcc
Robert Helmer <rhelmer@mozilla.com>
2023-07-17 10:27:11 -0700

    correct type that ScanResults is an array

--- src/db/tables/onerep_scans.ts
+++ src/db/tables/onerep_scans.ts
@@ -23,7 +23,11 @@ async function getOnerepScanResults(
 async function getLatestOnerepScan(
   onerepProfileId: number
 ): Promise<
-  { created_at: number; updated_at: number; onerep_scan_results: ScanResult }[]
+  {
+    created_at: number;
+    updated_at: number;
+    onerep_scan_results: ScanResult[];
+  }[]
 > {
   return (await knex("onerep_scans")
     .select("created_at", "updated_at", "onerep_scan_results")
@@ -33,7 +37,7 @@ async function getLatestOnerepScan(
     {
       created_at: number;
       updated_at: number;
-      onerep_scan_results: ScanResult;
+      onerep_scan_results: ScanResult[];
     }[]
   >;
 }
