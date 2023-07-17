d01cd8a1ba88065359e89afd7480b1b8274e7a2f
Florian Zia <zia.florian@gmail.com>
2023-07-17 11:58:10 +0200

    chore: Remove use-client

--- src/app/(proper_react)/redesign/(authenticated)/user/dashboard/layout.tsx
+++ src/app/(proper_react)/redesign/(authenticated)/user/dashboard/layout.tsx
@@ -2,8 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-"use-client";
-
 import { ReactNode } from "react";
 import { getServerSession } from "next-auth";
 import { getL10n, getL10nBundles } from "../../../../../functions/server/l10n";
