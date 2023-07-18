266b4cf8e88d6ab2a040c77cea4641caa9d28c56
Robert Helmer <rhelmer@mozilla.com>
2023-07-18 10:47:31 -0700

    add react server functions for feature flags

---
+++ src/app/functions/server/featureFlags.ts
@@ -0,0 +1,25 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import {
+  FeatureFlag,
+  getFeatureFlagByName,
+} from "../../../db/tables/featureFlags";
+
+export async function getFlag(name: string): Promise<FeatureFlag> {
+  const data = await getFeatureFlagByName(name);
+  const flag: FeatureFlag = {
+    name: data.name,
+    isEnabled: data.is_enabled,
+    description: data.description,
+    dependencies: data.dependencies,
+    allowList: data.allow_list,
+    waitList: data.wait_list,
+    expiredAt: data.expired_at,
+    deletedAt: data.deleted_at,
+    owner: data.owner,
+  };
+
+  return flag;
+}
