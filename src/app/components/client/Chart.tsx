ad9e281c2962067126c3d9a6271ac4c7e587fc0c
Kaitlyn <kandres@mozilla.com>
2023-07-19 10:18:01 -0400

    add about exposure num modal

--- src/app/components/client/Chart.tsx
+++ src/app/components/client/Chart.tsx
@@ -7,6 +7,14 @@
 import { CSSProperties } from "react";
 import { useL10n } from "../../hooks/l10n";
 import styles from "./Chart.module.scss";
+import { QuestionMarkCircle } from "../server/Icons";
+import { useOverlayTrigger } from "react-aria";
+import { useOverlayTriggerState } from "react-stately";
+import { Button } from "../server/Button";
+import { ModalOverlay } from "./dialog/ModalOverlay";
+import { Dialog } from "./dialog/Dialog";
+import ModalImage from "../client/assets/modal-default-img.svg";
+import Image from "next/image";
 
 export type Props = {
   data: Array<[string, number]>;
@@ -14,6 +22,13 @@ export type Props = {
 };
 export const DoughnutChart = (props: Props) => {
   const l10n = useL10n();
+
+  const explainerDialogState = useOverlayTriggerState({});
+  const explainerDialogTrigger = useOverlayTrigger(
+    { type: "dialog" },
+    explainerDialogState
+  );
+
   const sumOfFixedExposures = props.data.reduce(
     (total, [_label, num]) => total + num,
     0
@@ -59,88 +74,137 @@ export const DoughnutChart = (props: Props) => {
     );
   });
 
-  return (
-    <figure className={styles.chartContainer}>
-      <div className={styles.chartAndLegendWrapper}>
-        <svg
-          // https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Roles/img_role#svg_and_roleimg
-          role="img"
-          aria-label={l10n
-            .getString("exposure-chart-heading", {
-              nr: sumOfFixedExposures,
-            })
-            .replace("<nr>", "")
-            .replace("</nr>", "")
-            .replace("<label>", "")
-            .replace("</label>", "")}
-          viewBox={`0 0 ${diameter} ${diameter}`}
-          className={styles.chart}
+  const modalContent = (
+    <div className={styles.modalBodyContent}>
+      <p>{l10n.getString("modal-active-number-of-exposures-part-one")}</p>
+      <p>{l10n.getString("modal-active-number-of-exposures-part-two")}</p>
+      <p>{l10n.getString("modal-active-number-of-exposures-part-three")}</p>
+      <div className={styles.confirmButtonWrapper}>
+        <Button
+          variant="primary"
+          onClick={() => explainerDialogState.close()}
+          autoFocus={true}
+          className={styles.startButton}
         >
-          <circle
-            cx={diameter / 2}
-            cy={diameter / 2}
-            r={radius}
-            fill="none"
-            strokeWidth={ringWidth}
-            className={styles.gutter}
-          />
-          {slices}
-          {l10n.getFragment("exposure-chart-heading", {
-            elems: {
-              nr: (
-                <text
-                  className={styles.headingNr}
-                  fontSize={headingNumberSize}
-                  x={diameter / 2}
-                  y={diameter / 2 - headingGap / 2}
-                  textAnchor="middle"
-                />
-              ),
-              label: (
-                <text
-                  className={styles.headingLabel}
-                  fontSize={headingLabelSize}
-                  x={diameter / 2}
-                  y={diameter / 2 + headingLabelSize + headingGap / 2}
-                  textAnchor="middle"
-                />
-              ),
-            },
-            vars: { nr: sumOfFixedExposures },
-          })}
-        </svg>
-        <div className={styles.legend}>
-          <table>
-            <thead>
-              <tr>
-                {/* The first column contains the chart colour,
-                    which is irrelevant to screen readers. */}
-                <td aria-hidden={true} />
-                <th>{l10n.getString("exposure-chart-legend-heading-type")}</th>
-                <th>{l10n.getString("exposure-chart-legend-heading-nr")}</th>
-              </tr>
-            </thead>
-            <tbody>
-              {props.data.map(([label, num]) => (
-                <tr key={label}>
-                  <td aria-hidden={true}>
-                    <svg viewBox="0 0 10 10">
-                      <rect rx={2} width="10" height="10" />
-                    </svg>
-                  </td>
-                  <td>{label}</td>
-                  <td>
-                    {l10n.getString("exposure-chart-legend-value-nr", {
-                      nr: num,
-                    })}
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
+          {l10n.getString("modal-cta-ok")}
+        </Button>
       </div>
-      <figcaption>{l10n.getString("exposure-chart-caption")}</figcaption>
-    </figure>
+    </div>
+  );
+
+  return (
+    <>
+      <figure className={styles.chartContainer}>
+        <div className={styles.chartAndLegendWrapper}>
+          <svg
+            // https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Roles/img_role#svg_and_roleimg
+            role="img"
+            aria-label={l10n
+              .getString("exposure-chart-heading", {
+                nr: sumOfFixedExposures,
+              })
+              .replace("<nr>", "")
+              .replace("</nr>", "")
+              .replace("<label>", "")
+              .replace("</label>", "")}
+            viewBox={`0 0 ${diameter} ${diameter}`}
+            className={styles.chart}
+          >
+            <circle
+              cx={diameter / 2}
+              cy={diameter / 2}
+              r={radius}
+              fill="none"
+              strokeWidth={ringWidth}
+              className={styles.gutter}
+            />
+            {slices}
+            {l10n.getFragment("exposure-chart-heading", {
+              elems: {
+                nr: (
+                  <text
+                    className={styles.headingNr}
+                    fontSize={headingNumberSize}
+                    x={diameter / 2}
+                    y={diameter / 2 - headingGap / 2}
+                    textAnchor="middle"
+                  />
+                ),
+                label: (
+                  <text
+                    className={styles.headingLabel}
+                    fontSize={headingLabelSize}
+                    x={diameter / 2}
+                    y={diameter / 2 + headingLabelSize + headingGap / 2}
+                    textAnchor="middle"
+                  />
+                ),
+              },
+              vars: { nr: sumOfFixedExposures },
+            })}
+          </svg>
+          <div className={styles.legend}>
+            <table>
+              <thead>
+                <tr>
+                  {/* The first column contains the chart colour,
+                      which is irrelevant to screen readers. */}
+                  <td aria-hidden={true} />
+                  <th>
+                    {l10n.getString("exposure-chart-legend-heading-type")}
+                  </th>
+                  <th>{l10n.getString("exposure-chart-legend-heading-nr")}</th>
+                </tr>
+              </thead>
+              <tbody>
+                {props.data.map(([label, num]) => (
+                  <tr key={label}>
+                    <td aria-hidden={true}>
+                      <svg viewBox="0 0 10 10">
+                        <rect rx={2} width="10" height="10" />
+                      </svg>
+                    </td>
+                    <td>{label}</td>
+                    <td>
+                      {l10n.getString("exposure-chart-legend-value-nr", {
+                        nr: num,
+                      })}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+        <figcaption>
+          {l10n.getString("exposure-chart-caption")}
+          <button
+            aria-label={l10n.getString("modal-open-alt")}
+            onClick={() => explainerDialogState.open()}
+          >
+            <QuestionMarkCircle
+              width="15"
+              height="15"
+              alt={l10n.getString("modal-open-alt")}
+            />
+          </button>
+        </figcaption>
+      </figure>
+      {explainerDialogState.isOpen && (
+        <ModalOverlay
+          state={explainerDialogState}
+          {...explainerDialogTrigger.overlayProps}
+          isDismissable={true}
+        >
+          <Dialog
+            title={l10n.getString("modal-active-number-of-exposures-title")}
+            illustration={<Image src={ModalImage} alt="" />}
+            onDismiss={() => explainerDialogState.close()}
+          >
+            {modalContent}
+          </Dialog>
+        </ModalOverlay>
+      )}
+    </>
   );
 };
