10dd5d08d31812d1d682f67dea65a28a39deaa69
Robert Helmer <rhelmer@mozilla.com>
2023-07-24 08:33:05 -0700

    move components into subdir

---
+++ src/app/(proper_react)/redesign/(authenticated)/admin/feature-flags/components/AddFeatureFlag.tsx
@@ -0,0 +1,111 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+"use client";
+
+import { FormEventHandler } from "react";
+import { useRouter } from "next/navigation";
+
+// TODO investigate way to harmonize this with `FeatureFlag` type
+interface FeatureFlagElement {
+  name: {
+    value: string;
+  };
+  isEnabled: {
+    value: string;
+  };
+  dependencies: {
+    value: string;
+  };
+  allowList: {
+    value: string;
+  };
+  waitList: {
+    value: string;
+  };
+  owner: {
+    value: string;
+  };
+}
+
+export const AddFeatureFlag = () => {
+  const router = useRouter();
+
+  const handleSubmit = (
+    event: FormEventHandler<HTMLFormElement> & {
+      target: FeatureFlagElement;
+    }
+  ) => {
+    event.preventDefault();
+
+    const data = {
+      name: event.target.name.value,
+      isEnabled: event.target.isEnabled ? true : false,
+      dependencies: event.target.dependencies.value.split(","),
+      allowList: event.target.allowList.value.split(","),
+      waitList: event.target.waitList.value.split(","),
+      owner: event.target.owner.value,
+    };
+
+    const endpoint = "/api/v1/admin/feature-flags" as string;
+    console.debug("body:", data);
+
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    };
+
+    fetch(endpoint, options)
+      .then((response) => {
+        if (response.ok) {
+          router.refresh();
+        }
+      })
+      .catch((ex) => console.error(ex));
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Enabled</th>
+            <th>Dependencies</th>
+            <th>Allow List</th>
+            <th>Wait List</th>
+            <th>Owner</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <input id="name" name="name" />
+            </td>
+            <td>
+              <input id="isEnabled" name="isEnabled" type="checkbox" />
+            </td>
+            <td>
+              <input id="dependencies" name="dependencies" />
+            </td>
+            <td>
+              <input id="allowList" name="allowList" />
+            </td>
+            <td>
+              <input id="waitList" name="waitList" />
+            </td>
+            <td>
+              <input id="owner" name="owner" />
+            </td>
+            <td>
+              <button name="Add">Add</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </form>
+  );
+};
---
+++ src/app/(proper_react)/redesign/(authenticated)/admin/feature-flags/components/ModifyInputField.tsx
@@ -0,0 +1,46 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+"use client";
+import { useRouter } from "next/navigation";
+
+export const ModifyInputField = (props: {
+  id: string | undefined;
+  name: string | undefined;
+  defaultValue: string | undefined;
+}) => {
+  const router = useRouter();
+
+  // @ts-ignore FIXME determine correct event type
+  const handleChange = (event) => {
+    const id = event.target.id;
+    const name = event.target.name;
+    const value = event.target.value;
+
+    const endpoint = `/api/v1/admin/feature-flags/${name as string}`;
+
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ id, name, value }),
+    };
+    fetch(endpoint, options)
+      .then((response) => {
+        if (response.ok) {
+          router.refresh();
+        }
+      })
+      .catch((ex) => console.error(ex));
+  };
+  return (
+    <input
+      id={props.id}
+      name={props.name}
+      defaultValue={props.defaultValue}
+      onBlur={handleChange}
+    />
+  );
+};
