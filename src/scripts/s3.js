ac1098f40bfa62be99b3590d33183fd57255b476
Joey Zhou <jozhou@mozilla.com>
2023-07-28 14:00:06 -0700

    update s3 with dotenv

--- src/scripts/s3.js
+++ src/scripts/s3.js
@@ -1,5 +1,6 @@
 import { Upload } from "@aws-sdk/lib-storage";
 import { S3 } from "@aws-sdk/client-s3";
+import 'dotenv/config'
 
 const accessKeyId = process.env.AWS_ACCESS_KEY_ID || '';
 const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY || '';
