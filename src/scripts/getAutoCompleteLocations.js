f0c41fcb48236da9b374a4a86ed7e062b38bbbaf
Joey Zhou <jozhou@mozilla.com>
2023-07-31 15:25:02 -0700

    lint

--- src/scripts/getAutoCompleteLocations.js
+++ src/scripts/getAutoCompleteLocations.js
@@ -7,9 +7,9 @@ import "dotenv/config"
 const stream = createWriteStream(resolve('./locationAutocompleteData.json'));
 try {
   const fetchUrl = `https://s3.amazonaws.com/${process.env.S3_BUCKET}/autocomplete/locationAutocompleteData.json`;
-  console.debug({fetchUrl})
-const { body } = await fetch(fetchUrl);
-await finished(Readable.fromWeb(body).pipe(stream));
-} catch(e) {
+  console.debug({ fetchUrl })
+  const { body } = await fetch(fetchUrl);
+  await finished(Readable.fromWeb(body).pipe(stream));
+} catch (e) {
   console.error(e)
 }
