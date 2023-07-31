0e4d03603fce94ddb6bd9b008bb04416fa57ec04
Joey Zhou <jozhou@mozilla.com>
2023-07-31 16:09:49 -0700

    do not fetch when .json exists

--- src/scripts/getAutoCompleteLocations.js
+++ src/scripts/getAutoCompleteLocations.js
@@ -1,15 +1,20 @@
-import { resolve } from 'node:path'
-import { createWriteStream } from 'fs';
+import { createWriteStream, existsSync } from 'fs';
 import { Readable } from 'stream';
 import { finished } from 'stream/promises';
 import "dotenv/config"
 
-const stream = createWriteStream(resolve('./locationAutocompleteData.json'));
-try {
-  const fetchUrl = `https://s3.amazonaws.com/${process.env.S3_BUCKET}/autocomplete/locationAutocompleteData.json`;
-  console.debug({ fetchUrl })
-  const { body } = await fetch(fetchUrl);
-  await finished(Readable.fromWeb(body).pipe(stream));
-} catch (e) {
-  console.error(e)
+const dataPath = "./locationAutocompleteData.json";
+
+if (!existsSync(dataPath)) {
+  const stream = createWriteStream(dataPath);
+  try {
+    const fetchUrl = `https://s3.amazonaws.com/${process.env.S3_BUCKET}/autocomplete/locationAutocompleteData.json`;
+    console.debug({ fetchUrl })
+    const { body } = await fetch(fetchUrl);
+    await finished(Readable.fromWeb(body).pipe(stream));
+  } catch (e) {
+    console.error(e)
+  }
+} else {
+  console.log('file already exists: ', dataPath)
 }
