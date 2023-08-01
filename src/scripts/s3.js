024406635e1fee4d0daacf5b8a6f7a5fbccd6238
Joey Zhou <jozhou@mozilla.com>
2023-08-01 13:37:33 -0700

    copyright headers

--- src/scripts/s3.js
+++ src/scripts/s3.js
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+ 
 import { Upload } from "@aws-sdk/lib-storage";
 import { S3 } from "@aws-sdk/client-s3";
 import "dotenv/config"
