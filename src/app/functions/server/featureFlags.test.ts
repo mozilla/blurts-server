d794be2e50a8d499008757341ba9c6030d5e1f15
Robert Helmer <rhelmer@mozilla.com>
2023-08-07 18:23:52 -0700

    correct types in unit test, and test expired/deleted conditions

--- src/app/functions/server/featureFlags.test.ts
+++ src/app/functions/server/featureFlags.test.ts
@@ -4,7 +4,11 @@
 
 // import { it, expect, jest } from "@jest/globals";
 
-import { getFeatureFlagByName } from "../../../db/tables/featureFlags";
+import {
+  getFeatureFlagByName,
+  FeatureFlagsEnabled,
+} from "../../../db/tables/featureFlags";
+
 jest.mock("../../../db/tables/featureFlags");
 
 jest.mock("next-auth", () => ({
@@ -27,32 +31,40 @@ beforeEach(() => {
   jest.resetModules();
 });
 
-it("throws if there is no valid session", async () => {
-  await expect(isFlagEnabled("testFlag")).resolves.toBe(false);
-});
-
-it("returns false if flag not provided or nonexistent", async () => {
-  // @ts-expect-error TS should prevent, but let's test at runtime anyway.
-  await expect(isFlagEnabled()).rejects.toThrowError(
-    "No name provided to isFlagEnabled"
-  );
-
-  await expect(isFlagEnabled("invalidFlag")).resolves.toBe(false);
-});
-
 it("returns flag state if exists", async () => {
-  //@ts-ignore jest mock
-  getFeatureFlagByName.mockImplementation((name) => {
+  (getFeatureFlagByName as jest.Mock).mockImplementation((name) => {
     if (name === "validFalseFlag") {
       return { name: "validFalseFlag", is_enabled: false };
     } else if (name === "validTrueFlag") {
       return { name: "validTrueFlag", is_enabled: true };
+    } else if (name === "validTrueExpiredFlag") {
+      return {
+        name: "validTrueExpiredFlag",
+        is_enabled: true,
+        expired_at: Date.now(),
+      };
+    } else if (name === "validTrueDeletedFlag") {
+      return {
+        name: "validTrueDeletedFlag",
+        is_enabled: true,
+        deleted_at: Date.now(),
+      };
     } else {
       console.debug("flag name:", name);
       fail("Unknown flag passed to mock");
     }
   });
 
-  await expect(isFlagEnabled("validFalseFlag")).resolves.toBe(false);
-  await expect(isFlagEnabled("validTrueFlag")).resolves.toBe(true);
+  await expect(
+    isFlagEnabled("validFalseFlag" as FeatureFlagsEnabled)
+  ).resolves.toBe(false);
+  await expect(
+    isFlagEnabled("validTrueFlag" as FeatureFlagsEnabled)
+  ).resolves.toBe(true);
+  await expect(
+    isFlagEnabled("validTrueExpiredFlag" as FeatureFlagsEnabled)
+  ).resolves.toBe(false);
+  await expect(
+    isFlagEnabled("validTrueDeletedFlag" as FeatureFlagsEnabled)
+  ).resolves.toBe(false);
 });
