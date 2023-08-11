9ac5ee22b06665b33722920e06450df36736747e
Florian Zia <zia.florian@gmail.com>
2023-08-11 17:30:21 +0200

    chore: Add unit test for dashboard tabs

--- src/app/(proper_react)/redesign/(authenticated)/user/dashboard/Dashboard.test.tsx
+++ src/app/(proper_react)/redesign/(authenticated)/user/dashboard/Dashboard.test.tsx
@@ -3,7 +3,8 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 import { it, expect } from "@jest/globals";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { composeStory } from "@storybook/react";
 import { axe } from "jest-axe";
 import Meta, {
@@ -26,4 +27,26 @@ it("passes the axe accessibility test suite", async () => {
   const ComposedDashboard = composeStory(DashboardWithoutScan, Meta);
   const { container } = render(<ComposedDashboard />);
   expect(await axe(container)).toHaveNoViolations();
+});
+
+it("switches between tab panels", async () => {
+  const user = userEvent.setup();
+  const ComposedDashboard = composeStory(DashboardWithoutScan, Meta);
+  render(<ComposedDashboard />);
+
+  const tabActionNeededTrigger = screen.getByRole("tab", {
+    name: "Action needed",
+  });
+  const tabActionNeededTriggerAriaSelected =
+    tabActionNeededTrigger.getAttribute("aria-selected");
+  expect(tabActionNeededTriggerAriaSelected).toBe("true");
+  const tabFixedTrigger = screen.getByRole("tab", {
+    name: "Fixed",
+  });
+  const tabFixedTriggerAriaSelected =
+    tabFixedTrigger.getAttribute("aria-selected");
+  expect(tabFixedTriggerAriaSelected).toBe("false");
+  await user.click(tabFixedTrigger);
+  expect(tabFixedTriggerAriaSelected).toBe("true");
+  expect(tabActionNeededTriggerAriaSelected).toBe("false");
 });
