0eb4062f1b53cd423c8eba17e65e8696dffc643d
Robert Helmer <rhelmer@mozilla.com>
2023-08-14 15:42:41 -0700

    MNTOR-1948 - add sample UI for testing

---
+++ src/app/(proper_react)/redesign/(authenticated)/user/dashboard/subscribed/page.tsx
@@ -0,0 +1,75 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import { getServerSession } from "next-auth";
+import { getOnerepProfileId } from "../../../../../../../db/tables/subscribers";
+import { authOptions } from "../../../../../../api/utils/auth";
+import {
+  activateProfile,
+  getAllScanResults,
+  isEligibleForPremium,
+  optoutProfile,
+} from "../../../../../../functions/server/onerep";
+import {
+  getLatestOnerepScan,
+  setOnerepScanResults,
+} from "../../../../../../../db/tables/onerep_scans";
+
+export default async function Subscribed() {
+  if (!(await isEligibleForPremium())) {
+    throw new Error("Not eligible for premium");
+  }
+
+  const session = await getServerSession(authOptions);
+  if (!session || !session.user || !session.user.subscriber) {
+    throw new Error("No session");
+  }
+
+  const result = await getOnerepProfileId(session.user.subscriber.id);
+  const profileId = result[0]["onerep_profile_id"] as number;
+
+  try {
+    await activateProfile(profileId);
+    await optoutProfile(profileId);
+  } catch (ex) {
+    console.debug(ex); // TODO handle
+  }
+
+  const dev =
+    process.env.NODE_ENV === "development" || process.env.APP_ENV === "heroku";
+  const latestScan = await getLatestOnerepScan(profileId);
+  if (!latestScan) {
+    throw new Error("Must have performed manual scan");
+  }
+
+  const scans = await getAllScanResults(profileId);
+
+  // In dev mode, record scans every time this page is reloaded.
+  // The webhoook does this in production.
+  if (dev) {
+    await setOnerepScanResults(
+      profileId,
+      latestScan.onerep_scan_id,
+      { data: scans },
+      "initial"
+    );
+  }
+
+  return (
+    <div>
+      <div>
+        <h3>You are now subscribed</h3>
+        {dev ? (
+          <div>
+            <h3>Dev mode enabled</h3>
+            <p>Reload this page to update scan</p>
+            <pre>{JSON.stringify(scans, null, 2)}</pre>
+          </div>
+        ) : (
+          ""
+        )}
+      </div>
+    </div>
+  );
+}
---
+++ src/app/(proper_react)/redesign/(authenticated)/user/dashboard/unsubscribed/page.tsx
@@ -0,0 +1,39 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import { getServerSession } from "next-auth";
+import { getOnerepProfileId } from "../../../../../../../db/tables/subscribers";
+import { authOptions } from "../../../../../../api/utils/auth";
+import {
+  deactivateProfile,
+  isEligibleForPremium,
+} from "../../../../../../functions/server/onerep";
+
+export default async function Unsubscribed() {
+  if (!(await isEligibleForPremium())) {
+    throw new Error("Not eligible for premium");
+  }
+
+  const session = await getServerSession(authOptions);
+  if (!session || !session.user || !session.user.subscriber) {
+    throw new Error("No session");
+  }
+
+  const result = await getOnerepProfileId(session.user.subscriber.id);
+  const profileId = result[0]["onerep_profile_id"] as number;
+
+  try {
+    await deactivateProfile(profileId);
+  } catch (ex) {
+    console.debug(ex); // TODO handle
+  }
+
+  return (
+    <div>
+      <div>
+        <h3>You are now unsubscribed</h3>
+      </div>
+    </div>
+  );
+}
