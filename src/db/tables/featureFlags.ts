7b9095999e9be30c09fb62acf0f12612147dafce
Joey Zhou <jozhou@mozilla.com>
2023-08-16 17:04:58 -0700

    var

--- src/db/tables/featureFlags.ts
+++ src/db/tables/featureFlags.ts
@@ -42,8 +42,8 @@ async function addFeatureFlag(flag: FeatureFlag) {
     is_enabled: flag.isEnabled,
     description: flag.description,
     dependencies: flag.dependencies,
-    allow_list: flag.allowList?.map((email) => email.trim()).filter((e) => !!e),
-    wait_list: flag.waitList?.map((email) => email.trim()).filter((e) => !!e),
+    allow_list: flag.allowList?.map((e) => e.trim()).filter((e) => !!e),
+    wait_list: flag.waitList?.map((e) => e.trim()).filter((e) => !!e),
     expired_at: flag.expiredAt,
     owner: flag.owner,
   };
@@ -96,7 +96,7 @@ async function updateOwner(name: string, owner: string) {
 }
 
 async function updateAllowList(name: string, allowList: string[]) {
-  allowList = allowList.map((email) => email.trim()).filter((e) => !!e);
+  allowList = allowList.map((e) => e.trim()).filter((e) => !!e);
   log.info("updateAllowList", { name, allowList });
   const res = await knex("feature_flags")
     .where("name", name)
@@ -112,7 +112,7 @@ async function updateAllowList(name: string, allowList: string[]) {
 }
 
 async function updateWaitList(name: string, waitList: string[]) {
-  waitList = waitList.map((email) => email.trim()).filter((e) => !!e);
+  waitList = waitList.map((e) => e.trim()).filter((e) => !!e);
   log.info("updateWaitList", { name, waitList });
   const res = await knex("feature_flags")
     .where("name", name)
