3a23e08ad6832b1642894771a43ed02136a1ae39
Robert Helmer <rhelmer@mozilla.com>
2023-08-22 07:05:46 -0700

    fix: filter on en-US explicitly (#3329)
    * fix: filter on en-US explicitly
    
    ---------
    
    Co-authored-by: Vincent <Vinnl@users.noreply.github.com>
    Co-authored-by: Vincent <git@vincenttunru.com>

--- src/app/(nextjs_migration)/layout.tsx
+++ src/app/(nextjs_migration)/layout.tsx
@@ -47,7 +47,9 @@ export default async function MigrationLayout({
       {falseDoorFlag &&
         waitlistLink &&
         countryCode.toLowerCase() === "us" &&
-        locale === "en" && <HandleFalseDoorTest link={waitlistLink} />}
+        ["en", "en-GB", "en-US", "en-CA"].includes(locale) && (
+          <HandleFalseDoorTest link={waitlistLink} />
+        )}
     </L10nProvider>
   );
 }
