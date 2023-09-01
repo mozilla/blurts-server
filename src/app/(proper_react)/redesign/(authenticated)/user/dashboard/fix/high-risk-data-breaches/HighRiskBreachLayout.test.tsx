bf0c01889cfe265a6bf038b7ab5ebfed4df4b035
Kaitlyn <kandres@mozilla.com>
2023-09-01 12:56:52 -0400

    add more test

--- src/app/(proper_react)/redesign/(authenticated)/user/dashboard/fix/high-risk-data-breaches/HighRiskBreachLayout.test.tsx
+++ src/app/(proper_react)/redesign/(authenticated)/user/dashboard/fix/high-risk-data-breaches/HighRiskBreachLayout.test.tsx
@@ -6,10 +6,40 @@ import { render } from "@testing-library/react";
 import { it, expect } from "@jest/globals";
 import { composeStory } from "@storybook/react";
 import { axe } from "jest-axe";
-import Meta, { CreditCard } from "./HighRiskBreachLayout.stories";
+import Meta, {
+  CreditCard,
+  BankAccount,
+  SSN,
+  PIN,
+  None,
+} from "./HighRiskBreachLayout.stories";
 
 it("high risk data breach component passes the axe accessiblity test suite", async () => {
   const ComposedHighRiskDataBreachComponent = composeStory(CreditCard, Meta);
+  const { container } = render(<ComposedHighRiskDataBreachComponent />);
+  expect(await axe(container)).toHaveNoViolations();
+});
+
+it("bank account data breach component passes the axe accessiblity test suite", async () => {
+  const ComposedHighRiskDataBreachComponent = composeStory(BankAccount, Meta);
+  const { container } = render(<ComposedHighRiskDataBreachComponent />);
+  expect(await axe(container)).toHaveNoViolations();
+});
+
+it("SSN data breach component passes the axe accessiblity test suite", async () => {
+  const ComposedHighRiskDataBreachComponent = composeStory(SSN, Meta);
+  const { container } = render(<ComposedHighRiskDataBreachComponent />);
+  expect(await axe(container)).toHaveNoViolations();
+});
+
+it("PIN data breach component passes the axe accessiblity test suite", async () => {
+  const ComposedHighRiskDataBreachComponent = composeStory(PIN, Meta);
+  const { container } = render(<ComposedHighRiskDataBreachComponent />);
+  expect(await axe(container)).toHaveNoViolations();
+});
+
+it("Zero state breach component passes the axe accessiblity test suite", async () => {
+  const ComposedHighRiskDataBreachComponent = composeStory(None, Meta);
   const { container } = render(<ComposedHighRiskDataBreachComponent />);
   expect(await axe(container)).toHaveNoViolations();
 });
