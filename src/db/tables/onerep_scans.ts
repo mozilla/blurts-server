ae6cf2e38b33e56f5f3339b2dd09ae5d5b427a4f
Vincent <git@vincenttunru.com>
2023-09-08 19:12:31 +0200

    Add API to manually mark scan results as resolved

--- src/db/tables/onerep_scans.ts
+++ src/db/tables/onerep_scans.ts
@@ -103,6 +103,44 @@ async function addOnerepScanResults(
   });
 }
 
+async function isOnerepScanResultForSubscriber(params: {
+  onerepScanResultId: number;
+  subscriberId: number;
+}): Promise<boolean> {
+  const result = await knex("onerep_scan_results")
+    .innerJoin(
+      "onerep_scans",
+      "onerep_scan_results.onerep_scan_id",
+      "onerep_scans.onerep_scan_id"
+    )
+    .innerJoin(
+      "subscribers",
+      "onerep_scans.onerep_profile_id",
+      "subscribers.onerep_profile_id"
+    )
+    .where(
+      "onerep_scan_results.onerep_scan_result_id",
+      params.onerepScanResultId
+    )
+    .andWhere("subscribers.id", params.subscriberId)
+    .first("onerep_scan_result_id");
+
+  return typeof result?.onerep_scan_result_id === "number";
+}
+
+async function markOnerepScanResultAsResolved(
+  onerepScanResultId: number
+): Promise<void> {
+  await knex("onerep_scan_results")
+    .update({
+      manually_resolved: true,
+      // @ts-ignore knex.fn.now() results in it being set to a date,
+      // even if it's not typed as a JS date object:
+      updated_at: knex.fn.now(),
+    })
+    .where("onerep_scan_result_id", onerepScanResultId);
+}
+
 async function getScansCount(
   startDate: string,
   endDate: string,
@@ -120,4 +158,6 @@ export {
   setOnerepManualScan,
   addOnerepScanResults,
   getScansCount,
+  isOnerepScanResultForSubscriber,
+  markOnerepScanResultAsResolved,
 };
