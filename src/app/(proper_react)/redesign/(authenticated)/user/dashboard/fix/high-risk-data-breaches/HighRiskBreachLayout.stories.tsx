8eccdb63751388214ecbbce7b41517a68b47dc0d
Florian Zia <zia.florian@gmail.com>
2023-09-11 22:39:43 +0200

    chore: Adjust content in HighRiskBreachLayout story

--- src/app/(proper_react)/redesign/(authenticated)/user/dashboard/fix/high-risk-data-breaches/HighRiskBreachLayout.stories.tsx
+++ src/app/(proper_react)/redesign/(authenticated)/user/dashboard/fix/high-risk-data-breaches/HighRiskBreachLayout.stories.tsx
@@ -64,7 +64,7 @@ export const BankAccount: Story = {
   args: {
     pageData: {
       type: "bank-account",
-      title: "Credit card",
+      title: "Bank account",
       illustration: bankAccountIllustration,
       exposedData: guidedExperienceBreaches.highRisk.ssnBreaches,
       content,
