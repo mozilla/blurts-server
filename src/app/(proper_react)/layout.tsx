20bc2248c3ab7c8801f14ac7cbf42971dbc34790
Florian Zia <zia.florian@gmail.com>
2023-09-11 19:19:40 +0200

    feat: Record metrics for premium modal

--- src/app/(proper_react)/layout.tsx
+++ src/app/(proper_react)/layout.tsx
@@ -3,15 +3,22 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 import { ReactNode } from "react";
+import Script from "next/script";
 import { getL10nBundles, getLocale } from "../functions/server/l10n";
 import { L10nProvider } from "../../contextProviders/localization";
 import { ReactAriaI18nProvider } from "../../contextProviders/react-aria";
+import { getNonce } from "../(nextjs_migration)/functions/server/getNonce";
 
 export default function Layout({ children }: { children: ReactNode }) {
   const l10nBundles = getL10nBundles();
 
   return (
     <L10nProvider bundleSources={l10nBundles}>
+      <Script
+        type="module"
+        src="/nextjs_migration/client/js/analytics.js"
+        nonce={getNonce()}
+      />
       <ReactAriaI18nProvider locale={getLocale(l10nBundles)}>
         {children}
       </ReactAriaI18nProvider>
