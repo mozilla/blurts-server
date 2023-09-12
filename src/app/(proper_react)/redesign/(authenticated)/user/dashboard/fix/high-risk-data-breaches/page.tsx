9d3fe8f64f0e995fcd16c8f685da175db9e9f0fa
Kaitlyn <kandres@mozilla.com>
2023-09-12 16:26:30 -0400

    Show zero state high risk data breaches

--- src/app/(proper_react)/redesign/(authenticated)/user/dashboard/fix/high-risk-data-breaches/page.tsx
+++ src/app/(proper_react)/redesign/(authenticated)/user/dashboard/fix/high-risk-data-breaches/page.tsx
@@ -2,7 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import Link from "next/link";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 import { HighRiskBreachLayout } from "./HighRiskBreachLayout";
