8810f636cfab96fd333c85efd4f5cad0a8282d65
Joey Zhou <jozhou@mozilla.com>
2023-09-13 13:11:05 -0700

    add donut chart

--- src/app/(proper_react)/redesign/(authenticated)/user/dashboard/fix/data-broker-profiles/welcome-to-premium/page.tsx
+++ src/app/(proper_react)/redesign/(authenticated)/user/dashboard/fix/data-broker-profiles/welcome-to-premium/page.tsx
@@ -4,13 +4,14 @@
 
 import Link from "next/link";
 import buttonStyles from "../../../../../../../../components/server/button.module.scss";
-import styles from "../dataBrokerProfiles.module.scss";
+import styles from "./welcomeToPremium.module.scss";
 import { getL10n } from "../../../../../../../../functions/server/l10n";
 import { getLatestOnerepScan } from "../../../../../../../../../db/tables/onerep_scans";
 import { getServerSession } from "next-auth";
 import { authOptions } from "../../../../../../../../api/utils/auth";
 import { getOnerepProfileId } from "../../../../../../../../../db/tables/subscribers";
 import { redirect } from "next/navigation";
+import { DoughnutChart } from "../../../../../../../../components/client/Chart";
 
 export default async function WelcomeToPremium() {
   const l10n = getL10n();
@@ -26,47 +27,70 @@ export default async function WelcomeToPremium() {
   const scanResultItems = scanResult?.onerep_scan_results?.data ?? [];
   const countOfDataBrokerProfiles = scanResultItems.length;
 
+  const chartData: Array<[string, number]> = [
+    ["Exposure", 97],
+    ["Other", 3],
+  ];
+
   return (
-    <div>
-      <div className={styles.content}>
-        <h3>
-          {l10n.getString(
-            "welcome-to-premium-data-broker-profiles-title-part-one"
-          )}
-          <br />
-          {l10n.getString(
-            "welcome-to-premium-data-broker-profiles-title-part-two"
-          )}
-        </h3>
-        <p>
-          {l10n.getString(
-            "welcome-to-premium-data-broker-profiles-description-part-one",
-            {
-              profile_total_num: countOfDataBrokerProfiles,
-              exposure_reduction_percentage: 90,
-            }
-          )}
-        </p>
-        <p>
-          {l10n.getString(
-            "welcome-to-premium-data-broker-profiles-description-part-two"
-          )}
-        </p>
-        <p>
-          {l10n.getString(
-            "welcome-to-premium-data-broker-profiles-description-part-three"
-          )}
-        </p>
+    <>
+      <div>
+        <div className={`${styles.content}`}>
+          <h4>
+            {l10n.getString(
+              "welcome-to-premium-data-broker-profiles-title-part-one"
+            )}
+            <br />
+            {l10n.getString(
+              "welcome-to-premium-data-broker-profiles-title-part-two"
+            )}
+          </h4>
+          <p>
+            {l10n.getString(
+              "welcome-to-premium-data-broker-profiles-description-part-one",
+              {
+                profile_total_num: countOfDataBrokerProfiles,
+                exposure_reduction_percentage: 90,
+              }
+            )}
+          </p>
+          <p>
+            {l10n.getFragment(
+              "welcome-to-premium-data-broker-profiles-description-part-two",
+              {
+                elems: {
+                  link_to_info: (
+                    <a href="/" target="_blank" rel="noopener noreferrer" />
+                  ),
+                },
+              }
+            )}
+          </p>
+          <p>
+            {l10n.getString(
+              "welcome-to-premium-data-broker-profiles-description-part-three"
+            )}
+          </p>
+        </div>
+        <div className={styles.content}></div>
+        <div className={styles.buttonsWrapper}>
+          <Link
+            className={`${buttonStyles.button} ${buttonStyles.primary}`}
+            href="/redesign/user/dashboard/fix/high-risk-data-breaches"
+          >
+            {l10n.getString(
+              "welcome-to-premium-data-broker-profiles-cta-label"
+            )}
+          </Link>
+        </div>
       </div>
-      <div className={styles.content}></div>
-      <div className={styles.buttonsWrapper}>
-        <Link
-          className={`${buttonStyles.button} ${buttonStyles.primary}`}
-          href="/redesign/user/dashboard/fix/high-risk-data-breaches"
-        >
-          {l10n.getString("welcome-to-premium-data-broker-profiles-cta-label")}
-        </Link>
+      <div>
+        <DoughnutChart
+          hasRunScan={true}
+          data={chartData}
+          isEligibleForFreeScan={true}
+        />
       </div>
-    </div>
+    </>
   );
 }
