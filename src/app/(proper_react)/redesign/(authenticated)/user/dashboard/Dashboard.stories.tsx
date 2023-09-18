8ae402f784f8da60c0700a008ebe4ffbcf5f03c1
Vincent <git@vincenttunru.com>
2023-09-18 16:01:09 +0200

    Add labels and options for Storybook controls

--- src/app/(proper_react)/redesign/(authenticated)/user/dashboard/Dashboard.stories.tsx
+++ src/app/(proper_react)/redesign/(authenticated)/user/dashboard/Dashboard.stories.tsx
@@ -16,16 +16,27 @@ import {
 import { SubscriberBreach } from "../../../../../../utils/subscriberBreaches";
 import { LatestOnerepScanData } from "../../../../../../db/tables/onerep_scans";
 
+const brokerOptions = {
+  "no-scan": "No scan started",
+  empty: "No scan results",
+  unresolved: "With unresolved scan results",
+  resolved: "All scan results resolved",
+};
+const breachOptions = {
+  empty: "No data breaches",
+  unresolved: "With unresolved data breaches",
+  resolved: "All data breaches resolved",
+};
 type DashboardWrapperProps = (
   | {
       countryCode: "us";
-      brokers: "no-scan" | "empty" | "unresolved" | "resolved";
+      brokers: keyof typeof brokerOptions;
       premium: boolean;
     }
   | {
       countryCode: "nl";
     }
-) & { breaches: "empty" | "unresolved" | "resolved" };
+) & { breaches: keyof typeof breachOptions };
 const DashboardWrapper = (props: DashboardWrapperProps) => {
   const mockedResolvedBreach: SubscriberBreach = createRandomBreach({
     dataClasses: [
@@ -133,6 +144,23 @@ const DashboardWrapper = (props: DashboardWrapperProps) => {
 const meta: Meta<typeof DashboardWrapper> = {
   title: "Pages/Dashboard",
   component: DashboardWrapper,
+  argTypes: {
+    brokers: {
+      options: Object.keys(brokerOptions),
+      description: "Scan results",
+      control: {
+        type: "radio",
+        labels: brokerOptions,
+      },
+    },
+    breaches: {
+      options: Object.keys(breachOptions),
+      control: {
+        type: "radio",
+        labels: breachOptions,
+      },
+    },
+  },
 };
 export default meta;
 type Story = StoryObj<typeof DashboardWrapper>;
