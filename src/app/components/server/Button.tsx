bf03f55b68866c546984a829fae0014d624ebfb5
Vincent <git@vincenttunru.com>
2023-09-21 16:16:54 +0200

    fixup! Remove re-use of CSS Modules across components

--- src/app/components/server/Button.tsx
+++ src/app/components/server/Button.tsx
@@ -2,6 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+"use client";
+
 import { ComponentProps, ReactNode, useRef } from "react";
 import Link from "next/link";
 import styles from "./button.module.scss";
