bc1f68d85f2061105aa98eab6eef3e70661f6b25
Vincent <git@vincenttunru.com>
2023-09-21 15:45:09 +0200

    Store scan status in our database
    The website has to display different content depending on whether
    the scan is already finished, or still in progress, so we need to
    keep track of that too.

---
+++ src/db/migrations/20230921132056_add_status_column.js
@@ -0,0 +1,27 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function up(knex) {
+  await knex.schema.alterTable("onerep_scans", table => {
+    table.string("onerep_scan_status");
+  });
+  await knex("onerep_scans").update({ onerep_scan_status: "finished" });
+  await knex.schema.alterTable("onerep_scans", table => {
+    table.dropNullable("onerep_scan_status");
+  });
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function down(knex) {
+  return knex.schema.alterTable("onerep_scans", table => {
+    table.dropColumn("onerep_scan_status");
+  });
+}
