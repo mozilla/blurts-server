dd195565973e519b1528181d1b5b50c90d8e9e69
Florian Zia <zia.florian@gmail.com>
2023-09-22 12:56:19 +0200

    chore: Get nonce inline

--- src/app/(proper_react)/layout.tsx
+++ src/app/(proper_react)/layout.tsx
@@ -3,25 +3,24 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 import { ReactNode } from "react";
+import { headers } from "next/headers";
 import Script from "next/script";
 import { getL10nBundles, getLocale } from "../functions/server/l10n";
 import { L10nProvider } from "../../contextProviders/localization";
 import { ReactAriaI18nProvider } from "../../contextProviders/react-aria";
-import { getNonce } from "../functions/server/getNonce";
 
 export default function Layout({ children }: { children: ReactNode }) {
   const l10nBundles = getL10nBundles();
 
   const ga4MeasurementId =
     process.env.NEXT_PUBLIC_GA4_MEASUREMENT_ID || "G-CXG8K4KW4P";
-  const nonce = getNonce() || "";
 
   return (
     <L10nProvider bundleSources={l10nBundles}>
       {navigator.doNotTrack !== "1" && (
         <Script
           src={`https://www.googletagmanager.com/gtag/js?id=${ga4MeasurementId}`}
-          nonce={nonce}
+          nonce={headers().get("x-nonce") ?? ""}
         />
       )}
       <ReactAriaI18nProvider locale={getLocale(l10nBundles)}>
