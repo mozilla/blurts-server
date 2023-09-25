ee00a83ba6c65f8e3e1bee1952f5157d9fef7889
Robert Helmer <rhelmer@mozilla.com>
2023-09-25 10:08:14 -0700

    ignore dashboard.ts uncovered

--- src/app/functions/server/dashboard.ts
+++ src/app/functions/server/dashboard.ts
@@ -161,6 +161,7 @@ export function getDashboardSummary(
     /* c8 ignore next */
     const increment = b.emailsAffected.length ?? 0;
 
+    /* c8 ignore next 9 */
     // count emails
     if (dataClasses.includes(BreachDataTypes.Email)) {
       summary.totalExposures += increment;
@@ -171,7 +172,7 @@ export function getDashboardSummary(
       }
     }
 
-    /* c8 ignore start */
+    /* c8 ignore next 9 */
     // count phone numbers
     if (dataClasses.includes(BreachDataTypes.Phone)) {
       summary.totalExposures += increment;
@@ -181,7 +182,6 @@ export function getDashboardSummary(
         summary.dataBreachFixedNum += increment;
       }
     }
-    /* c8 ignore stop */
 
     // count password
     if (dataClasses.includes(BreachDataTypes.Passwords)) {
