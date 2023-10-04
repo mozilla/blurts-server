b51cce1202b0bc5f905da2e0cf06915eba41e938
Florian Zia <zia.florian@gmail.com>
2023-10-04 12:34:18 +0200

    fix: Typo in variable name

--- src/app/functions/server/getRelevantGuidedSteps.ts
+++ src/app/functions/server/getRelevantGuidedSteps.ts
@@ -218,11 +218,11 @@ function hasCompleted(data: InputData, stepId: StepLink["id"]): boolean {
     dataClass: (typeof BreachDataTypes)[keyof typeof BreachDataTypes]
   ): boolean {
     return !data.subscriberBreaches.some((breach) => {
-      const effectedDataClasses = breach.dataClassesEffected.map(
-        (dataClassEffected) => Object.keys(dataClassEffected).join("")
+      const affectedDataClasses = breach.dataClassesEffected.map(
+        (affectedDataClass) => Object.keys(affectedDataClass).join("")
       );
       return (
-        effectedDataClasses.includes(dataClass) &&
+        affectedDataClasses.includes(dataClass) &&
         !breach.resolvedDataClasses.includes(dataClass)
       );
     });
