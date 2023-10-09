e441375860cd6ca4b6efa1a884912b24d4838003
mansaj <jozhou@mozilla.com>
2023-10-09 13:43:00 -0700

    Update src/app/functions/server/dashboard.test.ts
    Co-authored-by: Vincent <Vinnl@users.noreply.github.com>

--- src/app/functions/server/dashboard.test.ts
+++ src/app/functions/server/dashboard.test.ts
@@ -562,12 +562,16 @@ describe("getDashboardSummary", () => {
       ).toBeGreaterThanOrEqual(0);
     }
 
-    summary.unresolvedSanitizedExposures.forEach((_, v) => {
-      expect(v).toBeGreaterThanOrEqual(0);
+    summary.unresolvedSanitizedExposures.forEach((unresolvedSanitizedExposure) => {
+      Object.values(unresolvedSanitizedExposure).forEach(count => {
+        expect(count).toBeGreaterThanOrEqual(0);
+      });
     });
 
-    summary.inProgressFixedSanitizedExposures.forEach((_, v) => {
-      expect(v).toBeGreaterThanOrEqual(0);
+    summary.inProgressFixedSanitizedExposures.forEach((inProgressFixedSanitizedExposure) => {
+      Object.values(inProgressFixedSanitizedExposure).forEach(count => {
+        expect(count).toBeGreaterThanOrEqual(0);
+      });
     });
   };
 
