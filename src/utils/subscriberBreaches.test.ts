ff587e1ddc1f830af6166ff7eb6fbdd280ce6eea
mansaj <jozhou@mozilla.com>
2023-10-12 09:05:08 -0700

    Update src/utils/subscriberBreaches.test.ts
    Co-authored-by: Vincent <Vinnl@users.noreply.github.com>

--- src/utils/subscriberBreaches.test.ts
+++ src/utils/subscriberBreaches.test.ts
@@ -524,7 +524,6 @@ describe("getSubBreaches", () => {
 
     const subBreaches = await getSubBreaches(subscriber, []);
     expect(subBreaches.length).toEqual(1);
-    console.log(JSON.stringify(subBreaches));
     expect(subBreaches[0].isResolved).toBe(true);
   });
 });
