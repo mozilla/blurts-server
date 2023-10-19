10fc258bd8f0f5bd29c43e7f1dae623922a8d358
Kaitlyn <kandres@mozilla.com>
2023-10-19 11:35:26 -0400

    update comment for clairty

--- src/app/components/client/ExposureCard.tsx
+++ src/app/components/client/ExposureCard.tsx
@@ -246,7 +246,7 @@ const ScanResultCard = (props: ScanResultCardProps) => {
               </div>
             </div>
             {isPremiumBrokerRemovalEnabled &&
-            // Checking the status for automatically removed data brokers v. manually resolved are different
+            // Verifying the status for automatically removed data brokers v. manually resolved are handled differently
             props.scanResult.status === "new" &&
             !props.scanResult.manually_resolved ? (
               <span className={styles.fixItBtn}>{props.resolutionCta}</span>
