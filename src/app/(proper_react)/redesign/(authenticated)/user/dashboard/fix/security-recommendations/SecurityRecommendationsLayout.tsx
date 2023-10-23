9b1a9fbf08b0c368b74f05b4dccfaecb8137d964
Kaitlyn <kandres@mozilla.com>
2023-10-23 10:56:52 -0400

    MNTOR-2310 - Enable Security Recommendations Got it CTA

--- src/app/(proper_react)/redesign/(authenticated)/user/dashboard/fix/security-recommendations/SecurityRecommendationsLayout.tsx
+++ src/app/(proper_react)/redesign/(authenticated)/user/dashboard/fix/security-recommendations/SecurityRecommendationsLayout.tsx
@@ -72,7 +72,10 @@ export function SecurityRecommendationsLayout(
             // TODO: Add test once MNTOR-1700 logic is added
             /* c8 ignore next 3 */
             onPress={() => {
-              // TODO: MNTOR-1700 Add routing logic
+              location.href = getNextGuidedStep(
+                props.data,
+                stepMap[props.type],
+              ).href;
             }}
             autoFocus={true}
           >
