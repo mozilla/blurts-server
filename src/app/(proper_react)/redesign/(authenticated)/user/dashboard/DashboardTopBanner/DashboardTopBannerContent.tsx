76989f844ce74fbd317288db617ed07c68eaaf6b
Florian Zia <zia.florian@gmail.com>
2023-10-23 14:13:35 +0200

    fix: Get continuous protection CTA

--- src/app/(proper_react)/redesign/(authenticated)/user/dashboard/DashboardTopBanner/DashboardTopBannerContent.tsx
+++ src/app/(proper_react)/redesign/(authenticated)/user/dashboard/DashboardTopBanner/DashboardTopBannerContent.tsx
@@ -228,9 +228,9 @@ export const DashboardTopBannerContent = (props: DashboardTopBannerProps) => {
               )}
             </p>
             <div className={styles.cta}>
-              <Button href={relevantGuidedStep.href} small variant="primary">
-                {l10n.getString("dashboard-top-banner-no-exposures-found-cta")}
-              </Button>
+              <PremiumButton
+                label={"dashboard-top-banner-no-exposures-found-cta"}
+              />
             </div>
           </>
         );
@@ -306,11 +306,7 @@ export const DashboardTopBannerContent = (props: DashboardTopBannerProps) => {
               )}
             </p>
             <div className={styles.cta}>
-              <PremiumButton
-                label={
-                  "dashboard-top-banner-your-data-is-protected-all-fixed-cta"
-                }
-              />
+              <PremiumButton label="dashboard-top-banner-no-exposures-found-cta" />
             </div>
           </>
         );
