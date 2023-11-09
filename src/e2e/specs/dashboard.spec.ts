3d9990a59f6e7939a8fc2481f25e97b75a790e22
mozrokafor <94625718+mozrokafor@users.noreply.github.com>
2023-11-09 11:35:35 -0500

    C2301511 handling verifications of site header and side nav elements

--- src/e2e/specs/dashboard.spec.ts
+++ src/e2e/specs/dashboard.spec.ts
@@ -3,6 +3,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 import { test, expect } from "../fixtures/basePage.js";
+import { DashboardPage } from "../pages/dashBoardPage.js";
 import { checkAuthState } from "../utils/helpers.js";
 
 // bypass login
@@ -31,8 +32,67 @@ test.describe("Breaches Dashboard - Headers", () => {
     expect(await dashboardPage.dashboardNavButton.getAttribute("href")).toEqual(
       "/redesign/user/dashboard",
     );
-    expect(await dashboardPage.fAQsNavButton.getAttribute("href")).toEqual(
+    expect(await dashboardPage.FAQsNavButton.getAttribute("href")).toEqual(
       "https://support.mozilla.org/kb/firefox-monitor-faq",
     );
+  });
+
+  test("Verify that the site header and navigation bar is displayed correctly", async ({
+    dashboardPage,
+  }) => {
+    // link to testrail
+    test.info().annotations.push({
+      type: "testrail",
+      description:
+        "https://testrail.stage.mozaws.net/index.php?/cases/view/2301511",
+    });
+
+    // verify the navigation bar left side elements
+    await expect(async () => {
+      expect(
+        await dashboardPage.fireFoxMonitorLogoImgButton.isVisible(),
+      ).toBeTruthy();
+      expect(await dashboardPage.dashboardNavButton.isVisible()).toBeTruthy();
+      expect(await dashboardPage.exposuresHeading.isVisible()).toBeTruthy();
+      expect(await dashboardPage.FAQsNavButton.isVisible()).toBeTruthy();
+    }).toPass();
+
+    // verify the site header elements
+    await expect(async () => {
+      expect(await dashboardPage.actionNeededTab.isVisible()).toBeTruthy();
+      expect(await dashboardPage.fixedTab.isVisible()).toBeTruthy();
+    }).toPass();
+  });
+});
+
+test.use({ storageState: "./e2e/storageState.json" });
+test.describe("Breaches Dashboard - Headers - Outside of U.S.", () => {
+  test("Verify that the site header and navigation bar is displayed correctly", async ({
+    context,
+  }) => {
+    // link to testrail
+    test.info().annotations.push({
+      type: "testrail",
+      description:
+        "https://testrail.stage.mozaws.net/index.php?/cases/view/2301511",
+    });
+
+    const page = await context.newPage(); // Set location to Edinburgh
+    await context.grantPermissions(["geolocation"]);
+    await context.setGeolocation({ latitude: 55.9533, longitude: -3.1883 }); // Set location to Edinburgh
+
+    const dashboardPage = new DashboardPage(page);
+    await dashboardPage.open();
+
+    try {
+      await checkAuthState(page);
+    } catch {
+      console.log("[E2E_LOG] - No fxa auth required, proceeding...");
+    }
+
+    // verify the site header elements
+    await expect(async () => {
+      expect(await dashboardPage.upgradeToPremium.isHidden()).toBeTruthy();
+    }).toPass();
   });
 });
