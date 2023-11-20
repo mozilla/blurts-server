2b482ae35d415a9cffb0d1850904264cbafdeed6
mansaj <jozhou@mozilla.com>
2023-11-20 09:47:09 -0800

    fix: js

--- src/db/connect.js
+++ src/db/connect.js
@@ -3,7 +3,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 import initKnex, { Knex } from "knex";
-import knexConfig from "./knexfile";
+import knexConfig from "./knexfile.js";
 
 /** @returns {Knex} */
 const createDbConnection = () => {
