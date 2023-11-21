28b5f8f9bb550c92e2e372568732d18f22bc83d6
Joey Zhou <jozhou@mozilla.com>
2023-11-20 18:08:02 -0800

    chore: cleanup

--- src/app/api/v1/user/breaches/bulk-resolve/route.ts
+++ src/app/api/v1/user/breaches/bulk-resolve/route.ts
@@ -37,7 +37,6 @@ export async function PUT(req: NextRequest): Promise<NextResponse> {
       const { dataType: dataTypeToResolve }: BreachBulkResolutionRequest = j;
       console.log({ dataTypeToResolve });
 
-      // check if breach id is a part of affectEmail's breaches
       const { verifiedEmails } = await getAllEmailsAndBreaches(
         subscriber,
         allBreaches,
@@ -84,67 +83,6 @@ export async function PUT(req: NextRequest): Promise<NextResponse> {
       // set useBreachId to mark latest version of breach resolution
       // without this line, the get call might assume recency index
       currentBreachResolution.useBreachId = true;
-
-      // if (affectedEmailAsSubscriber) {
-      //   currentEmail = verifiedEmails.find(
-      //     (ve) => ve.email === affectedEmailAsSubscriber,
-      //   );
-      // } else {
-      //   currentEmail = verifiedEmails.find(
-      //     (ve) => ve.email === affectedEmailInEmailAddresses,
-      //   );
-      // }
-      // const currentBreaches = currentEmail?.breaches?.filter(
-      //   (b) => b.Id === breachIdNumber,
-      // );
-      // if (!currentBreaches) {
-      //   return NextResponse.json({
-      //     success: false,
-      //     message: "Error: breachId provided does not exist",
-      //   });
-      // }
-
-      // check if resolutionsChecked array is a subset of the breaches' datatypes
-      // const isSubset = resolutionsChecked.every((val) =>
-      //   currentBreaches[0].DataClasses.includes(val),
-      // );
-      // if (!isSubset) {
-      //   return NextResponse.json({
-      //     success: false,
-      //     message: `Error: the resolutionChecked param contains more than allowed data types: [${resolutionsChecked.join(
-      //       ", ",
-      //     )}]`,
-      //   });
-      // }
-
-      // /* new JsonB:
-      // {
-      //   email_id: {
-      //     recency_index: {
-      //       resolutions: ['email', ...],
-      //       isResolved: true
-      //     }
-      //   }
-      // }
-      // */
-
-      // const currentBreachDataTypes = currentBreaches[0].DataClasses; // get this from existing breaches
-      // const currentBreachResolution = subscriber.breach_resolution || {}; // get this from existing breach resolution if available
-      // const isResolved =
-      //   resolutionsChecked.length === currentBreachDataTypes.length;
-      // currentBreachResolution[affectedEmail] = {
-      //   ...(currentBreachResolution[affectedEmail] || {}),
-      //   ...{
-      //     [breachIdNumber]: {
-      //       resolutionsChecked,
-      //       isResolved,
-      //     },
-      //   },
-      // };
-
-      // set useBreachId to mark latest version of breach resolution
-      // without this line, the get call might assume recency index
-      // currentBreachResolution.useBreachId = true;
 
       const updatedSubscriber = await setBreachResolution(
         subscriber,
