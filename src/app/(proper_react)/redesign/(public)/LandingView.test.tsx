71a1c46ae82b91ee8ce735f1d63f8e2d4d0b2e96
Vincent <git@vincenttunru.com>
2023-11-21 12:22:35 +0100

    Add plans to the landing page on wide screens
    Version for smaller screens will be added later.

--- src/app/(proper_react)/redesign/(public)/LandingView.test.tsx
+++ src/app/(proper_react)/redesign/(public)/LandingView.test.tsx
@@ -46,4 +46,91 @@ describe("When Premium is available", () => {
     const { container } = render(<ComposedDashboard />);
     expect(await axe(container)).toHaveNoViolations();
   });
+
+  it("can open and close the tooltip with the keyboard", async () => {
+    const user = userEvent.setup();
+
+    const ComposedDashboard = composeStory(LandingUs, Meta);
+    render(<ComposedDashboard />);
+
+    expect(screen.queryByRole("dialog")).not.toBeInTheDocument();
+
+    const moreInfoButton = screen.getAllByRole("button", {
+      name: "More info",
+    })[0];
+    await user.click(moreInfoButton);
+
+    expect(screen.getByRole("dialog")).toBeInTheDocument();
+
+    await user.keyboard("[Escape]");
+    expect(screen.queryByRole("dialog")).not.toBeInTheDocument();
+  });
+
+  it("can switch from the yearly to the monthly plan with the keyboard", async () => {
+    const user = userEvent.setup();
+
+    const ComposedDashboard = composeStory(LandingUs, Meta);
+    render(<ComposedDashboard />);
+
+    // Regular expression:
+    //
+    //   Save   Starts with the characters `Save `,
+    //
+    //   (.+?)  followed by one or more (`+`) arbitrary characters (`.`), until
+    //          the next part of the regular expression matches…
+    //
+    //   %      …which is a single `%` character.
+    //
+    // All that combines to a string like "Save 13.37%".
+    expect(screen.getByText(/Save (.+?)%/)).toBeInTheDocument();
+
+    const yearlyToggle = screen.getByRole("radio", { name: "Yearly" });
+    await user.click(yearlyToggle);
+    await user.keyboard("[ArrowRight][Space]");
+
+    // Regular expression:
+    //
+    //   Save   Starts with the characters `Save `,
+    //
+    //   (.+?)  followed by one or more (`+`) arbitrary characters (`.`), until
+    //          the next part of the regular expression matches…
+    //
+    //   %      …which is a single `%` character.
+    //
+    // All that combines to a string like "Save 13.37%".
+    expect(screen.queryByText(/Save (.+?)%/)).not.toBeInTheDocument();
+  });
+
+  it("can move to the subscribe button with the keyboard", async () => {
+    const user = userEvent.setup();
+
+    const ComposedDashboard = composeStory(LandingUs, Meta);
+    render(<ComposedDashboard />);
+
+    const plusSubscribeButton = screen.getByRole("link", {
+      name: "Get data removal",
+    });
+    expect(plusSubscribeButton).not.toHaveFocus();
+
+    const yearlyToggle = screen.getByRole("radio", { name: "Yearly" });
+    await user.click(yearlyToggle);
+    await user.keyboard("[ArrowRight][ArrowRight]");
+
+    expect(plusSubscribeButton).toHaveFocus();
+  });
+
+  it("can initiate sign in from the pricing table", async () => {
+    const user = userEvent.setup();
+
+    const ComposedDashboard = composeStory(LandingUs, Meta);
+    render(<ComposedDashboard />);
+
+    expect(signIn).not.toHaveBeenCalled();
+
+    const yearlyToggle = screen.getByRole("radio", { name: "Yearly" });
+    await user.click(yearlyToggle);
+    await user.keyboard("[ArrowLeft][Space]");
+
+    expect(signIn).toHaveBeenCalledTimes(1);
+  });
 });
