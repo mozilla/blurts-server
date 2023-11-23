b443f0154272fd07646af7529ab9627d818747b4
Kaitlyn Andres <kandres@mozilla.com>
2023-11-23 12:49:06 -0500

    MNTOR-2259 - Password Resolution Steps (#3712)
    * WIP - Adding resolution logic to passwords/security logic
    
    * update to the right types
    
    * add logic to show next leaked passwords
    
    * switch content on click
    
    * add email to step count
    
    * add logic to check that step has completed
    
    * align recommendations step
    
    * switch between security questions and passwords
    
    * wip: don't show empty state at the end of passwords array
    
    * use async func instead o then()
    
    * use async func instead o then()
    
    * traverse through security questions
    
    * ignore eslint for useeffect dependency array
    
    * remove test ignore
    
    * add var comment fluent
    
    * add comment about writing unit tests later
    
    * ignore tests for now
    
    * remove secondary useEffect
    
    * add security question email match
    
    * pass all previous resolutions checked
    
    * fix bad rebase
    
    * fix bad rebase
    
    * combine unresolvedsecurityquestionsbreach and unresolvedpasswordbreach
    
    * remove useeffect
    
    * remove redundant const

--- src/app/(proper_react)/redesign/(authenticated)/user/dashboard/fix/leaked-passwords/LeakedPasswordsLayout.tsx
+++ src/app/(proper_react)/redesign/(authenticated)/user/dashboard/fix/leaked-passwords/LeakedPasswordsLayout.tsx
@@ -10,7 +10,9 @@ import { Button } from "../../../../../../../components/server/Button";
 import { useL10n } from "../../../../../../../hooks/l10n";
 import {
   LeakedPasswordsTypes,
+  findFirstUnresolvedBreach,
   getLeakedPasswords,
+  updatePasswordsBreachStatus,
 } from "./leakedPasswordsData";
 import Link from "next/link";
 import { getLocale } from "../../../../../../../functions/universal/getLocale";
@@ -18,10 +20,13 @@ import {
   StepDeterminationData,
   StepLink,
   getNextGuidedStep,
+  hasCompletedStep,
 } from "../../../../../../../functions/server/getRelevantGuidedSteps";
 import { FixView } from "../FixView";
 import { getGuidedExperienceBreaches } from "../../../../../../../functions/universal/guidedExperienceBreaches";
 import { hasPremium } from "../../../../../../../functions/universal/user";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 export interface LeakedPasswordsLayoutProps {
   type: LeakedPasswordsTypes;
@@ -31,6 +36,10 @@ export interface LeakedPasswordsLayoutProps {
 
 export function LeakedPasswordsLayout(props: LeakedPasswordsLayoutProps) {
   const l10n = useL10n();
+  const router = useRouter();
+  const [subscriberBreaches, setSubscriberBreaches] = useState(
+    props.data.subscriberBreaches,
+  );
 
   const stepMap: Record<LeakedPasswordsTypes, StepLink["id"]> = {
     passwords: "LeakedPasswordsPassword",
@@ -41,65 +50,130 @@ export function LeakedPasswordsLayout(props: LeakedPasswordsLayoutProps) {
   };
 
   const guidedExperienceBreaches = getGuidedExperienceBreaches(
-    props.data.subscriberBreaches,
+    subscriberBreaches,
     props.subscriberEmails,
   );
 
-  const nextStep = getNextGuidedStep(props.data, stepMap[props.type]);
-  const pageData = getLeakedPasswords({
-    dataType: props.type,
-    breaches: guidedExperienceBreaches,
-    l10n: l10n,
-    nextStep,
-  });
+  const unresolvedPasswordBreach = findFirstUnresolvedBreach(
+    guidedExperienceBreaches,
+    props.type,
+  );
 
-  // The non-null assertion here should be safe since we already did this check
-  // in `./[type]/page.tsx`:
-  const { title, illustration, content } = pageData!;
+  // TODO: Write unit tests MNTOR-2560
+  /* c8 ignore start */
+  const emailAffected =
+    props.subscriberEmails.find(
+      (email) => unresolvedPasswordBreach?.emailsAffected.includes(email),
+    ) ?? "";
+
+  const nextStep = getNextGuidedStep(props.data, stepMap[props.type]);
+
+  // Data class string to push to resolutionsChecked array
+  const resolvedDataClassName =
+    props.type === "passwords" ? "passwords" : "security-questions-and-answers";
+
   const isStepDone =
     props.type === "passwords-done" || props.type === "security-questions-done";
 
+  const unresolvedPasswordBreachContent = getLeakedPasswords({
+    dataType: props.type,
+    breaches: guidedExperienceBreaches,
+    l10n,
+    emailAffected,
+    nextStep,
+  });
+
+  const handleUpdateBreachStatus = async () => {
+    if (!unresolvedPasswordBreach || !emailAffected) {
+      return;
+    }
+
+    try {
+      unresolvedPasswordBreach.resolvedDataClasses.push(resolvedDataClassName);
+      // FIXME/BUG: MNTOR-2562 Remove empty [""] string
+      const formattedDataClasses =
+        unresolvedPasswordBreach.resolvedDataClasses.filter(Boolean);
+
+      await updatePasswordsBreachStatus(
+        emailAffected,
+        unresolvedPasswordBreach.id,
+        formattedDataClasses,
+      );
+
+      // Manually move to the next step when mark is fixed is selected
+      const updatedSubscriberBreaches = subscriberBreaches.map(
+        (subscriberBreach) => {
+          if (subscriberBreach.id === unresolvedPasswordBreach.id) {
+            subscriberBreach.resolvedDataClasses.push(resolvedDataClassName);
+          }
+          return subscriberBreach;
+        },
+      );
+
+      const isComplete = hasCompletedStep(
+        { ...props.data, subscriberBreaches: updatedSubscriberBreaches },
+        stepMap[props.type],
+      );
+
+      if (!isComplete) {
+        setSubscriberBreaches(updatedSubscriberBreaches);
+      }
+      // If all breaches in the step is fully resolved, take users to the next step
+      else {
+        router.push(nextStep.href);
+      }
+    } catch (error) {
+      console.error("Error updating breach status", error);
+    }
+  };
+  /* c8 ignore stop */
+
   return (
-    <FixView
-      subscriberEmails={props.subscriberEmails}
-      data={props.data}
-      nextStep={nextStep}
-      currentSection="leaked-passwords"
-      hideProgressIndicator={isStepDone}
-      showConfetti={isStepDone}
-    >
-      <ResolutionContainer
-        type="leakedPasswords"
-        title={title}
-        illustration={illustration}
-        isPremiumUser={hasPremium(props.data.user)}
-        cta={
-          !isStepDone && (
-            <>
-              <Button
-                variant="primary"
-                small
-                // TODO: Add test once MNTOR-1700 logic is added
-                /* c8 ignore next 3 */
-                onPress={() => {
-                  // TODO: MNTOR-1700 Add routing logic
-                }}
-                autoFocus={true}
-              >
-                {l10n.getString("leaked-passwords-mark-as-fixed")}
-              </Button>
-              <Link href={nextStep.href}>
-                {l10n.getString("leaked-passwords-skip")}
-              </Link>
-            </>
-          )
-        }
-        estimatedTime={!isStepDone ? 4 : undefined}
-        isStepDone={isStepDone}
+    unresolvedPasswordBreachContent &&
+    unresolvedPasswordBreach && (
+      <FixView
+        subscriberEmails={props.subscriberEmails}
         data={props.data}
+        nextStep={nextStep}
+        currentSection="leaked-passwords"
+        hideProgressIndicator={isStepDone}
+        showConfetti={isStepDone}
       >
-        <ResolutionContent content={content} locale={getLocale(l10n)} />
-      </ResolutionContainer>
-    </FixView>
+        <ResolutionContainer
+          type="leakedPasswords"
+          title={unresolvedPasswordBreachContent.title}
+          illustration={unresolvedPasswordBreachContent.illustration}
+          isPremiumUser={hasPremium(props.data.user)}
+          cta={
+            !isStepDone && (
+              <>
+                <Button
+                  variant="primary"
+                  small
+                  /* c8 ignore next 3 */
+                  onPress={() => {
+                    void handleUpdateBreachStatus();
+                  }}
+                  autoFocus={true}
+                >
+                  {l10n.getString("leaked-passwords-mark-as-fixed")}
+                </Button>
+                <Link href={nextStep.href}>
+                  {l10n.getString("leaked-passwords-skip")}
+                </Link>
+              </>
+            )
+          }
+          estimatedTime={!isStepDone ? 4 : undefined}
+          isStepDone={isStepDone}
+          data={props.data}
+        >
+          <ResolutionContent
+            content={unresolvedPasswordBreachContent.content}
+            locale={getLocale(l10n)}
+          />
+        </ResolutionContainer>
+      </FixView>
+    )
   );
 }
