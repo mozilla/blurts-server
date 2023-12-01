78e8f203470f554a8f78922e99d4c96cdbe91204
Vincent <git@vincenttunru.com>
2023-12-01 16:15:09 +0100

    Add small-screen version of the pricing table

--- src/app/(proper_react)/redesign/(public)/LandingView.test.tsx
+++ src/app/(proper_react)/redesign/(public)/LandingView.test.tsx
@@ -4,7 +4,15 @@
 
 import { it, expect } from "@jest/globals";
 import { composeStory } from "@storybook/react";
-import { render, screen } from "@testing-library/react";
+import {
+  getAllByRole,
+  getByRole,
+  getByText,
+  queryByRole,
+  queryByText,
+  render,
+  screen,
+} from "@testing-library/react";
 import { userEvent } from "@testing-library/user-event";
 import { axe } from "jest-axe";
 import { signIn } from "next-auth/react";
@@ -52,10 +60,14 @@ describe("When Premium is available", () => {
 
     const ComposedDashboard = composeStory(LandingUs, Meta);
     render(<ComposedDashboard />);
+    // We limit our queries to the pricing table, so as not to match similar
+    // elements that are also present in the pricing _cards_, i.e. the elements
+    // shown on small screens:
+    const pricingTable = screen.getByRole("grid");
 
-    expect(screen.queryByRole("dialog")).not.toBeInTheDocument();
+    expect(queryByRole(pricingTable, "dialog")).not.toBeInTheDocument();
 
-    const moreInfoButton = screen.getAllByRole("button", {
+    const moreInfoButton = getAllByRole(pricingTable, "button", {
       name: "More info",
     })[0];
     await user.click(moreInfoButton);
@@ -71,6 +83,10 @@ describe("When Premium is available", () => {
 
     const ComposedDashboard = composeStory(LandingUs, Meta);
     render(<ComposedDashboard />);
+    // We limit our queries to the pricing table, so as not to match similar
+    // elements that are also present in the pricing _cards_, i.e. the elements
+    // shown on small screens:
+    const pricingTable = screen.getByRole("grid");
 
     // Regular expression:
     //
@@ -82,9 +98,9 @@ describe("When Premium is available", () => {
     //   %      …which is a single `%` character.
     //
     // All that combines to a string like "Save 13.37%".
-    expect(screen.getByText(/Save (.+?)%/)).toBeInTheDocument();
+    expect(getByText(pricingTable, /Save (.+?)%/)).toBeInTheDocument();
 
-    const yearlyToggle = screen.getByRole("radio", { name: "Yearly" });
+    const yearlyToggle = getByRole(pricingTable, "radio", { name: "Yearly" });
     await user.click(yearlyToggle);
     await user.keyboard("[ArrowRight][Space]");
 
@@ -98,7 +114,84 @@ describe("When Premium is available", () => {
     //   %      …which is a single `%` character.
     //
     // All that combines to a string like "Save 13.37%".
-    expect(screen.queryByText(/Save (.+?)%/)).not.toBeInTheDocument();
+    expect(queryByText(pricingTable, /Save (.+?)%/)).not.toBeInTheDocument();
+  });
+
+  it("can switch from the yearly to the monthly plan with the mouse", async () => {
+    const user = userEvent.setup();
+
+    const ComposedDashboard = composeStory(LandingUs, Meta);
+    render(<ComposedDashboard />);
+    // We limit our queries to the pricing table, so as not to match similar
+    // elements that are also present in the pricing _cards_, i.e. the elements
+    // shown on small screens:
+    const pricingTable = screen.getByRole("grid");
+
+    // Regular expression:
+    //
+    //   Save   Starts with the characters `Save `,
+    //
+    //   (.+?)  followed by one or more (`+`) arbitrary characters (`.`), until
+    //          the next part of the regular expression matches…
+    //
+    //   %      …which is a single `%` character.
+    //
+    // All that combines to a string like "Save 13.37%".
+    expect(getByText(pricingTable, /Save (.+?)%/)).toBeInTheDocument();
+
+    const monthlyToggle = getByRole(pricingTable, "radio", { name: "Monthly" });
+    await user.click(monthlyToggle);
+
+    // Regular expression:
+    //
+    //   Save   Starts with the characters `Save `,
+    //
+    //   (.+?)  followed by one or more (`+`) arbitrary characters (`.`), until
+    //          the next part of the regular expression matches…
+    //
+    //   %      …which is a single `%` character.
+    //
+    // All that combines to a string like "Save 13.37%".
+    expect(queryByText(pricingTable, /Save (.+?)%/)).not.toBeInTheDocument();
+  });
+
+  it("switching to the monthly plan in portrait mode is preserved when changing to landscape mode", async () => {
+    const user = userEvent.setup();
+
+    const ComposedDashboard = composeStory(LandingUs, Meta);
+    render(<ComposedDashboard />);
+    const pricingTable = screen.getByRole("grid");
+    const cards = screen.getAllByRole("group");
+    const plusCard = cards[0];
+
+    // Regular expression:
+    //
+    //   Save   Starts with the characters `Save `,
+    //
+    //   (.+?)  followed by one or more (`+`) arbitrary characters (`.`), until
+    //          the next part of the regular expression matches…
+    //
+    //   %      …which is a single `%` character.
+    //
+    // All that combines to a string like "Save 13.37%".
+    expect(getByText(plusCard, /Save (.+?)%/)).toBeInTheDocument();
+    expect(getByText(pricingTable, /Save (.+?)%/)).toBeInTheDocument();
+
+    const monthlyToggle = getByRole(plusCard, "radio", { name: "Monthly" });
+    await user.click(monthlyToggle);
+
+    // Regular expression:
+    //
+    //   Save   Starts with the characters `Save `,
+    //
+    //   (.+?)  followed by one or more (`+`) arbitrary characters (`.`), until
+    //          the next part of the regular expression matches…
+    //
+    //   %      …which is a single `%` character.
+    //
+    // All that combines to a string like "Save 13.37%".
+    expect(queryByText(plusCard, /Save (.+?)%/)).not.toBeInTheDocument();
+    expect(queryByText(pricingTable, /Save (.+?)%/)).not.toBeInTheDocument();
   });
 
   it("can move to the subscribe button with the keyboard", async () => {
@@ -106,13 +199,17 @@ describe("When Premium is available", () => {
 
     const ComposedDashboard = composeStory(LandingUs, Meta);
     render(<ComposedDashboard />);
+    // We limit our queries to the pricing table, so as not to match similar
+    // elements that are also present in the pricing _cards_, i.e. the elements
+    // shown on small screens:
+    const pricingTable = screen.getByRole("grid");
 
-    const plusSubscribeButton = screen.getByRole("link", {
+    const plusSubscribeButton = getByRole(pricingTable, "link", {
       name: "Get data removal",
     });
     expect(plusSubscribeButton).not.toHaveFocus();
 
-    const yearlyToggle = screen.getByRole("radio", { name: "Yearly" });
+    const yearlyToggle = getByRole(pricingTable, "radio", { name: "Yearly" });
     await user.click(yearlyToggle);
     await user.keyboard("[ArrowRight][ArrowRight]");
 
@@ -124,12 +221,31 @@ describe("When Premium is available", () => {
 
     const ComposedDashboard = composeStory(LandingUs, Meta);
     render(<ComposedDashboard />);
+    const pricingTable = screen.getByRole("grid");
 
     expect(signIn).not.toHaveBeenCalled();
 
-    const yearlyToggle = screen.getByRole("radio", { name: "Yearly" });
+    const yearlyToggle = getByRole(pricingTable, "radio", { name: "Yearly" });
     await user.click(yearlyToggle);
     await user.keyboard("[ArrowLeft][Space]");
+
+    expect(signIn).toHaveBeenCalledTimes(1);
+  });
+
+  it("can initiate sign in from the pricing cards", async () => {
+    const user = userEvent.setup();
+
+    const ComposedDashboard = composeStory(LandingUs, Meta);
+    render(<ComposedDashboard />);
+    const cards = screen.getAllByRole("group");
+    const freeCard = cards[1];
+
+    expect(signIn).not.toHaveBeenCalled();
+
+    const signInButton = getByRole(freeCard, "button", {
+      name: "Start free monitoring",
+    });
+    await user.click(signInButton);
 
     expect(signIn).toHaveBeenCalledTimes(1);
   });
