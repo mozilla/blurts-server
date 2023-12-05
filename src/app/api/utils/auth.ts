4a12d95305d4d9e6e915c0b35fc5dfd150c16f79
mansaj <jozhou@mozilla.com>
2023-12-05 09:43:22 -0800

    Update src/app/api/utils/auth.ts
    Co-authored-by: Vincent <Vinnl@users.noreply.github.com>

--- src/app/api/utils/auth.ts
+++ src/app/api/utils/auth.ts
@@ -102,7 +102,9 @@ export const authOptions: AuthOptions = {
               account.refresh_token,
               JSON.stringify(profile),
             );
-            // MNTOR-2599: delete breach_resolution in token
+            // MNTOR-2599 The breach_resolution object can get pretty big,
+            // causing the session token cookie to balloon in size,
+            // eventually resulting in a 400 Bad Request due to headers being too large.
             delete updatedUser.breach_resolution;
             token.subscriber = updatedUser;
           }
