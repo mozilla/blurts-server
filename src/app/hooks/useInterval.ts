ebbff235a8c4ed5dfc1d7e6317fe86c90b1d9cb8
Robert Helmer <rhelmer@mozilla.com>
2023-12-18 16:38:42 -0800

    better comments

--- src/app/hooks/useInterval.ts
+++ src/app/hooks/useInterval.ts
@@ -5,9 +5,9 @@
 "use client";
 import { MutableRefObject, useEffect, useRef } from "react";
 
-// Ignored for test coverage, as tests would be too mock-heavy to be useful:
+// Ignored for test coverage, this is a very React-specific problem.
+// @see https://overreacted.io/making-setinterval-declarative-with-react-hooks/
 /* c8 ignore start */
-// Based on https://overreacted.io/making-setinterval-declarative-with-react-hooks/
 export function useInterval(callback: () => void, delay: number) {
   const savedCallback: MutableRefObject<(() => void) | undefined> = useRef();
 
