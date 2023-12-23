aa6d8b3b9e6be1b55cfec096835d2e80a70bf3be
Robert Helmer <rhelmer@mozilla.com>
2023-12-22 21:20:39 -0800

    WIP unit test

--- src/app/api/v1/onerep-events/route.test.ts
+++ src/app/api/v1/onerep-events/route.test.ts
@@ -6,28 +6,85 @@ import { it, expect } from "@jest/globals";
 import { OnerepWebhookRequest } from "./route.ts";
 import { processOnerepWebhook } from "./processOnerepWebhook.ts";
 
-// TODO log correct severity
-jest.mock("../../../functions/server/logging"),
-  (message, details) => console.info(message, details);
+import { optoutProfile } from "../../../functions/server/onerep";
+import { refreshStoredScanResults } from "../../../functions/server/refreshStoredScanResults";
+import { logger } from "../../../functions/server/logging.ts";
+
+jest.mock("../../../functions/server/onerep", () => ({
+  optoutProfile: jest.fn(),
+}));
+
+jest.mock("../../../functions/server/refreshStoredScanResults.ts", () => ({
+  refreshStoredScanResults: jest.fn(),
+}));
+
+jest.mock("../../../functions/server/logging.ts", () => {
+  class Logging {
+    info(message, details) {
+      console.info(message, details);
+    }
+  }
+
+  const logger = new Logging();
+  return {
+    logger,
+  };
+});
+
+jest.spyOn(logger, "info");
+
+const mockRequest = {
+  id: 99,
+  profile_id: 1133,
+  type: "scan.completed",
+  data: {
+    object: {
+      id: 0,
+      status: "finished",
+      profile_id: 1133,
+      reason: "initial",
+      created_at: "",
+      updated_at: "",
+      url: "",
+    },
+  },
+  created_at: "",
+} as OnerepWebhookRequest;
 
 it("accepts a valid initial OnerepWebhookRequest", () => {
-  const mockRequest = {
-    id: 0,
-    profile_id: 0,
-    type: "scan.completed",
-    data: {
-      object: {
-        id: 0,
-        status: "finished",
-        profile_id: 1133,
-        reason: "initial",
-        created_at: "",
-        updated_at: "",
-        url: "",
-      },
-    },
-    created_at: "",
-  } as OnerepWebhookRequest;
+  void processOnerepWebhook(mockRequest);
 
-  expect(processOnerepWebhook(mockRequest)).toBe("testing");
+  expect(logger.info).toHaveBeenCalledWith("received_onerep_webhook", {
+    profileId: 1133,
+    scanId: 0,
+    reason: "initial",
+  });
+  expect(optoutProfile).toHaveBeenCalledTimes(0);
+  expect(refreshStoredScanResults).toHaveBeenCalledWith(1133);
+});
+
+it("accepts a valid manual OnerepWebhookRequest", () => {
+  mockRequest.data.object.reason = "manual";
+  void processOnerepWebhook(mockRequest);
+
+  expect(logger.info).toHaveBeenCalledWith("received_onerep_webhook", {
+    profileId: 1133,
+    scanId: 0,
+    reason: "manual",
+  });
+  expect(optoutProfile).toHaveBeenCalledTimes(0);
+  expect(refreshStoredScanResults).toHaveBeenCalledWith(1133);
+});
+
+it("accepts a valid monitoring OnerepWebhookRequest", () => {
+  mockRequest.data.object.reason = "monitoring";
+  void processOnerepWebhook(mockRequest);
+
+  expect(logger.info).toHaveBeenCalledWith("received_onerep_webhook", {
+    profileId: 1133,
+    scanId: 0,
+    reason: "monitoring",
+  });
+  expect(optoutProfile).toHaveBeenCalledTimes(1);
+  // FIXME expect(refreshStoredScanResults).toHaveBeenCalledWith(1133);
 });
