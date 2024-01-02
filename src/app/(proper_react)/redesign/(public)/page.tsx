60827f8fd154826c974fb5dd5152b58986253fd6
Joey Zhou <jozhou@mozilla.com>
2024-01-02 12:40:55 -0800

    feat: add logic for swapping waitlist page

--- src/app/(proper_react)/redesign/(public)/page.tsx
+++ src/app/(proper_react)/redesign/(public)/page.tsx
@@ -4,7 +4,11 @@
 
 import { headers } from "next/headers";
 import { getCountryCode } from "../../../functions/server/getCountryCode";
-import { isEligibleForPremium } from "../../../functions/server/onerep";
+import {
+  isEligibleForPremium,
+  getProfilesStats,
+  monthlySubscriberQuota,
+} from "../../../functions/server/onerep";
 import { getEnabledFeatureFlags } from "../../../../db/tables/featureFlags";
 import { getL10n } from "../../../functions/server/l10n";
 import { View } from "./LandingView";
@@ -13,9 +17,13 @@ export default async function Page() {
   const enabledFlags = await getEnabledFeatureFlags({ ignoreAllowlist: true });
   const countryCode = getCountryCode(headers());
   const eligibleForPremium = isEligibleForPremium(countryCode, enabledFlags);
-  // TODO: Swap scanLimit value for accurate logic check
-  const scanLimit = true;
 
+  // request the profile stats for the last 30 days
+  const profileStats = await getProfilesStats(
+    new Date(new Date().setDate(new Date().getDate() - 30)),
+  );
+  const oneRepActivations = profileStats?.total_active || 0;
+  const scanLimit = oneRepActivations > monthlySubscriberQuota;
   return (
     <View
       eligibleForPremium={eligibleForPremium}
