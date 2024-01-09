5935e60e248fc9c1ee4b055fb1a3d544015c3766
Joey Zhou <jozhou@mozilla.com>
2024-01-08 16:22:48 -0800

    feat: attributions table

---
+++ src/db/migrations/20240108008813_marketing_attributions.js
@@ -0,0 +1,29 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+export async function up(knex) {
+  return knex.schema
+    .createTable("attributions", table => {
+      table.increments('id').primary()
+      table.integer("subscriber_id").references("subscribers.id").notNullable();
+      table.string("type").notNullable()
+      table.string("utm_source")
+      table.string("utm_campaign")
+      table.string("utm_medium")
+      table.string("utm_term")
+      table.string("entrypoint")
+      table.string("flow_id")
+      table.timestamp("created_at").defaultTo(knex.fn.now())
+      table.timestamp("modified_at").defaultTo(knex.fn.now())
+    })
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function down(knex) {
+  return knex.schema
+    .dropTableIfExists("attributions")
+}
