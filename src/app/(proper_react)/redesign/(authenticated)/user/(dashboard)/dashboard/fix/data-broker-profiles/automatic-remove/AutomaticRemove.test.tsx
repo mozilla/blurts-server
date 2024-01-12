b74ac07cf58bc60f0d56ea7dc72c9ba959ec71f5
Vincent <git@vincenttunru.com>
2024-01-12 11:36:39 +0100

    Count interactions with guided flow nav buttons

--- src/app/(proper_react)/redesign/(authenticated)/user/(dashboard)/dashboard/fix/data-broker-profiles/automatic-remove/AutomaticRemove.test.tsx
+++ src/app/(proper_react)/redesign/(authenticated)/user/(dashboard)/dashboard/fix/data-broker-profiles/automatic-remove/AutomaticRemove.test.tsx
@@ -9,6 +9,7 @@ import { composeStory } from "@storybook/react";
 import { axe } from "jest-axe";
 
 import Meta, { AutomaticRemoveViewStory } from "./AutomaticRemove.stories";
+import { useTelemetry } from "../../../../../../../../../hooks/useTelemetry";
 
 jest.mock("../../../../../../../../../hooks/useTelemetry");
 
@@ -46,4 +47,40 @@ it("hides the progress indicator on the automatic resolution Premium upsell", ()
     name: "Guided steps",
   });
   expect(guidedStepsNavigation).not.toBeInTheDocument();
+});
+
+it("counts how often users exit the guided flow", async () => {
+  const user = userEvent.setup();
+  const mockedRecord = useTelemetry();
+  const AutomaticRemoveView = composeStory(AutomaticRemoveViewStory, Meta);
+  render(<AutomaticRemoveView />);
+
+  const exitLink = screen.getByRole("link", { name: "Return to dashboard" });
+  // jsdom will complain about not being able to navigate to a different page
+  // after clicking the link; suppress that error, as it's not relevant to the
+  // test:
+  jest.spyOn(console, "error").mockImplementationOnce(() => undefined);
+  await user.click(exitLink);
+
+  expect(mockedRecord).toHaveBeenCalledWith("button", "click", {
+    button_id: "exited_guided_experience",
+  });
+});
+
+it("counts how often users skip to the next section", async () => {
+  const user = userEvent.setup();
+  const mockedRecord = useTelemetry();
+  const AutomaticRemoveView = composeStory(AutomaticRemoveViewStory, Meta);
+  render(<AutomaticRemoveView />);
+
+  const skipArrowLink = screen.getByRole("link", { name: "Go to next step" });
+  // jsdom will complain about not being able to navigate to a different page
+  // after clicking the link; suppress that error, as it's not relevant to the
+  // test:
+  jest.spyOn(console, "error").mockImplementationOnce(() => undefined);
+  await user.click(skipArrowLink);
+
+  expect(mockedRecord).toHaveBeenCalledWith("button", "click", {
+    button_id: "next_arrow",
+  });
 });
