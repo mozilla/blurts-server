0e4ba27de5245a4c8a14471f989b57aaeae4b315
mozrokafor <94625718+mozrokafor@users.noreply.github.com>
2024-01-17 10:23:48 -0500

    reporter update

--- playwright.config.js
+++ playwright.config.js
@@ -50,7 +50,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
 
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: process.env.CI ? [['github'], ['./e2ereporter.ts'], ['html']] : [['html'], ['list']],
+  reporter: process.env.CI ? [['github'], ['e2ereporter.ts'], ['html']] : [['html'], ['list']],
 
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
