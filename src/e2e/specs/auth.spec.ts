164a2affd821cebbb4f527c8fe3390c3019ca6b9
mozrokafor <94625718+mozrokafor@users.noreply.github.com>
2024-01-18 14:24:57 -0500

    link update

--- src/e2e/specs/auth.spec.ts
+++ src/e2e/specs/auth.spec.ts
@@ -29,7 +29,7 @@ test.describe(`${process.env.E2E_TEST_ENV} - Authentication flow verification @s
 
     // assert successful login
     // will poll for url as Fxa takes some time
-    expect(page.url()).toBe(`${process.env.E2E_TEST_BASE_URL}/user/welcome`);
+    expect(page.url()).toBe(`${process.env.E2E_TEST_BASE_URL}/user/dashboard`);
 
     await testInfo.attach(
       `${process.env.E2E_TEST_ENV}-signup-monitor-dashboard.png`,
