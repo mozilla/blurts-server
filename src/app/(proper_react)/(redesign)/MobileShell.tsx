5d8a68c003cfba8bc6bf118f783df6a3b55e0fe6
Kaitlyn <kandres@mozilla.com>
2024-01-29 12:51:53 -0500

    add more specific comment

--- src/app/(proper_react)/(redesign)/MobileShell.tsx
+++ src/app/(proper_react)/(redesign)/MobileShell.tsx
@@ -38,6 +38,9 @@ export const MobileShell = (props: Props) => {
   const wrapperRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
+    // As we transition focus away from the navigation bar in deeper sections
+    // of the experience, it's best to ensure its focus on the dashboard page,
+    // where users first encounter it and when they return to it
     /* c8 ignore next 3 */
     if (pathName === "/user/dashboard") {
       wrapperRef.current?.focus();
