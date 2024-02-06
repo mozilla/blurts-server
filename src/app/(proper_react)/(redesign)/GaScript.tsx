e160b214f41b855ae4d2b2312e5ab275f2dfe29f
Robert Helmer <rhelmer@mozilla.com>
2024-02-05 22:30:46 -0800

    try using old analytics script

--- src/app/(proper_react)/(redesign)/GaScript.tsx
+++ src/app/(proper_react)/(redesign)/GaScript.tsx
@@ -5,7 +5,6 @@
 "use client";
 
 import Script from "next/script";
-import { CONST_GA4_MEASUREMENT_ID } from "../../../constants";
 
 export type Props = {
   nonce: string;
@@ -19,33 +18,13 @@ declare global {
 
 export const GaScript = ({ nonce }: Props) => {
   /* c8 ignore next 2 */
-  const ga4MeasurementId = CONST_GA4_MEASUREMENT_ID || "G-CXG8K4KW4P";
-
-  const debugMode =
-    // `NEXT_PUBLIC_GA4_DEBUG_MODE` being false doesn't negatively impact
-    // the reliability of our tests:
-    /* c8 ignore next 2 */
-    process.env.NEXT_PUBLIC_GA4_DEBUG_MODE === "true" &&
-    process.env.NODE_ENV !== "test";
 
   return typeof navigator !== "undefined" && navigator.doNotTrack !== "1" ? (
-    <>
-      <Script
-        id="google-analytics"
-        src={`https://www.googletagmanager.com/gtag/js?id=${ga4MeasurementId}`}
-        nonce={nonce}
-        onLoad={() => {
-          window.dataLayer = window.dataLayer || [];
-          window.gtag = function (...args: unknown[]) {
-            window.dataLayer?.push(args);
-          };
-          window.gtag("js", new Date());
-          window.gtag("config", CONST_GA4_MEASUREMENT_ID, {
-            debug_mode: debugMode,
-          });
-        }}
-      />
-    </>
+    <Script
+      type="module"
+      src="/nextjs_migration/client/js/analytics.js"
+      nonce={nonce}
+    />
   ) : (
     <></>
   );
