dfda3e20ed1d1d84b14d50a910ee499ba78eebf3
Robert Helmer <rhelmer@mozilla.com>
2024-02-08 18:22:15 -0800

    temp disable

--- next.config.js
+++ next.config.js
@@ -8,10 +8,10 @@ import { withSentryConfig } from "@sentry/nextjs";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   productionBrowserSourceMaps: true,
-  sentry: {
-    disableServerWebpackPlugin: process.env.UPLOAD_SENTRY_SOURCEMAPS !== "true",
-    disableClientWebpackPlugin: process.env.UPLOAD_SENTRY_SOURCEMAPS !== "true",
-  },
+  // sentry: {
+    // disableServerWebpackPlugin: process.env.UPLOAD_SENTRY_SOURCEMAPS !== "true",
+    // disableClientWebpackPlugin: process.env.UPLOAD_SENTRY_SOURCEMAPS !== "true",
+  // },
   images: {
     remotePatterns: [
       {
