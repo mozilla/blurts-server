6ca632171853b3b184045a3fafa41ecf3895dc57
Kaitlyn <kandres@mozilla.com>
2024-02-20 05:43:45 -0500

    lint

--- src/app/(proper_react)/(redesign)/(authenticated)/user/(dashboard)/dashboard/fix/data-broker-profiles/manual-remove/RemovalCard.tsx
+++ src/app/(proper_react)/(redesign)/(authenticated)/user/(dashboard)/dashboard/fix/data-broker-profiles/manual-remove/RemovalCard.tsx
@@ -12,6 +12,7 @@ import { useState } from "react";
 import { getLocale } from "../../../../../../../../../functions/universal/getLocale";
 import { useTelemetry } from "../../../../../../../../../hooks/useTelemetry";
 import { ScanResultCard } from "../../../../../../../../../components/client/ScanResultCard";
+import { getExposureStatus } from "../../../../../../../../../components/server/StatusPill";
 
 export type Props = {
   scanResult: OnerepScanResultRow;
