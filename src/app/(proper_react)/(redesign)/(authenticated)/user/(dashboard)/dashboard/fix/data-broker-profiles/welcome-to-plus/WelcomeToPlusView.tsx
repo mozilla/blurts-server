b0c7dc05c3147e62949a59800cd0ac479b133af6
Florian Zia <zia.florian@gmail.com>
2024-03-06 18:09:21 +0100

    feat: Add welcome view zero state

--- src/app/(proper_react)/(redesign)/(authenticated)/user/(dashboard)/dashboard/fix/data-broker-profiles/welcome-to-plus/WelcomeToPlusView.tsx
+++ src/app/(proper_react)/(redesign)/(authenticated)/user/(dashboard)/dashboard/fix/data-broker-profiles/welcome-to-plus/WelcomeToPlusView.tsx
@@ -2,6 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+import Image from "next/image";
 import styles from "./welcomeToPlus.module.scss";
 import { PercentageChart } from "../../../../../../../../../components/client/PercentageChart";
 import {
@@ -15,6 +16,8 @@ import {
 import { FixView } from "../../FixView";
 import { ExtendedReactLocalization } from "../../../../../../../../../hooks/l10n";
 import { TelemetryButton } from "../../../../../../../../../components/client/TelemetryButton";
+import noBreachesIllustration from "../../images/high-risk-breaches-none.svg";
+import { CONST_ONEREP_DATA_BROKER_COUNT } from "../../../../../../../../../../constants";
 
 export type Props = {
   data: StepDeterminationData;
@@ -32,7 +35,8 @@ export function WelcomeToPlusView(props: Props) {
         (result.status === "optout_in_progress" ||
           result.status === "waiting_for_verification"),
     ) ?? [];
-  const countOfDataBrokerProfiles = scanResultsInProgress.length;
+  const scanResultsInProgressCount = scanResultsInProgress.length;
+  const showZeroState = scanResultsInProgressCount === 0;
   const summary = getDashboardSummary(
     scanResultsInProgress,
     props.data.subscriberBreaches,
@@ -52,29 +56,48 @@ export function WelcomeToPlusView(props: Props) {
             {l10n.getString(
               "welcome-to-premium-data-broker-profiles-title-part-one",
             )}
-            <br />
-            {l10n.getString(
-              "welcome-to-premium-data-broker-profiles-title-part-two",
+            {showZeroState && (
+              <>
+                <br />
+                {l10n.getString(
+                  "welcome-to-premium-data-broker-profiles-title-part-two",
+                )}
+              </>
             )}
           </h3>
           <p>
-            {l10n.getString(
-              "welcome-to-premium-data-broker-profiles-description-part-one",
-              {
-                profile_total_num: countOfDataBrokerProfiles,
-                exposure_reduction_percentage: dataPointReduction,
-              },
-            )}
+            {showZeroState
+              ? l10n.getString(
+                  "welcome-to-premium-data-broker-profiles-description-part-one",
+                  {
+                    profile_total_num: scanResultsInProgressCount,
+                    exposure_reduction_percentage: dataPointReduction,
+                  },
+                )
+              : l10n.getString(
+                  "welcome-to-premium-data-broker-profiles-zero-state-description-part-one",
+                  {
+                    data_broker_count: CONST_ONEREP_DATA_BROKER_COUNT,
+                  },
+                )}
           </p>
           <p>
-            {l10n.getString(
-              "welcome-to-premium-data-broker-profiles-description-part-two",
-            )}
+            {showZeroState
+              ? l10n.getString(
+                  "welcome-to-premium-data-broker-profiles-description-part-two",
+                )
+              : l10n.getString(
+                  "welcome-to-premium-data-broker-profiles-zero-state-description-part-two",
+                )}
           </p>
           <p>
-            {l10n.getString(
-              "welcome-to-premium-data-broker-profiles-description-part-three",
-            )}
+            {showZeroState
+              ? l10n.getString(
+                  "welcome-to-premium-data-broker-profiles-description-part-three",
+                )
+              : l10n.getString(
+                  "welcome-to-premium-data-broker-profiles-zero-state-description-part-three",
+                )}
           </p>
           <div className={styles.buttonsWrapper}>
             <TelemetryButton
@@ -95,9 +118,17 @@ export function WelcomeToPlusView(props: Props) {
             </TelemetryButton>
           </div>
         </div>
-        <div className={styles.chart}>
-          <PercentageChart exposureReduction={dataPointReduction} />
-        </div>
+        {showZeroState ? (
+          <div className={styles.chart}>
+            <PercentageChart exposureReduction={dataPointReduction} />
+          </div>
+        ) : (
+          <div
+            className={`${styles.illustrationWrapper} ${styles.hideOnMobile}`}
+          >
+            <Image src={noBreachesIllustration} alt="" />
+          </div>
+        )}
       </div>
     </FixView>
   );
