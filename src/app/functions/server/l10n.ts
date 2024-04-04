4f321e13f7d067d4bc431c103ee4e5a10d2c751b
Vincent <git@vincenttunru.com>
2024-04-04 22:40:47 +0200

    Make server components l10n use l10n abstraction

--- src/app/functions/server/l10n.ts
+++ src/app/functions/server/l10n.ts
@@ -4,6 +4,7 @@
 
 import "server-only";
 import { headers } from "next/headers";
+import { JSDOM } from "jsdom";
 import {
   GetL10nBundles,
   createGetL10nBundles,
@@ -59,81 +60,9 @@ export const getL10nBundles: GetL10nBundles = createGetL10nBundles({
   getAcceptLangHeader: () => headers().get("Accept-Language") ?? "en",
 });
 
-/**
- * Get the localisation sources for the locales relevant to the current user
- *
- * This function can run on the server side, and only returns serialisable data.
- * This means that it can either be used to construct a ReactLocalization object
- * on the server side, or be passed to Client Component to construct such an
- * object on the client side.
- *
- * @param userLocales The user's preferred locales, in the syntax of the Accept-Language HTTP header
- * @returns The sources for l10n bundles that can be used to construct a ReactLocalization object
- */
-// export function getL10nBundlesOld(
-//   userLocales = process.env.STORYBOOK === "true"
-//     ? "en"
-//     : headers().get("Accept-Language"),
-// ): LocaleData[] {
-//   const bundleSources: Record<string, string[]> = {};
-
-//   for (const filename of translationsContext.keys()) {
-//     // Filenames are formatted as `./<locale>/<module>.ftl`.
-//     // Example: ./en/bundle.ftl
-//     const locale = filename.split("/")[1];
-
-//     if (locale) {
-//       bundleSources[locale] ??= [];
-//       bundleSources[locale].push(loadSource(filename));
-
-//       if (locale === "en") {
-//         // `require` isn't usually valid JS, so skip type checking for that:
-//         // eslint-disable-next-line @typescript-eslint/no-explicit-any
-//         const pendingTranslationsContext = (require as any).context(
-//           "../../../../locales-pending",
-//           true,
-//           /\.ftl$/,
-//         );
-//         pendingTranslationsContext
-//           .keys()
-//           .forEach((pendingTranslationFilename: string) => {
-//             bundleSources.en.push(
-//               pendingTranslationsContext(pendingTranslationFilename),
-//             );
-//           });
-//       }
-//     }
-//   }
-
-//   const languages = userLocales ? acceptedLanguages(userLocales) : [];
-//   const supportedLocales = process.env.SUPPORTED_LOCALES?.split(",");
-//   const availableLocales = Object.keys(bundleSources);
-//   const filteredLocales =
-//     process.env.APP_ENV === "heroku"
-//       ? availableLocales
-//       : availableLocales.filter((locale) => supportedLocales?.includes(locale));
-//   const currentLocales = negotiateLanguages(languages, filteredLocales, {
-//     defaultLocale: "en",
-//   });
-
-//   const relevantBundleSources = currentLocales.map((relevantLocale) => ({
-//     locale: relevantLocale,
-//     bundleSources: bundleSources[relevantLocale],
-//   }));
-
-//   return relevantBundleSources;
-// }
-
-// In Storybook, the Fluent bundle is generated in the browser, so we don't need
-// to provide `parseMarkup` (and even can't, because JSDOM won't run there):
-const document =
-  process.env.STORYBOOK === "true"
-    ? // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      (undefined as any)
-    : // Using require here to conditionally load JSDOM without introducing asynchronicity (i.e. Promises):
-      // eslint-disable-next-line @typescript-eslint/no-var-requires
-      new (require("jsdom").JSDOM)().window.document;
+let document: Document;
 const parseMarkup: MarkupParser = (str) => {
+  document ??= new JSDOM().window.document;
   if (!str.includes("<") && !str.includes(">")) {
     return [{ nodeName: "#text", textContent: str } as Node];
   }
@@ -142,42 +71,8 @@ const parseMarkup: MarkupParser = (str) => {
   return Array.from(wrapper.childNodes);
 };
 
-// const bundles: Record<string, FluentBundle> = {};
-// function getBundle(localeData: LocaleData): FluentBundle {
-//   if (bundles[localeData.locale]) {
-//     return bundles[localeData.locale];
-//   }
-//   bundles[localeData.locale] = new FluentBundle(localeData.locale);
-//   localeData.bundleSources.forEach((bundleSource) => {
-//     bundles[localeData.locale].addResource(new FluentResource(bundleSource));
-//   });
-//   return bundles[localeData.locale];
-// }
-
 export const getL10n: GetL10n = createGetL10n({
   getL10nBundles: getL10nBundles,
   ReactLocalization: ReactLocalization,
   parseMarkup: parseMarkup,
 });
-
-// export function getL10nOld(
-//   localeData: LocaleData[] = getL10nBundles(),
-// ): ExtendedReactLocalization {
-//   const bundles: FluentBundle[] = localeData.map((data) => getBundle(data));
-
-//   // The ReactLocalization instance stores and caches the sequence of generated
-//   // bundles. You can store it in your app's state.
-//   const l10n = new ReactLocalization(
-//     bundles,
-//     process.env.STORYBOOK === "true" ? undefined : parseMarkup,
-//   );
-
-//   const getFragment: GetFragment = (id, args, fallback) =>
-//     l10n.getElement(createElement(Fragment, null, fallback ?? id), id, args);
-
-//   const extendedL10n: ExtendedReactLocalization =
-//     l10n as ExtendedReactLocalization;
-//   extendedL10n.getFragment = getFragment;
-
-//   return extendedL10n;
-// }
