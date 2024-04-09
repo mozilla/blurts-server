a0bfb578aba38c3f65d8bb82a2c7e4d1707bda2e
Joey Zhou <jozhou@mozilla.com>
2024-04-09 14:42:38 -0700

    fix: remove the two old columns

--- src/db/migrations/20240408161125_add_monthly_report_column.js
+++ src/db/migrations/20240408161125_add_monthly_report_column.js
@@ -4,12 +4,18 @@
 
 export function up (knex) {
   return knex.schema.table('subscribers', table => {
+    table.dropColumn("monthly_email_optout")
+    table.dropColumn("monthly_email_at")
     table.boolean('monthly_monitor_report').defaultTo(false)
+    table.timestamp('monthly_monitor_report_at')
   })
 }
 
 export function down (knex) {
   return knex.schema.table('subscribers', table => {
+    table.boolean('monthly_email_optout')
+    table.timestamp('monthly_email_at')
     table.dropColumn('monthly_monitor_report')
+    table.dropColumn("monthly_monitor_report_at")
   })
 }
