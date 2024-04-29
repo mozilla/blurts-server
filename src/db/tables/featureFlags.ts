50191f66d3869130c20a5fdbb000abfdd3729a0b
Kaitlyn <kandres@mozilla.com>
2024-04-29 11:41:26 -0400

    dont repeat feature falgs

--- src/db/tables/featureFlags.ts
+++ src/db/tables/featureFlags.ts
@@ -39,7 +39,6 @@ export type FeatureFlagName =
   | "RedesignedEmails"
   | "UpdatedEmailPreferencesOption"
   | "MonthlyActivityEmail"
-  | "RedesignedEmails"
   | "CsatSurvey"
   | "CancellationFlow"
   | "ConfirmCancellation"
