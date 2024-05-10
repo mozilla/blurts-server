9f6a204561ec08ad846936341d735b0d1a210f4e
Kaitlyn <kandres@mozilla.com>
2024-05-10 11:47:27 -0400

    pass getsubscriberbyid instead of session user

--- src/app/(proper_react)/(redesign)/(authenticated)/user/(dashboard)/settings/AlertAddressForm.tsx
+++ src/app/(proper_react)/(redesign)/(authenticated)/user/(dashboard)/settings/AlertAddressForm.tsx
@@ -30,9 +30,12 @@ import { useSession } from "next-auth/react";
 import { FeatureFlagName } from "../../../../../../../db/tables/featureFlags";
 import { Session } from "next-auth";
 import { hasPremium } from "../../../../../../functions/universal/user";
+import { useRouter } from "next/navigation";
+import { SubscriberRow } from "knex/types/tables";
 
 export type Props = {
   user: Session["user"];
+  subscriber: SubscriberRow;
   enabledFeatureFlags: FeatureFlagName[];
 };
 
@@ -41,14 +44,13 @@ const AlertAddressContext = createContext<RadioGroupState | null>(null);
 export const AlertAddressForm = (props: Props) => {
   const l10n = useL10n();
   const session = useSession();
+  const router = useRouter();
 
   // Set value to null if undefined (disabled breach alerts)
-  const breachAlertsEmailsAllowed =
-    props.user.subscriber?.all_emails_to_primary ?? null;
+  const breachAlertsEmailsAllowed = props.subscriber.all_emails_to_primary;
 
   // Have monitor monthly reports enabled by default
-  const monitorReportAllowed =
-    props.user.subscriber?.monthly_monitor_report ?? true;
+  const monitorReportAllowed = props.subscriber.monthly_monitor_report;
 
   const defaultActivateAlertEmail =
     typeof breachAlertsEmailsAllowed === "boolean";
@@ -58,6 +60,9 @@ export const AlertAddressForm = (props: Props) => {
 
   const [activateMonthlyMonitorReport, setActivateMonthlyMonitorReport] =
     useState(monitorReportAllowed);
+
+  console.log({ monitorReportAllowed });
+  console.log({ activateMonthlyMonitorReport });
 
   const commsValue = () => {
     switch (breachAlertsEmailsAllowed) {
@@ -85,6 +90,12 @@ export const AlertAddressForm = (props: Props) => {
         // Fetch a new token with up-to-date subscriber info - specifically,
         // with this setting updated.
         void session.update();
+        // Make sure the dashboard re-fetches the breaches on the next visit,
+        // in order to make resolved breaches move to the "Fixed" tab.
+        // If we had used server actions, we could've called
+        // `revalidatePath("/user/dashboard")` there, but the API doesn't appear
+        // to necessarily share a cache with the client.
+        router.refresh();
       });
     },
   });
@@ -100,6 +111,13 @@ export const AlertAddressForm = (props: Props) => {
       }),
     }).then(() => {
       void session.update();
+      // Make sure the dashboard re-fetches the breaches on the next visit,
+      // in order to make resolved breaches move to the "Fixed" tab.
+      // If we had used server actions, we could've called
+      // `revalidatePath("/user/dashboard")` there, but the API doesn't appear
+      // to necessarily share a cache with the client.
+      router.refresh();
+      console.log("i refreshed");
     });
   };
 
@@ -113,6 +131,12 @@ export const AlertAddressForm = (props: Props) => {
       }
       return newValue;
     });
+    // Make sure the dashboard re-fetches the breaches on the next visit,
+    // in order to make resolved breaches move to the "Fixed" tab.
+    // If we had used server actions, we could've called
+    // `revalidatePath("/user/dashboard")` there, but the API doesn't appear
+    // to necessarily share a cache with the client.
+    router.refresh();
   };
 
   const { radioGroupProps, labelProps } = useRadioGroup(
