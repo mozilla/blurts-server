0d60a136158e67740a38e546a391cd7e170565c6
Robert Helmer <rhelmer@mozilla.com>
2024-05-13 22:14:11 -0700

    complete comment thought

--- next.config.js
+++ next.config.js
@@ -179,7 +179,7 @@ const nextConfig = {
     // Without this setting, Next.js has Webpack trying and failing to load
     // uglify-js when compiling MJML email templates to HTML in `renderEmail.ts`:
     serverComponentsExternalPackages: ["mjml"],
-    // Sentry
+    // Sentry 8.x requires `instrumentation.ts` vs. it's previous custom approach.
     instrumentationHook: true,
   },
 };
