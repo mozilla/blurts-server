27a316e22f327313e73edfc73d2b73057bb761a9
James Espy <jamesespy@R0X44KL2QN.local>
2024-05-17 14:03:23 -0600

    added styles to text

--- src/app/(proper_react)/(redesign)/(public)/how-it-works/HowItWorksView.tsx
+++ src/app/(proper_react)/(redesign)/(public)/how-it-works/HowItWorksView.tsx
@@ -22,13 +22,23 @@ export const HowItWorksView = () => {
 
   return (
     <main>
-      <header className="styles.title">
-        <h1>{l10n.getString("how-it-works-banner-title")}</h1>
+      <header className={styles.header}>
+        <h1 className={styles.title}>
+          {l10n.getString("how-it-works-banner-title")}
+        </h1>
         <p>
           {l10n.getFragment("how-it-works-banner-text", {
             elems: {
-              data_brokers_link: <a href="/data-brokers">data brokers</a>,
-              data_breaches_link: <a href="/data-breach">data breaches</a>,
+              data_brokers_link: (
+                <a href="/data-brokers" className={styles.brokersEmphasis}>
+                  data brokers
+                </a>
+              ),
+              data_breaches_link: (
+                <a href="/data-breach" className={styles.breachesEmphasis}>
+                  data breaches
+                </a>
+              ),
             },
           })}
         </p>
@@ -40,47 +50,53 @@ export const HowItWorksView = () => {
           <h2>
             {l10n.getFragment("section-1-intro-title", {
               elems: {
-                data_brokers: <span className={styles.emphasis} />,
+                data_brokers: <span className={styles.brokersEmphasis} />,
               },
             })}
           </h2>
           <p>{l10n.getString("section-1-intro-text-1")}</p>
           <hr className={styles.horizontalRule} />
-          <p>{l10n.getString("section-1-intro-text-2")}</p>
+          <h3 className={styles.introCTA}>
+            {l10n.getString("section-1-intro-text-2")}
+          </h3>
           <Button variant="primary">
             {l10n.getString("section-1-intro-cta-button")}
           </Button>
         </div>
         {/* STEP 1 */}
         <div className={styles.step}>
-          <h3>{l10n.getString("section-1-step-1-title")}</h3>
-          <h3>{l10n.getString("section-1-step-1-subtitle")}</h3>
+          <h3 className={`${styles.brokersEmphasis} ${styles.stepTitle}`}>
+            {l10n.getString("section-1-step-1-title")}
+          </h3>
+          <h3 className={styles.stepSubtitle}>
+            {l10n.getString("section-1-step-1-subtitle")}
+          </h3>
           <div>
             <Image src={NameIcon} alt="name icon" />
-            <p>
+            <p className={styles.stepBody}>
               {l10n.getFragment("section-1-step-1-name", {
                 elems: {
-                  name: <span className={styles.bolded} />,
+                  name: <span className={styles.boldedText} />,
                 },
               })}
             </p>
           </div>
           <div>
             <Image src={LocationIcon} alt="location icon" />
-            <p>
+            <p className={styles.stepBody}>
               {l10n.getFragment("section-1-step-1-location", {
                 elems: {
-                  location: <span className={styles.bolded} />,
+                  location: <span className={styles.boldedText} />,
                 },
               })}
             </p>
           </div>
           <div>
             <Image src={DOBIcon} alt="dob icon" />
-            <p>
+            <p className={styles.stepBody}>
               {l10n.getFragment("section-1-step-1-dob", {
                 elems: {
-                  dob: <span className={styles.bolded} />,
+                  dob: <span className={styles.boldedText} />,
                 },
               })}
             </p>
@@ -91,53 +107,93 @@ export const HowItWorksView = () => {
         {/* STEP 2 */}
         <div className={styles.step}>
           <Image src={RemoveStep2} alt="step 2 screenshot" />
-          <h3>{l10n.getString("section-1-step-2-title")}</h3>
-          <h3>{l10n.getString("section-1-step-2-subtitle")}</h3>
-          <p>{l10n.getString("section-1-step-2-text-1")}</p>
-          <p>{l10n.getString("section-1-step-2-text-2")}</p>
-          <p>{l10n.getString("section-1-step-2-text-3")}</p>
+          <h3 className={`${styles.brokersEmphasis} ${styles.stepTitle}`}>
+            {l10n.getString("section-1-step-2-title")}
+          </h3>
+          <h3 className={styles.stepSubtitle}>
+            {l10n.getString("section-1-step-2-subtitle")}
+          </h3>
+          <p className={styles.stepBody}>
+            {l10n.getString("section-1-step-2-text-1")}
+          </p>
+          <p className={styles.stepBody}>
+            {l10n.getString("section-1-step-2-text-2")}
+          </p>
+          <p className={styles.stepBody}>
+            {l10n.getString("section-1-step-2-text-3")}
+          </p>
         </div>
         {/* STEP 3 */}
         <div className={styles.step}>
-          <h3>{l10n.getString("section-1-step-3-title")}</h3>
-          <h3>{l10n.getString("section-1-step-3-subtitle")}</h3>
-          <p>{l10n.getString("section-1-step-3-text-1")}</p>
-          <p>{l10n.getString("section-1-step-3-text-2")}</p>
+          <h3 className={`${styles.brokersEmphasis} ${styles.stepTitle}`}>
+            {l10n.getString("section-1-step-3-title")}
+          </h3>
+          <h3 className={styles.stepSubtitle}>
+            {l10n.getString("section-1-step-3-subtitle")}
+          </h3>
+          <p className={styles.stepBody}>
+            {l10n.getString("section-1-step-3-text-1")}
+          </p>
+          <p className={styles.stepBody}>
+            {l10n.getString("section-1-step-3-text-2")}
+          </p>
           <Image src={RemoveStep3} alt="step 3 screenshot" />
         </div>
       </div>
 
       {/* SECTION: WE PROVIDE STEPS TO HELP RESOLVE DATA BREACHES */}
-      <div className={styles.removeFromDataBrokersWrapper}>
+      <div className={styles.resolveDataBreachesWrapper}>
         <div className={styles.header}>
           <h2>
             {l10n.getFragment("section-2-intro-title", {
               elems: {
-                data_breaches: <span className={styles.emphasis} />,
+                data_breaches: <span className={styles.breachesEmphasis} />,
               },
             })}
           </h2>
           <p>{l10n.getString("section-2-intro-text-1")}</p>
           <hr className={styles.horizontalRule} />
-          <p>{l10n.getString("section-2-intro-text-2")}</p>
+          <h3 className={styles.introCTA}>
+            {l10n.getString("section-2-intro-text-2")}
+          </h3>
           <Button variant="primary">
             {l10n.getString("section-2-intro-cta-button")}
           </Button>
         </div>
         {/* STEP 1 */}
         <div className={styles.stepGroup}>
-          <h3>{l10n.getString("section-2-step-1-title")}</h3>
-          <h3>{l10n.getString("section-2-step-1-subtitle")}</h3>
-          <p>{l10n.getString("section-2-step-1-text")}</p>
+          <h3 className={`${styles.breachesEmphasis} ${styles.stepTitle}`}>
+            {l10n.getString("section-2-step-1-title")}
+          </h3>
+          <h3 className={styles.stepSubtitle}>
+            {l10n.getString("section-2-step-1-subtitle")}
+          </h3>
+          <p className={styles.stepBody}>
+            {l10n.getString("section-2-step-1-text")}
+          </p>
           {/* STEP 2 */}
-          <h3>{l10n.getString("section-2-step-2-title")}</h3>
-          <h3>{l10n.getString("section-2-step-2-subtitle")}</h3>
-          <p>{l10n.getString("section-2-step-2-text-1")}</p>
-          <p>{l10n.getString("section-2-step-2-text-2")}</p>
+          <h3 className={`${styles.breachesEmphasis} ${styles.stepTitle}`}>
+            {l10n.getString("section-2-step-2-title")}
+          </h3>
+          <h3 className={styles.stepSubtitle}>
+            {l10n.getString("section-2-step-2-subtitle")}
+          </h3>
+          <p className={styles.stepBody}>
+            {l10n.getString("section-2-step-2-text-1")}
+          </p>
+          <p className={styles.stepBody}>
+            {l10n.getString("section-2-step-2-text-2")}
+          </p>
           {/* STEP 3 */}
-          <h3>{l10n.getString("section-2-step-3-title")}</h3>
-          <h3>{l10n.getString("section-2-step-3-subtitle")}</h3>
-          <p>{l10n.getString("section-2-step-3-text")}</p>
+          <h3 className={`${styles.breachesEmphasis} ${styles.stepTitle}`}>
+            {l10n.getString("section-2-step-3-title")}
+          </h3>
+          <h3 className={styles.stepSubtitle}>
+            {l10n.getString("section-2-step-3-subtitle")}
+          </h3>
+          <p className={styles.stepBody}>
+            {l10n.getString("section-2-step-3-text")}
+          </p>
         </div>
         <Image src={Resolve} alt="resolve data breaches screenshot" />
 
@@ -147,8 +203,8 @@ export const HowItWorksView = () => {
       </div>
 
       {/* FOOTER */}
-      <footer>
-        <h2>{l10n.getString("footer-title")}</h2>
+      <footer className={styles.footer}>
+        <h2 className={styles.footerCTA}>{l10n.getString("footer-title")}</h2>
         <div>
           <input
             placeholder={l10n.getString("footer-input-placeholder")}
@@ -159,7 +215,9 @@ export const HowItWorksView = () => {
           <Button variant="primary">
             {l10n.getString("footer-cta-button")}
           </Button>
-          <p>{l10n.getString("footer-have-i-been-pwned")}</p>
+          <p className={styles.note}>
+            {l10n.getString("footer-have-i-been-pwned")}
+          </p>
         </div>
       </footer>
     </main>
