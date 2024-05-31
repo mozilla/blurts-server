7009a150d4d3b8313f0696fd28c0ebe39f9c194e
James Espy <jamesespy@R0X44KL2QN.local>
2024-05-31 12:50:29 -0600

    removed dev code

--- src/app/(proper_react)/(redesign)/(public)/how-it-works/page.tsx
+++ src/app/(proper_react)/(redesign)/(public)/how-it-works/page.tsx
@@ -6,14 +6,10 @@ import { headers } from "next/headers";
 import { View } from "./HowItWorksView";
 import { getCountryCode } from "../../../../functions/server/getCountryCode";
 import { redirect } from "next/navigation";
-import { getServerSession } from "../../../../functions/server/getServerSession";
 
-export default async function Page() {
+export default function Page() {
   const headersList = headers();
   const countryCode = getCountryCode(headersList);
-
-  const session = await getServerSession();
-  console.log("user", session);
 
   if (countryCode !== "us") {
     return redirect("/");
