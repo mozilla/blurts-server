e6d5fb9a5ebd9420b3fa4fb90aed9c2acfb3a955
James Espy <jamesespy@R0X44KL2QN.local>
2024-06-05 13:20:20 -0600

    updated link syntax to use fluent string

--- src/app/(proper_react)/(redesign)/(authenticated)/user/(dashboard)/dashboard/fix/data-broker-profiles/welcome-to-plus/WelcomeToPlusView.tsx
+++ src/app/(proper_react)/(redesign)/(authenticated)/user/(dashboard)/dashboard/fix/data-broker-profiles/welcome-to-plus/WelcomeToPlusView.tsx
@@ -89,9 +89,7 @@ export function WelcomeToPlusView(props: Props) {
                           href="/how-it-works"
                           className={styles.howItWorksLink}
                           target="_blank"
-                        >
-                          Removals typically take 7-14 days
-                        </a>
+                        />
                       ),
                     },
                   },
