156d06b13790cd9c78aa65d57bee96c80aa69f0a
Robert Helmer <rhelmer@mozilla.com>
2024-06-11 14:06:44 -0700

    import constant

--- src/app/functions/server/getStuckRemovals.ts
+++ src/app/functions/server/getStuckRemovals.ts
@@ -2,6 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+import { CONST_DAY_MILLISECONDS } from "../../../constants";
 import { getAllScanResults } from "../../../db/tables/onerep_scans";
 import { getAllDataBrokers } from "./onerep";
 import { refreshStoredScanResults } from "./refreshStoredScanResults";
