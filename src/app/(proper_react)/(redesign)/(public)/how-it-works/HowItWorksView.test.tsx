85f352020c455ad85c6c94cfe71d840cbb318ac3
Robert Helmer <robert@roberthelmer.com>
2024-06-12 09:38:15 -0700

    Revert "added test for data removal buttons"
    This reverts commit 49ef85d00bdddb0a4cebc6422768f0d8cc822c21.

--- src/app/(proper_react)/(redesign)/(public)/how-it-works/HowItWorksView.test.tsx
+++ src/app/(proper_react)/(redesign)/(public)/how-it-works/HowItWorksView.test.tsx
@@ -11,7 +11,7 @@ import {
   //  queryByRole,
   //  queryByText,
   render,
-  screen,
+  //  screen,
   //  within,
 } from "@testing-library/react";
 //  import { userEvent } from "@testing-library/user-event";
@@ -27,44 +27,12 @@ describe("How it works page", () => {
     expect(await axe(container)).toHaveNoViolations();
   });
 
-  it("Data Removal buttons enter user into premium subscription flow", () => {
-    //  https://accounts.firefox.com/subscriptions/products/prod_OiV9RSaatywSRy?plan=price_1Nv4ODJNcmPzuWtRoYpoFHXd
-    const ComposedPage = composeStory(HowItWorks, Meta);
-    render(<ComposedPage />);
-    const dataRemovalBtns = screen.getAllByRole("link", {
-      name: "Get data removal",
-    });
-    const dataRemovalBtn1 = dataRemovalBtns[0];
-    const dataRemovalBtn2 = dataRemovalBtns[1];
-    expect(dataRemovalBtn1).toHaveAttribute(
-      "href",
-      "https://accounts.firefox.com/subscriptions/products/prod_OiV9RSaatywSRy?plan=price_1Nv4ODJNcmPzuWtRoYpoFHXd",
-    );
-    expect(dataRemovalBtn2).toHaveAttribute(
-      "href",
-      "https://accounts.firefox.com/subscriptions/products/prod_OiV9RSaatywSRy?plan=price_1Nv4ODJNcmPzuWtRoYpoFHXd",
-    );
-  });
+  it("Data Removal button enters user into premium subscription flow", () => {});
 
   it("Free Scan button enters user into account.mozilla.com", () => {});
 
   it("Only shows to users in US", () => {});
 });
-
-// it("opens the see all FAQ link into a new page", async () => {
-//   const user = userEvent.setup();
-//   const ComposedDashboard = composeStory(LandingUs, Meta);
-//   render(<ComposedDashboard />);
-
-//   const seeAllFaqBtn = screen.getByRole("link", { name: "See all FAQs" });
-//   await user.click(seeAllFaqBtn);
-//   expect(seeAllFaqBtn).toHaveAttribute("target", "_blank");
-
-//   // jsdom will complain about not being able to navigate to a different page
-//   // after clicking the link; suppress that error, as it's not relevant to the
-//   // test:
-//   jest.spyOn(console, "error").mockImplementationOnce(() => undefined);
-// });
 
 // it("shows a 'Sign In' button in the header if the user is not signed in", async () => {
 //   const ComposedDashboard = composeStory(LandingNonUs, Meta);
