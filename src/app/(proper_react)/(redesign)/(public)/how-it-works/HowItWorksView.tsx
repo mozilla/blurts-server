ccba4fb919b2b93a5c91256a9100a869629839cf
Robert Helmer <robert@roberthelmer.com>
2024-06-12 09:38:15 -0700

    Revert "section 2 styles"
    This reverts commit 2a63d9f5df8866066bc8a481dfc5ead9fb044884.

--- src/app/(proper_react)/(redesign)/(public)/how-it-works/HowItWorksView.tsx
+++ src/app/(proper_react)/(redesign)/(public)/how-it-works/HowItWorksView.tsx
@@ -151,83 +151,67 @@ export const HowItWorksView = () => {
           </div>
           <Image src={RemoveStep3} alt="step 3 screenshot" />
         </div>
-        <span className={styles.sectionCTAButton}>
-          <Button variant="primary">
-            {l10n.getString("section-1-cta-button")}
-          </Button>
-        </span>
       </div>
 
       {/* SECTION: WE PROVIDE STEPS TO HELP RESOLVE DATA BREACHES */}
       <div className={styles.sectionWrapper}>
-        <div className={styles.intro}>
-          <span className={styles.introContent}>
-            <h2>
-              {l10n.getFragment("section-2-intro-title", {
-                elems: {
-                  data_breaches: <span className={styles.breachesEmphasis} />,
-                },
-              })}
-            </h2>
-            <p>{l10n.getString("section-2-intro-text-1")}</p>
-            <hr className={styles.horizontalRule} />
-          </span>
-          <div className={styles.introCTA}>
-            <h3>{l10n.getString("section-2-intro-text-2")}</h3>
-            <Button variant="primary">
-              {l10n.getString("section-2-intro-cta-button")}
-            </Button>
-          </div>
-        </div>
-        <div className={styles.step}>
-          <div className={styles.stepTextContainer}>
-            {/* STEP 1 */}
-            <h3 className={`${styles.breachesEmphasis} ${styles.stepTitle}`}>
-              {l10n.getString("section-2-step-1-title")}
-            </h3>
-            <h3 className={styles.stepSubtitle}>
-              {l10n.getString("section-2-step-1-subtitle")}
-            </h3>
-            <p className={styles.stepBody}>
-              {l10n.getString("section-2-step-1-text")}
-            </p>
-            {/* STEP 2 */}
-            <div className={styles.stepGroupTitleAfter1st}>
-              <h3
-                className={`${styles.breachesEmphasis} ${styles.stepTitle} ${styles.stepGroupTitleAfter1st}`}
-              >
-                {l10n.getString("section-2-step-2-title")}
-              </h3>
-              <h3 className={styles.stepSubtitle}>
-                {l10n.getString("section-2-step-2-subtitle")}
-              </h3>
-              <p className={styles.stepBody}>
-                {l10n.getString("section-2-step-2-text-1")}
-              </p>
-              <p className={styles.stepBody}>
-                {l10n.getString("section-2-step-2-text-2")}
-              </p>
-            </div>
-            {/* STEP 3 */}
-            <div className={styles.stepGroupTitleAfter1st}>
-              <h3 className={`${styles.breachesEmphasis} ${styles.stepTitle}`}>
-                {l10n.getString("section-2-step-3-title")}
-              </h3>
-              <h3 className={styles.stepSubtitle}>
-                {l10n.getString("section-2-step-3-subtitle")}
-              </h3>
-              <p className={styles.stepBody}>
-                {l10n.getString("section-2-step-3-text")}
-              </p>
-            </div>
-          </div>
-          <Image src={Resolve} alt="resolve data breaches screenshot" />
-        </div>
-        <span className={styles.sectionCTAButton}>
+        <div className={styles.header}>
+          <h2>
+            {l10n.getFragment("section-2-intro-title", {
+              elems: {
+                data_breaches: <span className={styles.breachesEmphasis} />,
+              },
+            })}
+          </h2>
+          <p>{l10n.getString("section-2-intro-text-1")}</p>
+          <hr className={styles.horizontalRule} />
+          <h3 className={styles.introCTA}>
+            {l10n.getString("section-2-intro-text-2")}
+          </h3>
           <Button variant="primary">
-            {l10n.getString("section-2-cta-button")}
+            {l10n.getString("section-2-intro-cta-button")}
           </Button>
-        </span>
+        </div>
+        {/* STEP 1 */}
+        <div className={styles.stepGroup}>
+          <h3 className={`${styles.breachesEmphasis} ${styles.stepTitle}`}>
+            {l10n.getString("section-2-step-1-title")}
+          </h3>
+          <h3 className={styles.stepSubtitle}>
+            {l10n.getString("section-2-step-1-subtitle")}
+          </h3>
+          <p className={styles.stepBody}>
+            {l10n.getString("section-2-step-1-text")}
+          </p>
+          {/* STEP 2 */}
+          <h3 className={`${styles.breachesEmphasis} ${styles.stepTitle}`}>
+            {l10n.getString("section-2-step-2-title")}
+          </h3>
+          <h3 className={styles.stepSubtitle}>
+            {l10n.getString("section-2-step-2-subtitle")}
+          </h3>
+          <p className={styles.stepBody}>
+            {l10n.getString("section-2-step-2-text-1")}
+          </p>
+          <p className={styles.stepBody}>
+            {l10n.getString("section-2-step-2-text-2")}
+          </p>
+          {/* STEP 3 */}
+          <h3 className={`${styles.breachesEmphasis} ${styles.stepTitle}`}>
+            {l10n.getString("section-2-step-3-title")}
+          </h3>
+          <h3 className={styles.stepSubtitle}>
+            {l10n.getString("section-2-step-3-subtitle")}
+          </h3>
+          <p className={styles.stepBody}>
+            {l10n.getString("section-2-step-3-text")}
+          </p>
+        </div>
+        <Image src={Resolve} alt="resolve data breaches screenshot" />
+
+        <Button variant="primary">
+          {l10n.getString("section-2-cta-button")}
+        </Button>
       </div>
 
       {/* FOOTER */}
