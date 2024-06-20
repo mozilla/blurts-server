06be0cb68f82da4f2b09c5547fa4333d542d61e9
mansaj <mansaj@joeyzhou.com>
2024-06-20 08:50:22 -0700

    Revert "Fixed status label info string (MNTOR-3322)"

--- src/app/components/client/ExposuresFilterExplainer.tsx
+++ src/app/components/client/ExposuresFilterExplainer.tsx
@@ -119,7 +119,7 @@ export const ExposuresFilterStatusExplainer = (
             ) : (
               <li className={styles.statusListItem}>
                 <StatusPill type="fixed" />
-                {l10n.getString("modal-exposure-indicator-fixed")}
+                {l10n.getString("modal-exposure-indicator-removed")}
               </li>
             )}
             <li className={styles.statusListItem}>
