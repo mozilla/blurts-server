54d09d3b1db80f0229f5f82f23213a6b07c890d0
Kaitlyn <kandres@mozilla.com>
2024-06-25 13:17:51 -0400

    simplify telemetry event name into one line

--- src/app/(proper_react)/(redesign)/(authenticated)/user/(dashboard)/settings/CancelFlow.tsx
+++ src/app/(proper_react)/(redesign)/(authenticated)/user/(dashboard)/settings/CancelFlow.tsx
@@ -181,15 +181,6 @@ ${styles.staticAlternative}
     );
   };
 
-  const dialogDismissalTelemetryId = (): string => {
-    switch (step) {
-      case "all-set":
-        return "exited_youre_all_set";
-      default:
-        return "exited_cancel_flow";
-    }
-  };
-
   return (
     <>
       <Button
@@ -209,7 +200,9 @@ ${styles.staticAlternative}
             title={l10n.getString(dialogTitle())}
             illustration={<Animation />}
             onDismiss={() => dialogState.close()}
-            dismissalTelemetryId={dialogDismissalTelemetryId()}
+            dismissalTelemetryId={
+              step === "all-set" ? "exited_youre_all_set" : "exited_cancel_flow"
+            }
           >
             <div className={styles.contentWrapper}>
               {step === "confirm" && (
