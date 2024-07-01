feaf567e086b382c649ee7b7af7d62e75f53ce4a
Mukhamediyar Kudaikulov <mukhamediyarkudaikulov@moco.lan>
2024-07-01 03:45:51 -0700

    removed database retrieval from mock hibp endpoint

--- src/app/api/mock/hibp/breaches/route.ts
+++ src/app/api/mock/hibp/breaches/route.ts
@@ -6,31 +6,16 @@ import { NextResponse } from "next/server";
 import { logger } from "../../../../functions/server/logging";
 import mockAllBreaches from "../data/mockAllBreaches.json";
 import { errorIfProduction } from "../../utils/errorThrower";
-import { getBreaches } from "../../../../functions/server/getBreaches";
 import { Breach } from "../../../../functions/universal/breach";
 import { HibpLikeDbBreach } from "../../../../../utils/hibp";
 
 type BreachesListResponse = (Breach | HibpLikeDbBreach)[];
 
-export async function GET() {
+export function GET() {
   const prodError = errorIfProduction();
   if (prodError) return prodError;
 
   logger.info("Mock endpoint: /breaches");
 
-  // This fails when creating a PR
-  // let allBreaches = await getBreaches();
-  // if (allBreaches.length === 0) {
-  //   allBreaches = mockAllBreaches.data as BreachesListResponse;
-  // }
-
-  try {
-    let allBreaches = await getBreaches();
-    if (allBreaches.length === 0) {
-      allBreaches = mockAllBreaches.data as BreachesListResponse;
-    }
-    return NextResponse.json(allBreaches);
-  } catch {
-    return NextResponse.json(mockAllBreaches.data as BreachesListResponse);
-  }
+  return NextResponse.json(mockAllBreaches.data as BreachesListResponse);
 }
