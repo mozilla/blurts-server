40cfa1b76622c3cc5e67d39a463f621feca2df56
Kaitlyn <kandres@mozilla.com>
2024-07-23 09:56:34 -0400

    formatdate into ts

---
+++ src/utils/formatDate.ts
@@ -0,0 +1,28 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+// TODO: Add unit test when changing this code:
+/* c8 ignore next 8 */
+
+type DateTimeFormatOptions = {
+  year: "numeric";
+  month: "long";
+  day: "numeric";
+};
+
+function formatDate(date: string, locales: string): string {
+  const jsDate = new Date(date);
+  /** @type {{ year: 'numeric', month: 'long', day: 'numeric' }} */
+
+  const options: DateTimeFormatOptions = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+  const intlDateTimeFormatter = new Intl.DateTimeFormat(locales, options);
+
+  return intlDateTimeFormatter.format(jsDate);
+}
+
+export { formatDate };
