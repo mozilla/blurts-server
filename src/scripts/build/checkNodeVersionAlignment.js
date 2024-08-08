2cf4e7c0589cc9aeee64166ed83f857ce61f4de8
Vincent <git@vincenttunru.com>
2024-08-08 11:39:28 +0200

    Apply Prettier to checkNodeVersionAlignment
    Now that it's inside /src/, Prettier thinks it's relevant.

--- src/scripts/build/checkNodeVersionAlignment.js
+++ src/scripts/build/checkNodeVersionAlignment.js
@@ -30,26 +30,33 @@ const dockerFileNodeVersion = (await load("Dockerfile"))
 //   environment = { NODE_VERSION = "20.12.0", NPM_VERSION = "10.2.4" }
 const netlifyNodeVersion = (await load("netlify.toml"))
   .split("\n")
-  .find(line => line.includes('NODE_VERSION = "'))
+  .find((line) => line.includes('NODE_VERSION = "'))
   .split('NODE_VERSION = "')[1]
   .split('", NPM_VERSION')[0];
 
 const esbuildVersion = (await load("esbuild.cronjobs.js"))
   .split("\n")
-  .find(line => line.includes("target:"))
+  .find((line) => line.includes("target:"))
   .split('"node')[1]
   .split('",')[0];
 
-const ghaWorkflowPaths = await readdir(resolve(__dirname, "../../../.github/workflows"));
+const ghaWorkflowPaths = await readdir(
+  resolve(__dirname, "../../../.github/workflows"),
+);
 const ghaWorkflows = await Promise.all(
-  ghaWorkflowPaths.map(async (workflowFileName) =>
-    [workflowFileName, await load(`.github/workflows/${workflowFileName}`)],
-  ),
+  ghaWorkflowPaths.map(async (workflowFileName) => [
+    workflowFileName,
+    await load(`.github/workflows/${workflowFileName}`),
+  ]),
 );
 const ghaVersions = ghaWorkflows.flatMap(([filename, contents]) => {
-  return contents.split("\n")
-    .filter(line => line.includes("node-version:"))
-    .map(line => [filename, line.split("node-version: ")[1].replaceAll("'", "")]);
+  return contents
+    .split("\n")
+    .filter((line) => line.includes("node-version:"))
+    .map((line) => [
+      filename,
+      line.split("node-version: ")[1].replaceAll("'", ""),
+    ]);
 });
 
 const getMinorOnly = (version) => {
@@ -59,15 +66,24 @@ const getMinorOnly = (version) => {
 const checkVersion = (source, version) => {
   const voltaVersion = packageJson.volta.node;
   if (getMinorOnly(voltaVersion) !== getMinorOnly(version)) {
-    console.error(`Error: version [${version}] in ${source} does not match the Volta version ([${voltaVersion}]).`)
+    console.error(
+      `Error: version [${version}] in ${source} does not match the Volta version ([${voltaVersion}]).`,
+    );
     process.exit(1);
   }
 };
 
 checkVersion("the package.json engines", packageJson.engines.node);
-checkVersion("the package-lock.json engines", lockFile.packages[""].engines.node);
+checkVersion(
+  "the package-lock.json engines",
+  lockFile.packages[""].engines.node,
+);
 checkVersion("the Dockerfile", dockerFileNodeVersion);
 checkVersion("netlify.toml", netlifyNodeVersion);
 checkVersion("esbuild.cronjobs.json", esbuildVersion);
-ghaVersions.forEach(([workflow, version]) => checkVersion(`.github/workflows/${workflow}`, version));
-console.log(`Node version consistently set to [${getMinorOnly(packageJson.volta.node)}] everywhere!`);
+ghaVersions.forEach(([workflow, version]) =>
+  checkVersion(`.github/workflows/${workflow}`, version),
+);
+console.log(
+  `Node version consistently set to [${getMinorOnly(packageJson.volta.node)}] everywhere!`,
+);
