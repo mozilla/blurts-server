364227152ffe9afa32af8c31f203195345078674
Joey Zhou <jozhou@mozilla.com>
2024-08-09 10:16:18 -0700

    feat: parse redis url string

--- src/db/redis/configuration.ts
+++ src/db/redis/configuration.ts
@@ -2,8 +2,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+const url = new URL(process.env.REDIS_URL || "");
 export const redisConfiguration = {
-  host: process.env.REDIS_URL || "redis-mock",
-  password: process.env.REDIS_PASSWORD || "",
-  port: Number(process.env.REDIS_PORT) || 6379,
+  host: url.hostname || "redis-mock",
+  password: url.password || "",
+  port: Number(url.port) || 6379,
 };
