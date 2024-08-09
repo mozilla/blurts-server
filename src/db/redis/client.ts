25e500f005db0364e49e6ca40d8fd4452af4d529
Joey Zhou <jozhou@mozilla.com>
2024-08-09 09:47:16 -0700

    feat: redis mock client

--- src/db/redis/client.ts
+++ src/db/redis/client.ts
@@ -2,12 +2,20 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+import { logger } from "@sentry/utils";
 import { createRedisInstance } from "./util";
+import { createRedisMockInstance } from "./util-mock";
 import type { Redis } from "ioredis";
 
 let cached: Redis;
 
 export const redisClient = () => {
-  if (!cached) cached = createRedisInstance();
+  if (process.env.REDIS_URL?.includes("redis-mock")) {
+    cached = createRedisMockInstance();
+    logger.info("redis_mock_client_created_success");
+  } else {
+    cached = createRedisInstance();
+    logger.info("redis_client_created_success");
+  }
   return cached;
 };
--- src/db/redis/util-mock.ts
+++ src/db/redis/util-mock.ts
@@ -2,8 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-export const redisConfiguration = {
-  host: process.env.REDIS_URL || "redis-mock",
-  password: process.env.REDIS_PASSWORD || "",
-  port: Number(process.env.REDIS_PORT) || 6379,
-};
+import Redis from "ioredis-mock";
+
+export function createRedisMockInstance() {
+  return new Redis();
+}
