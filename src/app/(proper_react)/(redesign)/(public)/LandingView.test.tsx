e1aa173e7184f4fc022368478e018313db1813b4
Vincent <git@vincenttunru.com>
2024-08-26 15:26:15 +0200

    Rename "All breaches" to "Recent data breaches"

--- src/app/(proper_react)/(redesign)/(public)/LandingView.test.tsx
+++ src/app/(proper_react)/(redesign)/(public)/LandingView.test.tsx
@@ -820,14 +820,16 @@ describe("When Premium is available", () => {
     );
   });
 
-  it("counts the number of clicks All breaches link in top navbar", async () => {
+  it("counts the number of clicks on the Recent data breaches link in top navbar", async () => {
     const mockedRecord = useTelemetry();
     const ComposedDashboard = composeStory(LandingUs, Meta);
     render(<ComposedDashboard />);
 
     const user = userEvent.setup();
 
-    const navbarLink = screen.getByRole("link", { name: "All breaches" });
+    const navbarLink = screen.getByRole("link", {
+      name: "Recent data breaches",
+    });
     // jsdom will complain about not being able to navigate to a different page
     // after clicking the link; suppress that error, as it's not relevant to the
     // test:
--- src/app/(proper_react)/(redesign)/(public)/LandingView.tsx
+++ src/app/(proper_react)/(redesign)/(public)/LandingView.tsx
@@ -274,7 +274,7 @@ export const TopNavBar = ({ l10n }: { l10n: ExtendedReactLocalization }) => {
             link_id: "navbar_how_it_works",
           }}
         >
-          {l10n.getString("landing-all-hero-navbar-link-how-it-works")}
+          {l10n.getString("landing-premium-hero-navbar-link-how-it-works")}
         </TelemetryLink>
         <TelemetryLink
           className={styles.navbarLinks}
@@ -283,7 +283,7 @@ export const TopNavBar = ({ l10n }: { l10n: ExtendedReactLocalization }) => {
             link_id: "navbar_pricing",
           }}
         >
-          {l10n.getString("landing-all-hero-navbar-link-pricing")}
+          {l10n.getString("landing-premium-hero-navbar-link-pricing")}
         </TelemetryLink>
         <TelemetryLink
           data-testid="navbar_faqs"
@@ -293,7 +293,7 @@ export const TopNavBar = ({ l10n }: { l10n: ExtendedReactLocalization }) => {
             link_id: "navbar_faqs",
           }}
         >
-          {l10n.getString("landing-all-hero-navbar-link-faqs")}
+          {l10n.getString("landing-premium-hero-navbar-link-faqs")}
         </TelemetryLink>
         <TelemetryLink
           className={styles.navbarLinks}
@@ -302,7 +302,7 @@ export const TopNavBar = ({ l10n }: { l10n: ExtendedReactLocalization }) => {
             link_id: "navbar_breaches",
           }}
         >
-          {l10n.getString("landing-all-hero-navbar-link-all-breaches")}
+          {l10n.getString("landing-premium-hero-navbar-link-recent-breaches")}
         </TelemetryLink>
       </div>
     </div>
