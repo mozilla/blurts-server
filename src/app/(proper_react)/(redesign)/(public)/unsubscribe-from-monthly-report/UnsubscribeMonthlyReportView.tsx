ecc928691db88650b83a3833055f3c2d74bea403
Kaitlyn <kandres@mozilla.com>
2024-08-30 10:32:16 -0400

    Unsubscription from monthly report confirmation page

---
+++ src/app/(proper_react)/(redesign)/(public)/unsubscribe-from-monthly-report/UnsubscribeMonthlyReportView.tsx
@@ -0,0 +1,47 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+"use client";
+
+import { useState } from "react";
+import { Button } from "../../../../components/client/Button";
+import styles from "./UnsubscribeMonthlyReport.module.scss";
+import UnsubscriptionImage from "./images/confirm-unsubscribe.svg";
+import Image from "next/image";
+import { useL10n } from "../../../../hooks/l10n";
+
+export const UnsubscribeMonthlyReportView = () => {
+  const [unsubscribeSuccess, setUnsubscribeSuccess] = useState(false);
+  const l10n = useL10n();
+  const copy = {
+    confirmation: {
+      header: l10n.getString("unsubscribe-from-monthly-report-header"),
+      body: l10n.getString("unsubscribe-from-monthly-report-body"),
+    },
+    success: {
+      header: l10n.getString("unsubscribe-success-from-monthly-report-header"),
+      body: l10n.getString("unsubscribe-success-from-monthly-report-body"),
+    },
+  };
+
+  const { header, body } = unsubscribeSuccess
+    ? copy.success
+    : copy.confirmation;
+
+  return (
+    <main className={styles.unSubscribeMonthlyReportContainer}>
+      <Image src={UnsubscriptionImage} alt="" />
+      <h1>{header}</h1>
+      <p>{body}</p>
+      <Button
+        className={styles.cta}
+        variant="primary"
+        //TODO: Add func call here
+        onPress={() => setUnsubscribeSuccess(!unsubscribeSuccess)}
+      >
+        {l10n.getString("unsubscribe-cta")}
+      </Button>
+    </main>
+  );
+};
