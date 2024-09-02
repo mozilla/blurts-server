e9727dde451aee5013e6ccdc882ea3ca8da490e6
Vincent <git@vincenttunru.com>
2024-09-02 17:52:01 +0200

    Redirect to the correct step from /fix

--- src/app/(proper_react)/(redesign)/(authenticated)/user/(dashboard)/dashboard/fix/page.tsx
+++ src/app/(proper_react)/(redesign)/(authenticated)/user/(dashboard)/dashboard/fix/page.tsx
@@ -3,7 +3,36 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 import { redirect } from "next/navigation";
+import { headers } from "next/headers";
+import {
+  getNextGuidedStep,
+  StepDeterminationData,
+} from "../../../../../../../functions/server/getRelevantGuidedSteps";
+import { getCountryCode } from "../../../../../../../functions/server/getCountryCode";
+import { getSubscriberBreaches } from "../../../../../../../functions/server/getSubscriberBreaches";
+import { getOnerepProfileId } from "../../../../../../../../db/tables/subscribers";
+import { getLatestOnerepScanResults } from "../../../../../../../../db/tables/onerep_scans";
+import { getServerSession } from "../../../../../../../functions/server/getServerSession";
 
-export default function FixPage() {
-  redirect("/user/dashboard/fix/data-broker-profiles/start-free-scan");
+export default async function FixPage() {
+  const session = await getServerSession();
+  if (!session?.user?.subscriber?.id) {
+    return redirect("/");
+  }
+
+  const countryCode = getCountryCode(headers());
+  const breaches = await getSubscriberBreaches({
+    fxaUid: session.user.subscriber.fxa_uid,
+    countryCode,
+  });
+  const profileId = await getOnerepProfileId(session.user.subscriber.id);
+  const scanData = await getLatestOnerepScanResults(profileId);
+  const stepDeterminationData: StepDeterminationData = {
+    countryCode: countryCode,
+    user: session.user,
+    subscriberBreaches: breaches,
+    latestScanData: scanData,
+  };
+  const nextStep = getNextGuidedStep(stepDeterminationData);
+  redirect(nextStep.href);
 }
