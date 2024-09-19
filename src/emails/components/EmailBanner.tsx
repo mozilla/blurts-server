96ab628130a20f2269e8760a06e5cc58599d5704
Kaitlyn <kandres@mozilla.com>
2024-09-19 06:47:08 -0400

    add great work banner

--- src/emails/components/EmailBanner.tsx
+++ src/emails/components/EmailBanner.tsx
@@ -7,8 +7,9 @@ export const EmailBanner = (props: {
   content: string;
   ctaLabel: string;
   ctaTarget: string;
+  variant: "light" | "dark";
 }) => {
-  return (
+  const DarkBanner = (
     <mj-wrapper padding="24px 16px">
       <mj-section padding="24px" background-color="#7542E5" border-radius="8px">
         <mj-column vertical-align="middle" padding="0">
@@ -40,4 +41,50 @@ export const EmailBanner = (props: {
       </mj-section>
     </mj-wrapper>
   );
+
+  const LightBanner = (
+    <mj-wrapper padding="24px 16px">
+      <mj-section padding="24px" background-color="#F9F9FA" border-radius="8px">
+        <mj-wrapper full-width="full-width">
+          <mj-group>
+            <mj-column width="30%" padding-right="16px">
+              <mj-image
+                alt=""
+                src={`${process.env.SERVER_URL}/images/email/monthly-activity/thumbs-up.png`}
+                padding="0"
+                align="left"
+              />
+            </mj-column>
+            <mj-column vertical-align="top" width="70%" padding="0">
+              <mj-text
+                font-size="16px"
+                line-height="24px"
+                padding="0"
+                color="#000000"
+              >
+                <h3>{props.heading}</h3>
+                <p>{props.content}</p>
+              </mj-text>
+
+              <mj-button
+                href={props.ctaTarget}
+                background-color="#0060DF"
+                font-weight={600}
+                font-size="15px"
+                line-height="22px"
+                border-radius="8px"
+                color="white"
+                padding="0"
+                align="left"
+              >
+                {props.ctaLabel}
+              </mj-button>
+            </mj-column>
+          </mj-group>
+        </mj-wrapper>
+      </mj-section>
+    </mj-wrapper>
+  );
+
+  return props.variant === "light" ? LightBanner : DarkBanner;
 };
