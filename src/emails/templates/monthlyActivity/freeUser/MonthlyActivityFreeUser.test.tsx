25a7db3351cc83620effbbb48080d5cd9dee0fd1
Kaitlyn <kandres@mozilla.com>
2024-09-20 10:45:27 -0400

    unit test

---
+++ src/emails/templates/monthlyActivity/freeUser/MonthlyActivityFreeUser.test.tsx
@@ -0,0 +1,24 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import { it } from "@jest/globals";
+import { composeStory } from "@storybook/react";
+import { render, screen } from "@testing-library/react";
+import Meta, {
+  MonthlyReportFreeUserNoScanWithExposures,
+} from "./MonthlyActivityFreeUser.stories";
+
+it("shows the right label if a user has not yet run a scan", () => {
+  const ComposedEmail = composeStory(
+    MonthlyReportFreeUserNoScanWithExposures,
+    Meta,
+  );
+  render(<ComposedEmail />);
+
+  const getFirstScanFreeBtn = screen.getByRole("link", {
+    name: "Get first scan free",
+  });
+
+  expect(getFirstScanFreeBtn).toBeInTheDocument();
+});
