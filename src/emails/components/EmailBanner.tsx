810119364a4d837605dfa2cf2979337d16c67d0c
Kaitlyn <kandres@mozilla.com>
2024-09-20 08:14:06 -0400

    ignore test for email banner

--- src/emails/components/EmailBanner.tsx
+++ src/emails/components/EmailBanner.tsx
@@ -2,6 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+/* c8 ignore start */
 export const EmailBanner = (props: {
   heading: string;
   content: string;
@@ -88,3 +89,5 @@ export const EmailBanner = (props: {
 
   return props.variant === "light" ? LightBanner : DarkBanner;
 };
+
+/* c8 ignore stop */
