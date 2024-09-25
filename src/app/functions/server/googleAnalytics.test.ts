d8f6f6e948c93129b4885862fe3a9ec6bb013a39
Robert Helmer <rhelmer@mozilla.com>
2024-09-25 09:05:10 -0700

    remove debugging

---
+++ src/app/functions/server/googleAnalytics.test.ts
@@ -0,0 +1,47 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import { it, expect, jest } from "@jest/globals";
+import { sendPingToGA } from "./googleAnalytics";
+import { CONST_GA4_MEASUREMENT_ID } from "../../../constants";
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { getClientIdForSubscriber } from "../../../db/tables/google_analytics_clients";
+
+jest.mock("../../../db/tables/google_analytics_clients", () => {
+  return {
+    getClientIdForSubscriber: jest.fn(),
+  };
+});
+
+it("sends event name and parameters to GA", async () => {
+  global.fetch = jest.fn().mockResolvedValue({ ok: true });
+
+  // @disable typescript-eslint/no-unused-vars
+  (getClientIdForSubscriber as jest.Mock) = jest.fn().mockResolvedValueOnce({
+    client_id: "testClientId1",
+    cookie_timestamp: "1234",
+  });
+
+  await sendPingToGA(0, "testEvent", { testParam1: "testValue1" });
+
+  expect(global.fetch).toHaveBeenCalledWith(
+    `https://www.google-analytics.com/mp/collect?measurement_id=${CONST_GA4_MEASUREMENT_ID}&api_secret=${process.env.GA4_API_SECRET}`,
+    {
+      body: JSON.stringify({
+        client_id: "testClientId1.1234",
+        events: [
+          {
+            name: "testEvent",
+            params: {
+              testParam1: "testValue1",
+              debug_mode: "true",
+              engagement_time_msec: "1",
+            },
+          },
+        ],
+      }),
+      method: "POST",
+    },
+  );
+});
