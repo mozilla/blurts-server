414e1076ced233a15b21aba0adce239614e17bfc
Florian Zia <zia.florian@gmail.com>
2024-09-27 15:26:09 +0200

    chore: Split landing test groups out into their own spec files

--- src/e2e/specs/landing/landing-content.spec.ts
+++ src/e2e/specs/landing/landing-content.spec.ts
@@ -2,12 +2,11 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import { test, expect } from "../fixtures/basePage.js";
+import { test, expect } from "../../fixtures/basePage.js";
 import {
   defaultScreenshotOpts,
   emailInputShouldExist,
-  getVerificationCode,
-} from "../utils/helpers.js";
+} from "../../utils/helpers.js";
 
 test.describe(`${process.env.E2E_TEST_ENV} - Verify the Landing Page content`, () => {
   test.beforeEach(async ({ landingPage }) => {
@@ -232,143 +231,5 @@ test.describe(`${process.env.E2E_TEST_ENV} - Verify the Landing Page content`, (
     await landingPage.closeTooltips.click();
     await landingPage.monitorPlusTooltipTrigger.click();
     await expect(landingPage.monitorPlusTooltipText).toBeVisible();
-  });
-});
-
-test.describe(`${process.env.E2E_TEST_ENV} - Verify the Landing Page Functionality - without existing Account`, () => {
-  test.beforeEach(async ({ landingPage }) => {
-    await landingPage.open();
-  });
-
-  test('Verify "Get free scan" buttons functionality without existing account', async ({
-    landingPage,
-    page,
-    authPage,
-  }) => {
-    test.info().annotations.push({
-      type: "testrail",
-      description:
-        "https://testrail.stage.mozaws.net/index.php?/cases/view/2463502",
-    });
-
-    const randomEmail = `${Date.now()}_tstact@restmail.net`;
-    if (await emailInputShouldExist(landingPage)) {
-      await landingPage.monitorHeroFormEmailInputField.fill(randomEmail);
-      await landingPage.monitorHeroFormInputSubmitButton.click();
-      await page.waitForURL("**/oauth/**");
-    } else {
-      await landingPage.monitorHeroFormInputSubmitButton.click();
-      await authPage.emailInputField.waitFor({
-        state: "visible",
-        timeout: 10000,
-      });
-      await authPage.emailInputField.fill(randomEmail);
-      await authPage.continueButton.click();
-    }
-    // continue with the common steps
-    await authPage.passwordInputField.fill(
-      process.env.E2E_TEST_ACCOUNT_PASSWORD as string,
-    );
-    await authPage.passwordConfirmInputField.fill(
-      process.env.E2E_TEST_ACCOUNT_PASSWORD as string,
-    );
-    await authPage.ageInputField.fill("31");
-    await authPage.continueButton.click();
-    const vc = await getVerificationCode(randomEmail, page);
-    await authPage.enterVerificationCode(vc);
-    const successUrl = `${process.env.E2E_TEST_BASE_URL}/user/welcome`;
-    expect(page.url()).toBe(successUrl);
-  });
-
-  test('Verify the "Start free monitoring" button UI and functionality without existing account', async ({
-    landingPage,
-    page,
-    authPage,
-  }) => {
-    test.info().annotations.push(
-      {
-        type: "testrail id #1",
-        description:
-          "https://testrail.stage.mozaws.net/index.php?/cases/view/2463524",
-      },
-      {
-        type: "testrail id #2",
-        description:
-          "https://testrail.stage.mozaws.net/index.php?/cases/view/2463564",
-      },
-    );
-
-    await landingPage.startFreeMonitoringButton.click();
-
-    const randomEmail = `${Date.now()}_tstact@restmail.net`;
-    await authPage.signUp(randomEmail, page);
-
-    const successUrl = `${process.env.E2E_TEST_BASE_URL}/user/welcome`;
-    expect(page.url()).toBe(successUrl);
-  });
-});
-
-test.describe(`${process.env.E2E_TEST_ENV} - Verify the Landing Page Functionality - with existing account`, () => {
-  test.beforeEach(async ({ landingPage }) => {
-    await landingPage.open();
-  });
-
-  test('Verify "Get free scan" buttons functionality with existing account', async ({
-    landingPage,
-    page,
-    authPage,
-  }) => {
-    // link to testrail case
-    test.info().annotations.push({
-      type: "testrail",
-      description:
-        "https://testrail.stage.mozaws.net/index.php?/cases/view/2463503",
-    });
-
-    const existingEmail = process.env.E2E_TEST_ACCOUNT_EMAIL as string;
-
-    if (await emailInputShouldExist(landingPage)) {
-      // Scenario where the form is still used
-      await landingPage.monitorHeroFormEmailInputField.fill(existingEmail);
-      await landingPage.monitorHeroFormInputSubmitButton.click();
-      await page.waitForURL("**/oauth/**");
-    } else {
-      // Scenario where direct redirection happens
-      await landingPage.monitorHeroFormInputSubmitButton.click();
-      await authPage.emailInputField.waitFor({
-        state: "visible",
-        timeout: 10000,
-      });
-      await authPage.emailInputField.fill(existingEmail);
-      await authPage.continueButton.click();
-    }
-
-    // complete sign in form
-    await authPage.enterPassword();
-
-    // verify dashboard redirect
-    const successUrl = `${process.env.E2E_TEST_BASE_URL}/user/dashboard`;
-    expect(page.url()).toBe(successUrl);
-  });
-
-  test('Verify the "Start free monitoring" button UI and functionality with existing account', async ({
-    landingPage,
-    page,
-    authPage,
-  }) => {
-    test.info().annotations.push({
-      type: "testrail",
-      description:
-        "https://testrail.stage.mozaws.net/index.php?/cases/view/2463524",
-    });
-
-    await landingPage.startFreeMonitoringButton.click();
-
-    await authPage.enterEmail(process.env.E2E_TEST_ACCOUNT_EMAIL as string);
-    await authPage.enterPassword();
-
-    // verify dashboard redirect
-    const successUrl = `${process.env.E2E_TEST_BASE_URL}/user/dashboard`;
-    expect(page.url()).toBe(successUrl);
   });
 });
