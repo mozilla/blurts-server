70c6f1a330449491e2b059510b7977e6f50334c7
Robert Helmer <rhelmer@mozilla.com>
2024-09-30 14:51:55 -0700

    add GA4 un/subscribe events (#5101)
    * store GA client_id in DB on page load, and transmit purchase event on subscribe notification from FxA

--- src/app/(proper_react)/layout.tsx
+++ src/app/(proper_react)/layout.tsx
@@ -3,7 +3,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 import { ReactNode } from "react";
-import { headers } from "next/headers";
+import { headers, cookies } from "next/headers";
 import { getServerSession } from "../functions/server/getServerSession";
 import { getL10nBundles } from "../functions/l10n/serverComponents";
 import { getLocale } from "../functions/universal/getLocale";
@@ -14,6 +14,8 @@ import { getCountryCode } from "../functions/server/getCountryCode";
 import { PageLoadEvent } from "../components/client/PageLoadEvent";
 import { getExperimentationId } from "../functions/server/getExperimentationId";
 import { getEnabledFeatureFlags } from "../../db/tables/featureFlags";
+import { addClientIdForSubscriber } from "../../db/tables/google_analytics_clients";
+import { logger } from "../functions/server/logging";
 
 export default async function Layout({ children }: { children: ReactNode }) {
   const l10nBundles = getL10nBundles();
@@ -23,6 +25,39 @@ export default async function Layout({ children }: { children: ReactNode }) {
   const enabledFlags = await getEnabledFeatureFlags({
     email: session?.user.email ?? "",
   });
+
+  const cookieStore = cookies();
+  // This expects the default Google Analytics cookie documented here: https://support.google.com/analytics/answer/11397207?hl=en
+  const gaCookie = cookieStore.get("_ga");
+
+  if (gaCookie && gaCookie.value) {
+    const [gaCookieVersion, gaCookiePath, gaCookieClientId, gaCookieTimestamp] =
+      gaCookie.value.split(".");
+    if (session?.user.subscriber?.id) {
+      try {
+        const parsedCookieTimestamp = new Date(
+          parseInt(gaCookieTimestamp) * 1000,
+        );
+        await addClientIdForSubscriber(
+          session?.user.subscriber?.id,
+          gaCookieVersion,
+          parseInt(gaCookiePath),
+          gaCookieClientId,
+          parsedCookieTimestamp,
+        );
+      } catch (ex) {
+        if (ex instanceof Error) {
+          logger.error("Could not parse _ga cookie from header", {
+            message: ex.message,
+          });
+        } else {
+          logger.error("Could not parse _ga cookie from header", {
+            message: JSON.stringify(ex),
+          });
+        }
+      }
+    }
+  }
 
   return (
     <L10nProvider bundleSources={l10nBundles}>
