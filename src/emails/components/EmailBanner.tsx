3d4d3f29f3d2c24af8f4b061f55a2fe277b12558
Kaitlyn Andres <kandres@mozilla.com>
2024-10-01 11:23:02 -0400

    MNTOR-3014 - Monthly report free user template (#5060)
    * wip monthly report free user
    
    * adapt current exposures object to free user
    
    * Add banner
    
    * add banner states
    
    * add auto removed vs manually removed data
    
    * add styling
    
    * fix layout
    
    * add unlock with monitor plus cta
    
    * add sparkles
    
    * add null state for no manually resolved reexposures
    
    * add great work banner
    
    * add footer
    
    * remove deprecated string
    
    * add preview heading
    
    * add condition for nonus
    
    * support plural breaches
    
    * wq
    "wq
    :x
    xxxx:X
    
    :X
    Break up Breach alert into separate components
    
    * ignore test for email banner
    
    * remove unnecessary prop
    
    * use correct month value
    
    * fix build error
    
    * unit test
    
    * remove nested css
    
    * add unit test
    
    * clean up manually resolved box data
    
    * correct congratulatory banner logic
    
    * check right type
    
    * check right type
    
    * check right type
    
    * add comment
    
    * fix unit tests
    
    * remove monthly callout
    
    * lint
    
    * fix calculations
    
    * remove emailheader
    
    * add comments
    
    * remove emulate dark mode
    
    * remove unused props
    
    * cleanup premium subscription url
    
    * use correct argument
    
    * remove unused props
    
    * remove unused props
    
    * update locales to split out the stat val
    
    * utmcontentsuffix not passed as a prop
    
    * utmcontentsuffix not passed as a prop
    
    * fix tests
    
    * add telemetry
    
    * group level comment
    
    * correct string
    
    * nit
    
    * fix utm campaign param
    
    * add move strings to locales-pending
    
    * fix calculations
    
    * lint
    
    * remove strings

---
+++ src/emails/components/EmailBanner.tsx
@@ -0,0 +1,95 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import React from "react";
+
+/* c8 ignore start */
+export const EmailBanner = (props: {
+  heading: string;
+  content: string;
+  ctaLabel: string;
+  ctaTarget: string;
+  variant: "light" | "dark";
+}) => {
+  const DarkBanner = (
+    <mj-wrapper padding="24px 16px">
+      <mj-section padding="24px" background-color="#7542E5" border-radius="8px">
+        <mj-column vertical-align="middle" padding="0">
+          <mj-text
+            font-size="16px"
+            line-height="24px"
+            padding="0"
+            color="white"
+          >
+            <h3>{props.heading}</h3>
+            <p>{props.content}</p>
+          </mj-text>
+        </mj-column>
+        <mj-column vertical-align="middle" padding="0" width="230px">
+          <mj-button
+            href={props.ctaTarget}
+            background-color="transparent"
+            font-weight={600}
+            font-size="15px"
+            line-height="22px"
+            border-radius="8px"
+            border="2px solid white"
+            color="white"
+            padding="0"
+          >
+            {props.ctaLabel}
+          </mj-button>
+        </mj-column>
+      </mj-section>
+    </mj-wrapper>
+  );
+
+  const LightBanner = (
+    <mj-wrapper padding="24px 16px">
+      <mj-section padding="24px" background-color="#F9F9FA" border-radius="8px">
+        <mj-wrapper full-width="full-width">
+          <mj-group>
+            <mj-column width="30%" padding-right="16px">
+              <mj-image
+                alt=""
+                src={`${process.env.SERVER_URL}/images/email/monthly-activity/thumbs-up.png`}
+                padding="0"
+                align="left"
+              />
+            </mj-column>
+            <mj-column vertical-align="top" width="70%" padding="0">
+              <mj-text
+                font-size="16px"
+                line-height="24px"
+                padding="0"
+                color="#000000"
+              >
+                <h3>{props.heading}</h3>
+                <p>{props.content}</p>
+              </mj-text>
+
+              <mj-button
+                href={props.ctaTarget}
+                background-color="#0060DF"
+                font-weight={600}
+                font-size="15px"
+                line-height="22px"
+                border-radius="8px"
+                color="white"
+                padding="0"
+                align="left"
+              >
+                {props.ctaLabel}
+              </mj-button>
+            </mj-column>
+          </mj-group>
+        </mj-wrapper>
+      </mj-section>
+    </mj-wrapper>
+  );
+
+  return props.variant === "light" ? LightBanner : DarkBanner;
+};
+
+/* c8 ignore stop */
---
+++ src/emails/templates/monthlyActivity/freeUser/MonthlyActivityFreeUser.test.tsx
@@ -0,0 +1,103 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import { it } from "@jest/globals";
+import { composeStory } from "@storybook/react";
+import { render, screen } from "@testing-library/react";
+import Meta, {
+  MonthlyReportFreeUserNoScanNoExposuresRemaining,
+  MonthlyReportFreeUserNoScanWithExposures,
+  MonthlyReportFreeUserWithScan,
+  MonthlyReportFreeUserWithScanNoExposuresRemaining,
+} from "./MonthlyActivityFreeUser.stories";
+
+it("shows the right cta label if a user has not yet run a scan", () => {
+  const ComposedEmail = composeStory(
+    MonthlyReportFreeUserNoScanWithExposures,
+    Meta,
+  );
+  render(<ComposedEmail />);
+
+  const getFirstScanFreeBtn = screen.getByRole("link", {
+    name: "Get first scan free",
+  });
+
+  expect(getFirstScanFreeBtn).toBeInTheDocument();
+});
+
+it("shows the right cta label if a user has run a scan", () => {
+  const ComposedEmail = composeStory(MonthlyReportFreeUserWithScan, Meta);
+  render(<ComposedEmail />);
+
+  const getFirstScanFreeBtn = screen.getByRole("link", {
+    name: "Get ⁨Monitor Plus⁩",
+  });
+
+  expect(getFirstScanFreeBtn).toBeInTheDocument();
+});
+
+it("shows the right data exposure value if a user is a free user", () => {
+  const ComposedEmail = composeStory(
+    MonthlyReportFreeUserNoScanWithExposures,
+    Meta,
+  );
+  render(<ComposedEmail />);
+
+  // For free users, the data point count should be "Data breaches" instead
+  expect(screen.queryByText("Data exposures")).not.toBeInTheDocument();
+});
+
+it("shows the inactive state if there are 0 manually resolved data breaches", () => {
+  const ComposedEmail = composeStory(
+    MonthlyReportFreeUserNoScanWithExposures,
+    Meta,
+  );
+  render(<ComposedEmail />);
+
+  const manuallyResolvedDataBreaches = screen.getByText(
+    "Manually resolved data breaches",
+    { exact: false },
+  );
+
+  expect(manuallyResolvedDataBreaches).toHaveStyle("color: #9E9E9E");
+});
+
+it("says exposures instead of breaches once a user has run a scan", () => {
+  const ComposedEmail = composeStory(MonthlyReportFreeUserWithScan, Meta);
+  render(<ComposedEmail />);
+
+  const manuallyResolvedDataBreaches = screen.getByText(
+    "Manually resolved exposures",
+    { exact: false },
+  );
+
+  const dataExposures = screen.getByText("Data exposures", { exact: false });
+
+  expect(manuallyResolvedDataBreaches).toBeInTheDocument();
+  expect(dataExposures).toBeInTheDocument();
+});
+
+it("shows the congratulatory banner if no exposures remaining before scan", () => {
+  const ComposedEmail = composeStory(
+    MonthlyReportFreeUserNoScanNoExposuresRemaining,
+    Meta,
+  );
+  render(<ComposedEmail />);
+
+  const greatNewsHeader = screen.getByText("Great news!");
+
+  expect(greatNewsHeader).toBeInTheDocument();
+});
+
+it("shows the congratulatory banner if no exposures remaining after scan", () => {
+  const ComposedEmail = composeStory(
+    MonthlyReportFreeUserWithScanNoExposuresRemaining,
+    Meta,
+  );
+  render(<ComposedEmail />);
+
+  const greatNewsHeader = screen.getByText("Great news!");
+
+  expect(greatNewsHeader).toBeInTheDocument();
+});
