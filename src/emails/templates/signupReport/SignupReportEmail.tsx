bfe6dc0133f539da7e22307bd765ce7128a19469
Vincent <git@vincenttunru.com>
2024-10-09 15:47:04 +0200

    Remove need to import React in cronjobs
    This caused errors in production, but not in dev, making them easy
    to slip through.

--- src/emails/templates/signupReport/SignupReportEmail.tsx
+++ src/emails/templates/signupReport/SignupReportEmail.tsx
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import React from "react";
+import { Fragment } from "react";
 import { ExtendedReactLocalization } from "../../../app/functions/l10n";
 import { EmailFooter } from "../EmailFooter";
 import { EmailHeader } from "../EmailHeader";
@@ -40,7 +40,7 @@ export const SignupReportEmail = (props: Props) => {
           </mj-column>
         </mj-section>
         {props.breaches.map((breach, i) => (
-          <React.Fragment key={breach.Id}>
+          <Fragment key={breach.Id}>
             {i > 0 && (
               // > mj-spacer cannot be used inside mj-body, only inside: mj-attributes, mj-column, mj-hero.
               // And <mj-column>s "must be located under mj-section tags".
@@ -51,7 +51,7 @@ export const SignupReportEmail = (props: Props) => {
               </mj-section>
             )}
             <BreachCard breach={breach} l10n={l10n} />
-          </React.Fragment>
+          </Fragment>
         ))}
         <mj-section padding="20px">
           <mj-column>
