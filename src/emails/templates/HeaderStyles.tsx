efd074be25fe26e1367ec4fa5e667bceeb69d15d
Kaitlyn <kandres@mozilla.com>
2024-10-29 13:50:12 -0400

    Enforce dark text on footer and header

--- src/emails/templates/HeaderStyles.tsx
+++ src/emails/templates/HeaderStyles.tsx
@@ -23,16 +23,14 @@ export const HeaderStyles = () => {
         background-image: url(${process.env.SERVER_URL}/images/email/footer-bg-shapes.png);
         background-position: center bottom;
         background-repeat: no-repeat;
-        color: #000000;
-        background: #fffff;
+        color: #000000 !important;
       }
 
       .hero_background {
         background-image: url(${process.env.SERVER_URL}/images/email/hero-bg-gradient.png);
         background-repeat: repeat;
         background-position-x: 0;
-        background-color: #e4d2ff;
-        color: #ffffff;
+        color: #000000 !important;
       }
     `;
 
