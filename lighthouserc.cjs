17766e5ed1660d6f678666253328484877301581
Florian Zia <zia.florian@gmail.com>
2024-11-08 11:27:38 +0100

    wip: Debug lhci action

--- lighthouserc.cjs
+++ lighthouserc.cjs
@@ -2,20 +2,16 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-const pages = [
-  "/",
-  "/breaches",
-  "/breach-details/LinkedInScrape2023",
-  "/how-it-works",
-];
+const pages = ["/"];
 
 const collectBaseUrl =
   process.env.LIGHTHOUSE_COLLECT_URL ?? "http://localhost:3000";
+
 const lighthouseConfig = {
   ci: {
     collect: {
-      startServerCommand: "npm run start",
       url: pages.map((pathname) => `${collectBaseUrl}${pathname}`),
+      numberOfRuns: 1,
     },
     upload: {
       target: "filesystem",
