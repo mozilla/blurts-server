46c1843f6fc7c14f35fdaae00a9ac95df5544b4b
mansaj <jozhou@mozilla.com>
2024-11-12 16:29:12 -0800

    MNTOR-3776: backend for data brokers (#5288)
    * feat: new table for data brokers
    
    * fix: types and builds
    
    * fix: upsert
    
    * fix: unique index on data_broker name
    
    * fix: updated at
    
    * fix: updated_at in upsert
    
    ---------
    
    Co-authored-by: Robert Helmer <rhelmer@mozilla.com>

---
+++ src/db/migrations/20241105115743_add_data_brokers.js
@@ -0,0 +1,20 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+export async function up(knex) {
+  return knex.schema
+    .createTable("onerep_data_brokers", table => {
+      table.increments('id').primary()
+      table.string("data_broker").notNullable().unique()
+      table.string("status").notNullable()
+      table.string("url").notNullable()
+      table.timestamp("created_at").defaultTo(knex.fn.now())
+      table.timestamp("updated_at").defaultTo(knex.fn.now())
+    })
+}
+
+export async function down(knex) {
+  return knex.schema
+    .dropTable("onerep_data_brokers")
+} 
---
+++ src/db/tables/data_brokers.ts
@@ -0,0 +1,42 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import createDbConnection from "../connect";
+import { logger } from "../../app/functions/server/logging";
+import { DataBrokerRow } from "../../knex-tables";
+
+const knex = createDbConnection();
+
+async function upsertDataBrokers(
+  databrokers: Array<{
+    dataBroker: string;
+    status: string;
+    url: string;
+  }>,
+): Promise<DataBrokerRow[]> {
+  logger.info("upsert_data_brokers", { count: databrokers.length });
+
+  try {
+    const res = await knex("onerep_data_brokers")
+      .insert(
+        databrokers.map(({ dataBroker, status, url }) => ({
+          data_broker: dataBroker,
+          status,
+          url,
+          updated_at: knex.fn.now(),
+        })),
+      )
+      .onConflict("data_broker")
+      .merge(["status", "url"])
+      .returning("*");
+
+    logger.info("upsert_data_brokers_success", { count: res.length });
+    return res;
+  } catch (e) {
+    logger.error("upsert_data_brokers_error", { error: JSON.stringify(e) });
+    throw e;
+  }
+}
+
+export { upsertDataBrokers };
