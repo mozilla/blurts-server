6136d21c753b911ae3eb1a74be88b9fc1ffd9400
Joey Zhou <jozhou@mozilla.com>
2024-12-03 13:36:35 -0800

    doc: remove todo

--- src/app/functions/universal/dataBroker.ts
+++ src/app/functions/universal/dataBroker.ts
@@ -2,8 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-// TODO: Move pure functions that operate on scan results to this file
-
 export type DataBrokerRemovalStatus =
   | "active"
   | "on_hold"
