f0b5fc7eb948e73a173663cbd0295a134713bc1f
Florian Zia <zia.florian@gmail.com>
2024-12-10 12:56:35 +0100

    chore: Force feature flags for local E2E tests

--- src/e2e/specs/settings.spec.ts
+++ src/e2e/specs/settings.spec.ts
@@ -3,40 +3,22 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 import { test, expect } from "../fixtures/basePage.js";
-import { getEnabledFeatureFlags } from "../../db/tables/featureFlags.js";
 
 // bypass login
 test.use({ storageState: "./e2e/storageState.json" });
 test.describe(`${process.env.E2E_TEST_ENV} Settings Page`, () => {
   test("Verify settings page loads", async ({ settingsPage }) => {
-    const enabledFeatureFlags = await getEnabledFeatureFlags({
-      email: process.env.E2E_TEST_ACCOUNT_EMAIL as string,
-      isSignedOut: false,
-    });
-
     // should go directly to data breach page
     await settingsPage.open();
 
     await expect(settingsPage.settingsHeader).toBeVisible();
     await expect(settingsPage.addEmailButton).toBeVisible();
     await expect(settingsPage.emailHeader).toBeVisible();
-
-    if (!enabledFeatureFlags.includes("SettingsPageRedesign")) {
-      await expect(settingsPage.emailPrefHeader).toBeVisible();
-      (await settingsPage.deleteHeader.isVisible())
-        ? await expect(settingsPage.deleteHeader).toBeVisible()
-        : await expect(settingsPage.deactivateHeader).toBeVisible();
-    }
   });
 
   test("Verify that a user can select between the Breach alert preferences", async ({
     settingsPage,
   }) => {
-    const enabledFeatureFlags = await getEnabledFeatureFlags({
-      email: process.env.E2E_TEST_ACCOUNT_EMAIL as string,
-      isSignedOut: false,
-    });
-
     test.info().annotations.push({
       type: "issue",
       description:
@@ -45,10 +27,7 @@ test.describe(`${process.env.E2E_TEST_ENV} Settings Page`, () => {
 
     // go to monitor settings page
     await settingsPage.open();
-
-    if (enabledFeatureFlags.includes("SettingsPageRedesign")) {
-      await settingsPage.tabNotifications.click();
-    }
+    await settingsPage.tabNotifications.click();
 
     await expect(async () => {
       // select "send breach alerts to the affected email address" option
