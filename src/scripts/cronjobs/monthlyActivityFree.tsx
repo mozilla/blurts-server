d1a6d27fe7e15c474933a4556d23b37df5aa313a
Joey Zhou <jozhou@mozilla.com>
2024-12-18 10:36:28 -0800

    fix: hardcode batch size

--- src/scripts/cronjobs/monthlyActivityFree.tsx
+++ src/scripts/cronjobs/monthlyActivityFree.tsx
@@ -24,15 +24,7 @@ await run();
 await createDbConnection().destroy();
 
 async function run() {
-  const batchSize = Number.parseInt(
-    process.env.MONTHLY_ACTIVITY_FREE_EMAIL_BATCH_SIZE ?? "10",
-    10,
-  );
-  if (Number.isNaN(batchSize)) {
-    throw new Error(
-      `Could not send monthly activity emails, because the env var MONTHLY_ACTIVITY_FREE_EMAIL_BATCH_SIZE has a non-numeric value: [${process.env.MONTHLY_ACTIVITY_EMAIL_BATCH_SIZE}].`,
-    );
-  }
+  const batchSize = 5;
 
   logger.info(`Getting free subscribers with batch size: ${batchSize}`);
   const subscribersToEmail = (await getFreeSubscribersWaitingForMonthlyEmail())
