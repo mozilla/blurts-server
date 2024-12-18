d5bbc9c6346482ab3adaa015830e89a7ad5f849d
Robert Helmer <rhelmer@mozilla.com>
2024-12-18 15:25:23 -0800

    update TODO with link to ticket

--- src/db/tables/subscribers.ts
+++ src/db/tables/subscribers.ts
@@ -515,7 +515,7 @@ async function getFreeSubscribersWaitingForMonthlyEmail(
   }
 
   const wrappedQuery = knex
-    // @ts-ignore TODO update our knex types
+    // @ts-ignore TODO MNTOR-3890 Move away from this approach and simplify query.
     .from({ base_query: query }) // Use the existing query as a subquery
     .select("*")
     .whereIn("country_code", countryCodes)
