e6084b39de6003497675917d55b49ee47e31d533
Joey Zhou <jozhou@mozilla.com>
2025-01-13 17:03:24 -0800

    feat: more logic for churn email

--- src/scripts/cronjobs/churnDiscount.tsx
+++ src/scripts/cronjobs/churnDiscount.tsx
@@ -1,7 +1,10 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-
+import {
+  getChurnPreventionEmailSentAt,
+  markChurnPreventionEmailAsJustSent,
+} from "../../db/tables/subscribers";
 // import { getFreeSubscribersWaitingForMonthlyEmail } from "../../db/tables/subscribers";
 // import { getScanResultsWithBroker } from "../../db/tables/onerep_scans";
 // import { updateEmailPreferenceForSubscriber } from "../../db/tables/subscriber_email_preferences";
@@ -81,6 +84,17 @@ async function run() {
 
   for (const subscriber of subscribersToEmail) {
     try {
+      // we need to query our db to make sure the email wasn't sent in the past
+      const sentDate = await getChurnPreventionEmailSentAt(
+        parseInt(subscriber.userid, 10),
+      );
+      if (sentDate) {
+        logger.warn("send_churn_discount_email_warn", {
+          subscriberId: subscriber.userid,
+          message: `email already sent for the user at: ${sentDate}`,
+        });
+      }
+      // send email
       await sendChurnDiscountEmail(subscriber);
       logger.info("send_churn_discount_email_success", {
         subscriberId: subscriber.userid,
@@ -101,58 +115,6 @@ async function run() {
 
 async function sendChurnDiscountEmail(subscriber: FxaChurnSubscriber) {
   console.log(`sent email to: ${subscriber.userid}`);
-  // const sanitizedSubscriber = sanitizeSubscriberRow(subscriber);
-  // const l10n = getCronjobL10n(sanitizedSubscriber);
-  // /**
-  //  * Without an active user session, we don't know the user's country. This is
-  //  * our best guess based on their locale. At the time of writing, it's only
-  //  * used to determine whether to count SSN breaches (which we don't have
-  //  * recommendations for outside the US).
-  //  */
-  // const countryCodeGuess = getSignupLocaleCountry(subscriber);
-  // // OneRep suggested not relying on webhooks, but instead to fetch the latest
-  // // data from their API. Thus, let's refresh the data in our DB in real-time:
-  // if (subscriber.onerep_profile_id !== null) {
-  //   await refreshStoredScanResults(subscriber.onerep_profile_id);
-  // }
-  // const latestScan = await getScanResultsWithBroker(
-  //   subscriber.onerep_profile_id,
-  //   hasPremium(subscriber),
-  // );
-  // const subscriberBreaches = await getSubscriberBreaches({
-  //   fxaUid: subscriber.fxa_uid,
-  //   countryCode: countryCodeGuess,
-  // });
-  // const data = getDashboardSummary(latestScan.results, subscriberBreaches);
-  // const subject = l10n.getString("email-monthly-free-subject");
-  // const unsubscribeLink =
-  //   await getMonthlyActivityFreeUnsubscribeLink(subscriber);
-  // if (unsubscribeLink === null) {
-  //   throw new Error(
-  //     `Trying to send a monthly activity email to a free user, but the unsubscribe link could not be generated: [${unsubscribeLink}].`,
-  //   );
-  // }
-  // // Update the last-sent date *first*, so that if something goes wrong, we
-  // // don't keep resending the email a brazillion times.
-  // await updateEmailPreferenceForSubscriber(subscriber.id, true, {
-  //   monthly_monitor_report_free_at: new Date(Date.now()),
-  // });
-  // try {
-  //   await sendEmail(
-  //     sanitizedSubscriber.primary_email,
-  //     subject,
-  //     renderEmail(
-  //       <MonthlyActivityFreeEmail
-  //         subscriber={sanitizedSubscriber}
-  //         dataSummary={data}
-  //         l10n={l10n}
-  //         unsubscribeLink={unsubscribeLink}
-  //       />,
-  //     ),
-  //   );
-  // } catch (e) {
-  //   logger.error("send_churn_discount_email", {
-  //     exception: e,
-  //   });
-  // }
+  // mark as sent
+  // await markChurnPreventionEmailAsJustSent(parseInt(subscriber.userid, 10))
 }
