98cb1112bbfafc786f4531797388d6d81ea74ac6
Vincent <git@vincenttunru.com>
2025-01-16 12:07:00 +0100

    Revert "handle nimbus user id not set (#5503)"
    This reverts commit 4de104f3ef3f61bb040eaedcfb92c7335c6bd148.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -72,7 +72,7 @@ export default async function RootLayout({
     previewMode: nimbusPreviewMode === "true",
   });
 
-  const nimbus_user_id = experimentData["Enrollments"]?.nimbus_user_id ?? "";
+  const nimbus_user_id = experimentData["Enrollments"].nimbus_user_id;
   if (nimbus_user_id !== experimentationId) {
     Sentry.captureMessage(
       `Nimbus user ID from Cirrus: [${nimbus_user_id}] did not match experimentationId: [${experimentationId}]`,
