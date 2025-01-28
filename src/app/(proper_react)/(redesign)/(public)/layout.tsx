552c40773ab0427faea60e86c0e7d3b284cf9436
Florian Zia <zia.florian@gmail.com>
2025-01-28 13:46:50 +0100

    fix: Experiment preview for landing page

--- src/app/(proper_react)/(redesign)/(public)/layout.tsx
+++ src/app/(proper_react)/(redesign)/(public)/layout.tsx
@@ -7,6 +7,7 @@ import { PublicShell } from "./PublicShell";
 import {
   getAcceptLangHeaderInServerComponents,
   getL10n,
+  getL10nBundles,
 } from "../../../functions/l10n/serverComponents";
 import { headers } from "next/headers";
 import { getCountryCode } from "../../../functions/server/getCountryCode";
@@ -14,21 +15,41 @@ import { getLocale } from "../../../functions/universal/getLocale";
 import { getExperiments } from "../../../functions/server/getExperiments";
 import { getExperimentationId } from "../../../functions/server/getExperimentationId";
 import { getEnabledFeatureFlags } from "../../../../db/tables/featureFlags";
+import { getServerSession } from "../../../functions/server/getServerSession";
+import * as Sentry from "@sentry/nextjs";
 
 export default async function Layout(props: { children: ReactNode }) {
-  const headersList = await headers();
-  const countryCode = getCountryCode(headersList);
   const enabledFeatureFlags = await getEnabledFeatureFlags({
     isSignedOut: true,
   });
   const l10n = getL10n(await getAcceptLangHeaderInServerComponents());
-  const experimentationId = await getExperimentationId(null);
+  const currentLocale = getLocale(
+    getL10nBundles(await getAcceptLangHeaderInServerComponents()),
+  );
+  const session = await getServerSession();
+  const headersList = await headers();
+  const countryCode = getCountryCode(headersList);
+
+  // Check for Nimbus preview mode. Note that this requires a full page reload
+  // to activate: https://nextjs.org/docs/app/api-reference/file-conventions/layout#caveats
+  const nimbusPreviewMode = (await headers()).get("x-nimbus-preview-mode");
+  const experimentationId = await getExperimentationId(session?.user ?? null);
   const experimentData = await getExperiments({
-    experimentationId,
-    countryCode,
-    locale: getLocale(l10n),
-    previewMode: false,
+    experimentationId: experimentationId,
+    countryCode: countryCode,
+    locale: currentLocale,
+    previewMode: nimbusPreviewMode === "true",
   });
+
+  const nimbus_user_id = experimentData["Enrollments"]?.nimbus_user_id;
+  if (
+    typeof nimbus_user_id !== "undefined" &&
+    nimbus_user_id !== experimentationId
+  ) {
+    Sentry.captureMessage(
+      `Nimbus user ID from Cirrus: [${nimbus_user_id}] did not match experimentationId: [${experimentationId}]`,
+    );
+  }
 
   return (
     <PublicShell
