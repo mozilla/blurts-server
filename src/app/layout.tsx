48703ab87d9f513c4e4016629eab0991ee8009df
Vincent <git@vincenttunru.com>
2025-01-28 14:06:47 +0100

    Inspect _all_ enrollments for unexpected IDs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -78,13 +78,12 @@ export default async function RootLayout({
     previewMode: nimbusPreviewMode === "true",
   });
 
-  const nimbus_user_id = experimentData["Enrollments"]?.[0]?.nimbus_user_id;
-  if (
-    typeof nimbus_user_id !== "undefined" &&
-    nimbus_user_id !== experimentationId
-  ) {
+  const enrollmentWithConflictingUserId = (
+    experimentData.Enrollments ?? []
+  ).find((enrollment) => enrollment.nimbus_user_id !== experimentationId);
+  if (typeof enrollmentWithConflictingUserId !== "undefined") {
     Sentry.captureMessage(
-      `Nimbus user ID from Cirrus: [${nimbus_user_id}] did not match experimentationId: [${experimentationId}]`,
+      `Nimbus user ID from Cirrus: [${enrollmentWithConflictingUserId.nimbus_user_id}] did not match experimentationId: [${experimentationId}]`,
     );
   }
 
