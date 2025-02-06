c6d185b021e244e56c24f8a31d3b6f05faabbb23
Vincent <git@vincenttunru.com>
2025-02-06 15:43:51 +0100

    Remove unused `UpdatedEmailPreferencesOption` flag

--- src/db/tables/featureFlags.ts
+++ src/db/tables/featureFlags.ts
@@ -38,7 +38,6 @@ export async function getDeletedFeatureFlags() {
 }
 
 export const featureFlagNames = [
-  "UpdatedEmailPreferencesOption",
   "CancellationFlow",
   "DiscountCouponNextThreeMonths",
   "LatestScanDateCsatSurvey",
