2f9b7686d3ae100a9eac0e4990d74306742b6534
Kaitlyn <kandres@mozilla.com>
2025-02-11 10:06:30 -0500

    add database changes to qa_custom_brokers

---
+++ src/db/migrations/20250207185747_add_id_column_to_qa_custom_brokers.js
@@ -0,0 +1,24 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+ /**
+  * @param { import("knex").Knex } knex
+  * @returns { Promise<void> }
+  */
+ export function up(knex) {
+  return knex.schema
+    .table("qa_custom_brokers", table => {
+      table.string('id').nullable();
+    });
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function down(knex) {
+    return knex.schema.table('qa_custom_brokers',table => {
+      table.dropColumn('id');  
+    });
+}
