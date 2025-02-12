79ea26fc0e885ac4df567566dc917e5ca8227e53
Florian Zia <zia.florian@gmail.com>
2025-02-12 11:14:34 +0100

    chore: Lint root files

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 import * as path from "path";
 
 const buildEslintCommand = (filenames) =>
@@ -8,8 +12,10 @@ const buildEslintCommand = (filenames) =>
 /**
  * @type {import("lint-staged").Configuration}
  */
-export default {
+const config = {
   "*.{js,cjs,mjs,jsx,ts,tsx}": [buildEslintCommand],
   "*.{scss,css}": "stylelint --allow-empty-input --fix",
   "*.{js,ts,tsx,jsx,cjs,mjs,scss,css,md,html}": "prettier --write",
 };
+
+export default config;
