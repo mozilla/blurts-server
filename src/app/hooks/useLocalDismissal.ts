a1c6448302294426f5a511f8e9aaf9e29d50383e
Vincent <git@vincenttunru.com>
2025-02-13 14:07:09 +0100

    Revert "fix: c8 ignore comments in useLocalDismissal"
    This reverts commit aeba4e225a4573a378570fbdd22e0c040aca7347.

--- src/app/hooks/useLocalDismissal.ts
+++ src/app/hooks/useLocalDismissal.ts
@@ -45,10 +45,6 @@ export function useLocalDismissal(
 
   const dismiss = (dismissOptions?: DismissOptions) => {
     const maxAgeInSeconds =
-      /* c8 ignore next 6 */
-      // Since the Node 20.10 upgrade, it's been intermittently marking this
-      // (and this comment) as uncovered, even though I think it's covered by
-      // tests.
       typeof options.duration === "number"
         ? options.duration
         : COOKIE_DISMISSAL_MAX_AGE_IN_SECONDS;
@@ -68,9 +64,6 @@ export function useLocalDismissal(
 
 function hasDismissedCookie(cookieValue?: string, duration?: number): boolean {
   const dismissalTimeStamp =
-    /* c8 ignore next 4 */
-    // Since the Node 20.10 upgrade, it's been intermittently marking this (and
-    // this comment) as uncovered, even though I think it's covered by tests.
     typeof cookieValue === "string"
       ? Number.parseInt(cookieValue, 10)
       : // Unfortunately react-cookie seems to implicitly parse the cookie value
