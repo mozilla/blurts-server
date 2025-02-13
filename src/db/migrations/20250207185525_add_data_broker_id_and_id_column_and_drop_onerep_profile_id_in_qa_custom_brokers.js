6218c71591184f67f4b224ce7c62b3aada9a189e
Kaitlyn <kandres@mozilla.com>
2025-02-13 15:58:23 -0500

    lint

--- src/db/migrations/20250207185525_add_data_broker_id_and_id_column_and_drop_onerep_profile_id_in_qa_custom_brokers.js
+++ src/db/migrations/20250207185525_add_data_broker_id_and_id_column_and_drop_onerep_profile_id_in_qa_custom_brokers.js
@@ -2,17 +2,16 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
- /**
-  * @param { import("knex").Knex } knex
-  * @returns { Promise<void> }
-  */
- export function up(knex) {
-  return knex.schema
-    .table("qa_custom_brokers", table => {
-      table.integer('id').nullable();
-      table.dropColumn("onerep_profile_id");
-      table.string('data_broker_id').nullable();
-    });
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function up(knex) {
+  return knex.schema.table("qa_custom_brokers", (table) => {
+    table.integer("id").nullable();
+    table.dropColumn("onerep_profile_id");
+    table.string("data_broker_id").nullable();
+  });
 }
 
 /**
@@ -20,9 +19,9 @@
  * @returns { Promise<void> }
  */
 export function down(knex) {
-  return knex.schema.table('qa_custom_brokers',table => {
-    table.dropColumn('id');  
+  return knex.schema.table("qa_custom_brokers", (table) => {
+    table.dropColumn("id");
     table.string("onerep_profile_id").nullable();
-    table.dropColumn('data_broker_id');  
+    table.dropColumn("data_broker_id");
   });
 }
