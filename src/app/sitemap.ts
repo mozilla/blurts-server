e04388e583e5b652268ce7a1e59b3bf8dec1669c
Vincent <git@vincenttunru.com>
2025-02-18 14:36:00 +0100

    Avoid build errors with empty database

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -5,6 +5,11 @@
 import type { MetadataRoute } from "next";
 import { getBreaches } from "./functions/server/getBreaches";
 
+// See https://nextjs.org/docs/app/api-reference/file-conventions/route-segment-config#dynamic
+// This ensures that we're not generating the sitemap at build time,
+// when we don't have a database filled with breaches.
+export const dynamic = "force-dynamic";
+
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const serverUrl = process.env.SERVER_URL!;
   const allBreaches = await getBreaches();
