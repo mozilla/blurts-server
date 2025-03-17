1c95b9b2c07b1747d81a5bed09655ee088e56e0e
Kaitlyn <kandres@mozilla.com>
2025-03-17 14:59:31 -0400

    Add user_notifications table

---
+++ src/db/migrations/20250317184212_add-user-announcements-table.js
@@ -0,0 +1,30 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function up(knex) {
+  return knex.schema.createTable("user_announcements", (table) => {
+    table.increments("id").primary();
+    table.integer("user_id").notNullable();
+    table.string("announcement_id").notNullable();
+    table.string("status").notNullable(); // seen, cleared, clicked
+    table.timestamp("seen_at").nullable();
+    table.timestamp("cleared_at").nullable();
+    table.boolean("is_history").defaultTo(false);
+    table.timestamp("created_at").defaultTo(knex.fn.now());
+    table.timestamp("updated_at").defaultTo(knex.fn.now());
+    table.unique(["user_id", "announcement_id"]); // ensure uniqueness of the user_announcement pair (user_id, announcement_id)
+  });
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists("user_announcements");
+};
