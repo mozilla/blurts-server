1efe71bf946abd4459985f839719a6df31e14f58
Kaitlyn <kandres@mozilla.com>
2025-03-21 14:27:15 -0400

    join user_announcements and announcement table

--- src/db/tables/user_announcements.ts
+++ src/db/tables/user_announcements.ts
@@ -2,15 +2,23 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import { UserAnnouncementsRow } from "knex/types/tables";
+import { AnnouncementRow } from "knex/types/tables";
 import createDbConnection from "../connect";
 import { logger } from "../../app/functions/server/logging";
 
 const knex = createDbConnection();
 
+export type UserAnnouncementWithDetails = AnnouncementRow & {
+  status: string;
+  seen_at: Date | null;
+  cleared_at: Date | null;
+  user_created_at: Date;
+  user_updated_at: Date;
+};
+
 export async function initializeUserAnnouncements(
   userId: number,
-): Promise<UserAnnouncementsRow[]> {
+): Promise<UserAnnouncementWithDetails[]> {
   try {
     const existing = await knex("user_announcements")
       .where("user_id", userId)
@@ -30,30 +38,38 @@ export async function initializeUserAnnouncements(
 
       if (insertData.length > 0) {
         await knex("user_announcements").insert(insertData);
-        logger.info(`Initialized user_announcements for user ${userId}`);
       }
     }
 
-    const userAnnouncements = await knex("user_announcements")
-      .join(
-        "announcements",
-        "user_announcements.announcement_id",
-        "=",
-        "announcements.announcement_id",
-      )
+    const results: UserAnnouncementWithDetails[] = await knex(
+      "user_announcements as ua",
+    )
+      .join("announcements as a", "ua.announcement_id", "a.announcement_id")
+      .where("ua.user_id", userId)
       .select(
-        "announcements.*",
-        "user_announcements.status",
-        "user_announcements.seen_at",
-        "user_announcements.cleared_at",
-        "user_announcements.created_at",
-        "user_announcements.updated_at",
-      )
-      .where("user_announcements.user_id", userId);
+        "a.announcement_id",
+        "a.title",
+        "a.description",
+        "a.label",
+        "a.cta_label",
+        "a.cta_link",
+        "a.small_image_path",
+        "a.big_image_path",
+        "a.audience",
+        "a.created_at as created_at",
+        "a.updated_at as updated_at",
 
-    return userAnnouncements;
+        // user-specific info
+        "ua.status",
+        "ua.seen_at",
+        "ua.cleared_at",
+        "ua.created_at as user_created_at",
+        "ua.updated_at as user_updated_at",
+      );
+
+    return results;
   } catch (error) {
-    logger.error("Error initializing and fetching user announcements:", error);
+    logger.error("Error initializing user announcements:", error);
     throw error;
   }
 }
