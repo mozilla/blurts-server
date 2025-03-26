9a80cbc52e7118548a0e4f94869da584493b2fc4
Florian Zia <zia.florian@gmail.com>
2025-03-26 11:22:52 +0100

    chore: Add CookiesProvider for react-cookies 8.0.1 update

---
+++ src/contextProviders/cookies.tsx
@@ -0,0 +1,14 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+"use client";
+
+import { ReactNode } from "react";
+import { CookiesProvider as OriginalCookiesProvider } from "react-cookie";
+
+const CookiesProvider = ({ children }: { children: ReactNode }) => (
+  <OriginalCookiesProvider>{children}</OriginalCookiesProvider>
+);
+
+export default CookiesProvider;
