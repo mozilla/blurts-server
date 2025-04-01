f647797d1e7368bddc59381bde97760f68aad9d4
Vincent <git@vincenttunru.com>
2025-04-01 15:23:18 +0200

    Set up OpenTelemetry
    This is just following some tutorials; I haven't managed to test
    this locally yet, and I'm not sure if the service name is correct.

--- src/instrumentation.node.ts
+++ src/instrumentation.node.ts
@@ -3,8 +3,14 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 import * as Sentry from "@sentry/nextjs";
+import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
+import { resourceFromAttributes } from "@opentelemetry/resources";
+import { NodeSDK } from "@opentelemetry/sdk-node";
+import { SimpleSpanProcessor } from "@opentelemetry/sdk-trace-node";
+import { ATTR_SERVICE_NAME } from "@opentelemetry/semantic-conventions";
 
 export function register() {
+  // Initialize Sentry
   Sentry.init({
     environment: process.env.APP_ENV,
     dsn: process.env.SENTRY_DSN,
@@ -17,4 +23,13 @@ export function register() {
     // Setting this option to true will print useful information to the console while you're setting up Sentry.
     debug: false,
   });
+
+  // Initialize OpenTelemetry
+  const sdk = new NodeSDK({
+    resource: resourceFromAttributes({
+      [ATTR_SERVICE_NAME]: "firefoxmonitor",
+    }),
+    spanProcessor: new SimpleSpanProcessor(new OTLPTraceExporter()),
+  });
+  sdk.start();
 }
