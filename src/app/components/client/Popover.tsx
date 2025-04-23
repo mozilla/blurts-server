72e60caa01e5eeae8b32436f0c79a3306ccbc604
Kaitlyn Andres <kandres@mozilla.com>
2025-04-23 10:08:26 -0400

    Update src/app/components/client/Popover.tsx
    Co-authored-by: Florian Zia <zia.florian@gmail.com>

--- src/app/components/client/Popover.tsx
+++ src/app/components/client/Popover.tsx
@@ -31,7 +31,6 @@ function Popover({ children, offset, state, ...props }: PopoverProps) {
   return (
     <Overlay>
       <div {...underlayProps} className={styles.underlay} />
-
       <div
         {...popoverProps}
         ref={props.popoverRef as React.RefObject<HTMLDivElement | null>}
