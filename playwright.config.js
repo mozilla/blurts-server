443a05afd55344d39f9c4f698487c1e429782dfb
Florian Zia <zia.florian@gmail.com>
2025-05-02 13:51:25 +0200

    chore: Force feature flags for local E2E tests

--- playwright.config.js
+++ playwright.config.js
@@ -2,46 +2,46 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import { defineConfig, devices } from '@playwright/test'
+import { defineConfig, devices } from "@playwright/test";
 /**
  * Read environment variables from file.
  * https://www.npmjs.com/package/dotenv-flow
  */
-import * as dotenvFlow from 'dotenv-flow'
-dotenvFlow.config()
+import * as dotenvFlow from "dotenv-flow";
+dotenvFlow.config();
 /**
  * @see https://playwright.dev/docs/test-configuration
  */
 
 const webServerConfig = {
-  command: 'npm run build; npm start',
+  command: "npm run build; npm start",
   // Building the app can take some time:
   timeout: 600_000,
-  port: 6060
-}
+  port: 6060,
+};
 
-const shouldStartWebServer = process.env.E2E_TEST_ENV === "local"
+const shouldStartWebServer = process.env.E2E_TEST_ENV === "local";
 
 export default defineConfig({
-  testDir: 'src/e2e/specs',
+  testDir: "src/e2e/specs",
   /* Maximum time one test can run for. */
   timeout: 60_000,
 
   /* Global setup */
-  globalSetup: 'src/e2e/global-setup.ts',
+  globalSetup: "src/e2e/global-setup.ts",
 
   /* Max time in milliseconds the whole test suite can to prevent CI breaking. */
   globalTimeout: 1_800_000,
 
   // adding missing snapshots for later comparison
-  updateSnapshots: 'missing',
+  updateSnapshots: "missing",
 
   expect: {
     /**
      * Maximum time expect() should wait for the condition to be met.
      * For example in `await expect(locator).toHaveText();`
      */
-    timeout: 2_000
+    timeout: 2_000,
   },
 
   /* Run tests in files in parallel */
@@ -57,7 +57,7 @@ export default defineConfig({
   workers: process.env.CI ? "75%" : undefined,
 
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: process.env.CI ? [['github'], ['html']] : 'html',
+  reporter: process.env.CI ? [["github"], ["html"]] : "html",
 
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
@@ -68,36 +68,42 @@ export default defineConfig({
     baseURL: process.env.E2E_TEST_BASE_URL,
 
     /* automatically take screenshot only on failures */
-    screenshot: 'only-on-failure',
+    screenshot: "only-on-failure",
 
     /* automatically record video on retry  */
-    video: 'retry-with-video',
+    video: "retry-with-video",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry'
+    trace: "on-first-retry",
+
+    /* Set feature flags for local E2E tests */
+    extraHTTPHeaders: {
+      "x-forced-feature-flags":
+        "SidebarNavigationRedesign,EditScanProfileDetails",
+    },
   },
 
   /* Configure projects for major browsers */
   projects: [
     {
-      name: 'chromium',
+      name: "chromium",
       use: {
-        ...devices['Desktop Chrome'],
+        ...devices["Desktop Chrome"],
         viewport: {
           width: 1920,
-          height: 1080
-        }
-      }
+          height: 1080,
+        },
+      },
     },
     {
-      name: 'firefox',
+      name: "firefox",
       use: {
-        ...devices['Desktop Firefox'],
+        ...devices["Desktop Firefox"],
         viewport: {
           width: 1920,
-          height: 1080
-        }
-      }
+          height: 1080,
+        },
+      },
     },
 
     /* Test against mobile viewports. */
@@ -122,8 +128,8 @@ export default defineConfig({
   ],
 
   /* Folder for test artifacts such as screenshots, videos, traces, etc. */
-  outputDir: 'src/e2e/test-results/',
+  outputDir: "src/e2e/test-results/",
 
   // Run your local dev server before starting the tests -- should run only on PRs or when prompted
-  ...(shouldStartWebServer && { webServer: webServerConfig })
-})
+  ...(shouldStartWebServer && { webServer: webServerConfig }),
+});
