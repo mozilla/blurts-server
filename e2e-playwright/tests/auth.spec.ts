ea0dfa44e70158c8ccb08c808604c08e3a66583c
Florian Zia <zia.florian@gmail.com>
2025-07-25 20:22:05 +0200

    fix: Missing expect import

--- e2e-playwright/tests/auth.spec.ts
+++ e2e-playwright/tests/auth.spec.ts
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import { test } from "../fixtures/baseTest";
+import { test, expect } from "../fixtures/baseTest";
 import { test as authenticatedTest } from "../fixtures/authenticatedTest";
 import { getBaseTestEnvUrl } from "../utils/environment";
 import { goToFxA, signInUser } from "../utils/fxa";
