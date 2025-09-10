d96ed435492f5a339869a37504759be3874f8270
Vincent <git@vincenttunru.com>
2025-09-10 22:16:46 +0200

    Delete Mozilla account at end of functional tests

--- functional-tests/tests/global-teardown.ts
+++ functional-tests/tests/global-teardown.ts
@@ -29,6 +29,16 @@ async function deleteTestUserAccounts(browser: Browser) {
     const page = await context.newPage();
 
     await page.goto(`${getBaseTestEnvUrl()}/user/settings/manage-account`);
+
+    await page.getByRole("button", { name: "Open user menu" }).click();
+    const manageAccountLink = page.getByRole("link", {
+      name: "Manage your ⁨Mozilla account⁩",
+      exact: false,
+    });
+    const manageFxaAccountUrl = await manageAccountLink.getAttribute("href");
+    // Close the user menu
+    await manageAccountLink.press("Escape");
+
     await page.getByRole("button", { name: "Delete account" }).click();
     const deleteDialog = page.getByRole("dialog", {
       name: "Your ⁨Monitor⁩ account will be permanently deleted",
@@ -39,11 +49,29 @@ async function deleteTestUserAccounts(browser: Browser) {
     await deleteButtonConfirm.click();
     await page.waitForURL(`${getBaseTestEnvUrl()}/`);
 
+    await page.goto(manageFxaAccountUrl!);
+    await page.getByRole("link", { name: "Delete Account" }).click();
+    const consequenceAcknowledgements = await page
+      .getByTestId("checkbox-container")
+      .all();
+    for (const acknowledgement of consequenceAcknowledgements) {
+      await acknowledgement.click();
+    }
+    await page.getByRole("button", { name: "Continue" }).click();
+
+    await page
+      .getByLabel("Enter password")
+      .fill(process.env.E2E_TEST_ACCOUNT_BASE_PASSWORD as string);
+    await page.getByRole("button", { name: "Delete" }).click();
+
     await context.close();
   }
 }
 
 teardown("Delete test user accounts", async ({ browser }) => {
+  // Going through both the Monitor and the FxA delete flow for
+  // every account takes relatively long:
+  teardown.slow();
   await deleteTestUserAccounts(browser);
   removeTestStorage();
 });
