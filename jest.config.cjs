511f9967c82194f4296e2dcfb0352780ded711c5
Vincent <git@vincenttunru.com>
2025-09-15 13:14:19 +0200

    npx @next/codemod@canary next-lint-to-eslint-cli .
    (And fix newly-appearing lint errors.)
    
    `next lint` will be deprecated with Next.js 16.

--- jest.config.cjs
+++ jest.config.cjs
@@ -2,6 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+// This is a CJS file, so `require` is fine here:
+// eslint-disable-next-line @typescript-eslint/no-require-imports
 const nextJest = require("next/jest");
 const createJestConfig = nextJest({ dir: "./" });
 
