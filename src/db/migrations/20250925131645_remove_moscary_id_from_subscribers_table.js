17ebbd4f05432d1bfd82812f28185e171cf46dce
Vincent <git@vincenttunru.com>
2025-09-25 15:23:34 +0200

    Remove moscary_id from the subscribers table

---
+++ src/db/migrations/20250925131645_remove_moscary_id_from_subscribers_table.js
@@ -0,0 +1,25 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function up(knex) {
+  return knex.schema.alterTable("subscribers", (table) => {
+    table.dropIndex("moscary_id");
+    table.dropColumn("moscary_id");
+  });
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function down(knex) {
+  return knex.schema.alterTable("subscribers", (table) => {
+    table.uuid("moscary_id").nullable();
+    table.index("moscary_id");
+  });
+}
