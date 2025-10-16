9d1b3220d15bce2b15fd0ffa92197bf77aaeba5d
Kaitlyn <kandres@mozilla.com>
2025-10-16 18:39:43 -0400

    fix breaking tests

--- src/app/(proper_react)/(redesign)/MobileShell.tsx
+++ src/app/(proper_react)/(redesign)/MobileShell.tsx
@@ -14,10 +14,8 @@ import { UpsellBadge } from "../../components/client/toolbar/UpsellBadge";
 import { CloseBigIcon, ListIcon } from "../../components/server/Icons";
 import { UserMenu } from "../../components/client/toolbar/UserMenu";
 import { useL10n } from "../../hooks/l10n";
-import { PageLink } from "./PageLink";
 import { useTelemetry } from "../../hooks/useTelemetry";
 import { usePathname } from "next/navigation";
-import { CONST_SETTINGS_TAB_SLUGS } from "../../../constants";
 import { FeatureFlagName } from "../../../db/tables/featureFlags";
 import { SignInButton } from "../../components/client/SignInButton";
 import { TopNavBar } from "./(public)/TopNavBar";
@@ -157,86 +155,13 @@ export const MobileShell = (props: Props) => {
             </div>
           ) : (
             <div className={styles.mainMenu}>
-              {props.session ? (
-                <>
-                  <ul>
-                    <li key="home">
-                      <PageLink
-                        href="/user/dashboard"
-                        activeClassName={styles.isActive}
-                        hasTelemetry={{ link_id: "navigation_dashboard" }}
-                      >
-                        {l10n.getString("main-nav-link-dashboard-label")}
-                      </PageLink>
-                    </li>
-                    {props.countryCode === "us" && (
-                      <li key="how-it-works">
-                        <PageLink
-                          href="/how-it-works"
-                          activeClassName={styles.isActive}
-                          target="_blank"
-                          hasTelemetry={{ link_id: "navigation_how_it_works" }}
-                        >
-                          {l10n.getString("main-nav-link-how-it-works-label")}
-                        </PageLink>
-                      </li>
-                    )}
-                    <li key="faq">
-                      <PageLink
-                        href="https://support.mozilla.org/kb/firefox-monitor-faq"
-                        title={l10n.getString("main-nav-link-faq-tooltip")}
-                        target="_blank"
-                        hasTelemetry={{ link_id: "navigation_faq" }}
-                      >
-                        {l10n.getString("main-nav-link-faq-label")}
-                      </PageLink>
-                    </li>
-                    <li key="settings">
-                      <PageLink
-                        href="/user/settings"
-                        activeClassName={styles.isActive}
-                        hasTelemetry={{ link_id: "navigation_settings" }}
-                      >
-                        {l10n.getString("main-nav-link-settings-label")}
-                      </PageLink>
-                      <ul className={styles.subMenu}>
-                        {CONST_SETTINGS_TAB_SLUGS.filter(
-                          (submenuKey) => submenuKey !== "edit-profile",
-                        ).map((submenuKey) => {
-                          return (
-                            <li key={submenuKey}>
-                              <PageLink
-                                href={`/user/settings/${submenuKey}`}
-                                activeClassName={styles.isActive}
-                                hasTelemetry={{
-                                  link_id: `navigation_settings_${submenuKey}`,
-                                }}
-                              >
-                                {l10n.getString(
-                                  `settings-tab-label-${submenuKey}`,
-                                )}
-                              </PageLink>
-                            </li>
-                          );
-                        })}
-                      </ul>
-                    </li>
-                  </ul>
-                  <div className={styles.premiumCta}>
-                    <UpsellBadge
-                      // The last scan date is too noisy on mobile, so don't show it there:
-                      lastScanDate={null}
-                      enabledFeatureFlags={props.enabledFeatureFlags}
-                    />
-                  </div>
-                </>
-              ) : props.enabledFeatureFlags.includes("LandingPageRedesign") &&
-                props.experimentData[
-                  "landing-page-redesign-plus-eligible-experiment"
-                ].enabled &&
-                props.experimentData[
-                  "landing-page-redesign-plus-eligible-experiment"
-                ].variant === "redesign" ? (
+              {props.enabledFeatureFlags.includes("LandingPageRedesign") &&
+              props.experimentData[
+                "landing-page-redesign-plus-eligible-experiment"
+              ].enabled &&
+              props.experimentData[
+                "landing-page-redesign-plus-eligible-experiment"
+              ].variant === "redesign" ? (
                 // The old <TopNavBar /> component is no longer hit by unit tests
                 // that have already enabled the experiment, so ignore that for now:
                 // (But c8 is weird so just pretend that this ignore comment is
