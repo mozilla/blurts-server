42c96ffcb725f35d155e4dd5c839a9e076179bb2
Kaitlyn <kandres@mozilla.com>
2025-10-22 15:58:56 -0400

    remove tsx unused

--- src/app/(proper_react)/(redesign)/(authenticated)/user/(dashboard)/settings/panels/index.tsx
+++ src/app/(proper_react)/(redesign)/(authenticated)/user/(dashboard)/settings/panels/index.tsx
@@ -3,10 +3,6 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 import {
-  SettingsPanelEditInfo,
-  SettingsPanelEditInfoProps,
-} from "./SettingsPanelEditInfo";
-import {
   SettingsPanelEditInfoRedesign,
   SettingsPanelEditInfoRedesignProps,
 } from "./SettingsPanelEditInfoRedesign";
@@ -25,10 +21,7 @@ import {
 import styles from "./Panel.module.scss";
 import { TabType } from "../View";
 
-export type SettingsProps = (
-  | SettingsPanelEditInfoProps
-  | SettingsPanelEditInfoRedesignProps
-) &
+export type SettingsProps = SettingsPanelEditInfoRedesignProps &
   SettingsPanelNotificationsProps &
   SettingsPanelManageAccountProps &
   SettingsPanelEditProfileProps & {
@@ -39,21 +32,15 @@ export type SettingsProps = (
 function Panel(props: SettingsProps) {
   switch (props.activeTab) {
     case "edit-info":
-      return props.enabledFeatureFlags.includes("EditScanProfileDetails") ? (
-        <SettingsPanelEditInfoRedesign {...props} />
-      ) : (
-        <SettingsPanelEditInfo {...props} />
-      );
+      return <SettingsPanelEditInfoRedesign {...props} />;
     case "notifications":
       return <SettingsPanelNotifications {...props} />;
     case "manage-account":
       return <SettingsPanelManageAccount {...props} />;
     case "edit-profile":
-      if (props.enabledFeatureFlags.includes("EditScanProfileDetails")) {
-        return <SettingsPanelEditProfile {...props} />;
-      }
+      return <SettingsPanelEditProfile {...props} />;
     default:
-      return <SettingsPanelEditInfo {...props} />;
+      return <SettingsPanelEditInfoRedesign {...props} />;
   }
 }
 
