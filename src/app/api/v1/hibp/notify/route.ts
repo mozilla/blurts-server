e585800065f1ea81dbd3f0eaf0db60254c43612c
Kat Schelonka <kschelonka@mozilla.com>
2026-02-19 10:49:15 -0800

    chore: remove the double mpl header

--- src/app/api/v1/hibp/notify/route.ts
+++ src/app/api/v1/hibp/notify/route.ts
@@ -4,10 +4,6 @@
 
 export const runtime = "nodejs";
 
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-
 import { NextRequest, NextResponse } from "next/server";
 
 import { bearerToken } from "../../../utils/auth";
